c02418b7fe4f05313e57738c19368b20af354c5c
dtracers <dtracers@gmail.com>
2013-08-30 16:29:44 -0500

    added the expanding children menu for classes.
    Now if you click on View All Classes in the menu then you can see the
    list of classes that are you are in.
    (HARDCODED NOT USER SPECIFIC)

--- CourseSketchWebClient/js/menu/add_menu.js
+++ CourseSketchWebClient/js/menu/add_menu.js
@@ -3,8 +3,7 @@ function placeMenu() {
 }
 
 function getMenu() {
-	return	'<script type="text/javascript" src="js/expanding_menu.js"></script>' +
-			'<link rel="stylesheet" href="css/menu/menu.css">' +
+	return	'<link rel="stylesheet" href="css/menu/menu.css">' +
 			'<div id="menu">' +
 			'	<h1>' +
 			'		<a href="home.html" data-ajax="false"><img src="images/smallTitle.svg"></a>' +
@@ -13,7 +12,7 @@ function getMenu() {
 			'		<li><a href="home.html" data-ajax="false"' +
 			'			class="contentLink">Home</a></li>' +
 			'		<li class="header"><h3>Classes I\'m In</h3></li>' +
-			'		<!-- listofClasses is appended by the js in mainmenu.js . It uses GAPI to get the classes-->' +
+			'		<!-- listofClasses is appended by the js in add_menu.js . It uses class_data.js to get the classes -->' +
 			'		<li onclick="list_menu_classes()">' +
 			'			<div class="class_button">' +
 			'				<img id="expandable_arow_button" src="images/menu/triangle_right.png" width="15" height="15">' +
@@ -33,3 +32,21 @@ function getMenu() {
 			'	</ul>' +
 			'</div>';
 }
+
+function list_menu_classes() {
+	var html = '';
+	var class_list = document.getElementById('list_of_classes');
+	if(class_list_showing) {
+		class_list_showing = false;
+		class_list.style.display = "none";
+	} else {
+		class_list_showing = true;
+		html = createClassList(false, false);
+		class_list.style.display = "block";
+		// ADD CLASSES HERE
+	}
+	class_list.innerHTML = html;
+
+}
+
+var class_list_showing = false;
--- CourseSketchWebClient/js/school/class/add_class_list.js
+++ CourseSketchWebClient/js/school/class/add_class_list.js
@@ -2,17 +2,17 @@ function addClassList(id, isSimple, showImage) {
 	document.getElementById('due_assignments' + id).innerHTML = addClassList(isSimple);
 }
 
-function createClassList(isSimple) {
-	html+='<ul>';
+function createClassList(isComplex, showImage) {
+	var html = '<ul>';
 	var currentDate = new Date();
 	for(var i = 0; i< user_classes.length; i++) {
 		var list = user_classes[i];
 		var dueDate = list[2];
 		var dateType = getDateType(dueDate, currentDate);
 		html+='<li>';
-		html+='	<div class="' + isSimple?'class_item':'school_item' + ">';
+		html+='	<div class="' + (isComplex ? 'school_item':'class_item' ) + '">';
 		html+='		<div class="text">';
-		if(!isSimple) {
+		if(isComplex) {
 			html+='			<h3 class="name"><a href="'+list[0][1]+'">'+list[0][0]+'</a></h3>';
 			html+='			<p>'+list[2]+'</p>';
 		}else {
