65361841bc495453db0e999a629aeecada466cbb
dtracers <dtracers@gmail.com>
2013-09-20 01:35:26 -0500

    Addded dynamic css and js loading

--- coursesketchwebclient/js/utils/loadingManager.js
+++ coursesketchwebclient/js/utils/loadingManager.js
@@ -11,6 +11,42 @@ function addLoadEvent(func) {
     }
   }
 }
+
+function loadDynamicFile(filename, filetype, callBack) {
+	if(callBack && typeof callBack !== 'function') {
+		throw 'Not a valid callBack';  
+	}
+	var fileref = false;
+	if (filetype=="js"){ //if filename is a external JavaScript file
+		fileref=document.createElement('script');
+		fileref.setAttribute("type","text/javascript");
+		fileref.setAttribute("src", filename);
+	}
+	else if (filetype=="css"){ //if filename is an external CSS file
+		fileref=document.createElement("link");
+		fileref.setAttribute("rel", "stylesheet");
+		fileref.setAttribute("type", "text/css");
+		fileref.setAttribute("href", filename);
+	}
+	
+	if (fileref) {
+		if(callBack) {
+			fileref.onload = callBack; 
+		}
+		document.getElementsByTagName("head")[0].appendChild(fileref);
+	}
+}
+
+addLoadEvent(function () {
+	loadDynamicFile("js/menu/add_menu.js",'js',function() {
+		placeMenu();	
+	}
+	);
+});
+
+loadDynamicFile("css/jquery/jquery.mobile-1.0rc2.min.css",'css');
+loadDynamicFile("css/home.css",'css');
+
 /*
 
 EXAMPLE USES
