b68a861d379db4e38c1fe874a0d5a2510bd80b62
dtracers <dtracers@gmail.com>
2013-09-20 10:46:01 -0500

    Added more utility functions to the loading manager

--- coursesketchwebclient/js/utils/loadingManager.js
+++ coursesketchwebclient/js/utils/loadingManager.js
@@ -12,40 +12,84 @@ function addLoadEvent(func) {
   }
 }
 
-function loadDynamicFile(filename, filetype, callBack) {
-	if(callBack && typeof callBack !== 'function') {
-		throw 'Not a valid callBack';  
-	}
-	var fileref = false;
-	if (filetype=="js"){ //if filename is a external JavaScript file
-		fileref=document.createElement('script');
-		fileref.setAttribute("type","text/javascript");
-		fileref.setAttribute("src", filename);
-	}
-	else if (filetype=="css"){ //if filename is an external CSS file
-		fileref=document.createElement("link");
-		fileref.setAttribute("rel", "stylesheet");
-		fileref.setAttribute("type", "text/css");
-		fileref.setAttribute("href", filename);
-	}
-	
-	if (fileref) {
-		if(callBack) {
-			fileref.onload = callBack; 
+/**
+ * A class that handles the dynamic loading of files.
+ */
+function dynamicFileLoader() {
+
+	this.loadFile = function loadFile(filename, filetype, callBack) {
+		var fileref = this.createFile(filename, filetype, callBack);
+
+		if (fileref) {
+			document.getElementsByTagName("head")[0].appendChild(fileref);
 		}
-		document.getElementsByTagName("head")[0].appendChild(fileref);
+	}
+
+	this.createFile = function createFile(filename, filetype, callBack) {
+		if(callBack && typeof callBack !== 'function') {
+			throw 'Not a valid callBack';  
+		}
+		var fileref = false;
+		if (filetype=="js"){ //if filename is a external JavaScript file
+			fileref=document.createElement('script');
+			fileref.setAttribute("type","text/javascript");
+			fileref.setAttribute("src", filename);
+		}
+		else if (filetype=="css"){ //if filename is an external CSS file
+			fileref=document.createElement("link");
+			fileref.setAttribute("rel", "stylesheet");
+			fileref.setAttribute("type", "text/css");
+			fileref.setAttribute("href", filename);
+		}
+
+		if(fileref && callBack) {
+			fileref.onload = callBack;
+		}
+		return fileref;
+	}	
+
+	/**
+	 * Removes the given javascript file.
+	 */
+	this.removeFile = function removefile(filename, filetype){
+		var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none"; //determine element type to create nodelist from
+		var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none"; //determine corresponding attribute to test for
+		var allsuspects=document.getElementsByTagName(targetelement);
+		for (var i=allsuspects.length; i>=0; i--) { //search backwards within nodelist for matching elements to remove
+			if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1) {
+				allsuspects[i].parentNode.removeChild(allsuspects[i]); //remove element by calling parentNode.removeChild()
+			}
+		}
+	}
+
+	/**
+	 * Replaces the old filename with the new filename.
+	 * They must be the same filetype.
+	 */
+	this.replaceFile = function replaceFile(oldfilename, newfilename, filetype){
+		var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none"; //determine element type to create nodelist using
+		var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none"; //determine corresponding attribute to test for
+		var allsuspects=document.getElementsByTagName(targetelement);
+		for (var i=allsuspects.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
+			if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(oldfilename)!=-1){
+				var newelement=createjscssfile(newfilename, filetype);
+				allsuspects[i].parentNode.replaceChild(newelement, allsuspects[i]);
+			}
+		}
 	}
 }
 
+
+var loader = new dynamicFileLoader();
+loader.loadFile("css/jquery/jquery.mobile-1.0rc2.min.css",'css');
+loader.loadFile("css/home.css",'css');
+
 addLoadEvent(function () {
-	loadDynamicFile("js/menu/add_menu.js",'js',function() {
+	loader.loadFile("js/menu/add_menu.js",'js',function() {
 		placeMenu();	
 	}
 	);
 });
-
-loadDynamicFile("css/jquery/jquery.mobile-1.0rc2.min.css",'css');
-loadDynamicFile("css/home.css",'css');
 
 /*
 
