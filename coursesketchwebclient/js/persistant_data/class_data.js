569a77cf7d38e9192552649250db9529f6d74999
dtracers <dtracers@gmail.com>
2013-10-03 12:32:19 -0500

    Added Construction tape to uncompleted assignments.
    This applies to student assignments and professor assignments

--- coursesketchwebclient/js/persistant_data/class_data.js
+++ coursesketchwebclient/js/persistant_data/class_data.js
@@ -1,42 +1,65 @@
 var upcomming_assignments =
 	[
 	 	[
+	 	 	'assignment',
 	 	 	['Truss Assignment1','Truss_Link1','Assignment_Truss_1_ID'],
 	 	 	'In this assignment we will draw a truss trussty truss.' +
 	 	 	' This truss trussty truss will allow us to save sience.',
 	 	 	'test/truss_thumb.png',
 	 	 	['Phyics','PhysicsLink','Class_Physics_ID'],
-	 	 	new Date('Thu Aug 29 2013 11:24:27 GMT-0500 (CDT)')
+	 	 	new Date('Thu Aug 29 2013 11:24:27 GMT-0500 (CDT)'),
+	 	 	'completed'
 	 	],
 	 	[
+	 	 	'assignment',
 	 	 	['Chemistry Assignment1','Chemistry_Link1', 'Assignment_Chemistry_1_ID'],
 	 	 	'n this assignment we will do a chemistry diagram.',
 	 	 	'test/chemistry_thumb.png',
 	 	 	['Chemistry','ChemistryLink','Class_Chemistry_ID'],
 	 	 	new Date('Sun Sep 1 2013 11:24:27 GMT-0500 (CDT)'),
+	 	 	'started'
  	 	],
 	 	[
+	 	 	'assignment',
 	 	 	['Truss Assignment2','Truss_Link2', 'Assignment_Truss_2_ID'],
 	 	 	'In this second assignment we will make something cool. YAYYYYYY',
 	 	 	'test/truss_thumb.png',
 	 	 	['Phyics','PhysicsLink','Class_Physics_ID'],
-	 	 	new Date('Thu Aug 29 2013 11:24:27 GMT-0500 (CDT)')
+	 	 	new Date('Thu Aug 29 2013 11:24:27 GMT-0500 (CDT)'),
+	 	 	'notStarted'
  	 	],
 	];
 
 var user_classes =
 	[
-	 	[['Physics','PhysicsLink','Class_Physics_ID'],'Description for me class blah blah blabity blah',
-	 	 	'test/truss_thumb.png'],
-	 	[['Chemistry','ChemistryLink','Class_Chemistry_ID'],'Description for the chemistry class!',
-	 	 	'test/chemistry_thumb.png'],
-	 	[['Class 3','Class3Link','Class_Class_3_ID'],'Description for the 3rd class!', 'test/no_image.png'],
+	 	[
+	 	 	'class',
+		 	['Physics','PhysicsLink','Class_Physics_ID'],
+		 	'Description for me class blah blah blabity blah',
+	 	 	'test/truss_thumb.png'
+	 	],
+	 	[
+	 	 	'class',
+	 	 	['Chemistry','ChemistryLink','Class_Chemistry_ID'],
+	 	 	'Description for the chemistry class!',
+	 	 	'test/chemistry_thumb.png'
+	 	],
+	 	[
+	 	 	'class',
+	 	 	['Class 3','Class3Link','Class_Class_3_ID'],
+	 	 	'Description for the 3rd class!',
+	 	 	'test/no_image.png'
+	 	 ],
 	];
 	
 var questions =
 	[
-	 	[['TrussProblem1','TrussProblem1Link','Problem_Truss_1_ID'],'Question Text for problem one is blahbity blah blah',
-	 	 	'test/truss_thumb.png']
+	 	[
+	 	 	'problem',
+	 	 	['TrussProblem1','TrussProblem1Link','Problem_Truss_1_ID'],
+	 	 	'Question Text for problem one is blahbity blah blah',
+	 	 	'test/truss_thumb.png'
+	 	 ]
 	];
 
 var classAssignments = new listMap();	
