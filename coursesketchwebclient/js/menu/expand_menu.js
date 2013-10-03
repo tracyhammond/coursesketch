11134ef2f62d6818595dddca995125126d4e9edb
dtracers <dtracers@gmail.com>
2013-10-03 10:31:35 -0500

    Expanding menu works and arrows change correctly

--- coursesketchwebclient/js/menu/expand_menu.js
+++ coursesketchwebclient/js/menu/expand_menu.js
@@ -1,27 +1,27 @@
 /**
  * Creates the ability to expand menus.
  *
- * 
  * Assume creation before/during page getting ready.
+ * numberOfElements: this is the total number of panels that you want to expand.
+ * containerID: the id of the container containing expandable menu parts.
+ * scrollPage: if true then the page will scroll.
  */
 function expandingMenu(numberOfElements, containerID, scrollPage) {
 	var totalPanels = numberOfElements;
 	var panelSpeed = 500; //panel animate speed in milliseconds
 	var scrollPage = false; // set to true to attempt to scroll page as elements are expanded.
     var defaultOpenPanel = 0; //leave 0 for no panel open   
-    var accordian = false; //set panels to behave like an accordian, with one panel only ever open at once      
+    var accordian = accordian; //set panels to behave like an accordian, with one panel only ever open at once      
   
     var panelHeight = new Array();
     var currentPanel = defaultOpenPanel;
-    var iconCloseHeight = parseInt($('.icon-close').css('height'));
-    var iconOpenHeight = parseInt($('.icon-open').css('height'));
     var highlightOpen = true;
-    
-    var $containerScope = $(containerID);
+
 	$(document).ready(function () { 
+		var $containerScope = $('#' + containerID);
 		//Initialise collapsible panels
         function panelinit() {
-            for (var i=1; i<=totalpanels; i++) {
+            for (var i=1; i<=totalPanels; i++) {
             	var $specificPanel = $containerScope.find('#cp-'+i);
             	// Gets the total height of the content
                 panelHeight[i] = parseInt($specificPanel.find('.expandable-panel-content').css('height'));
@@ -33,11 +33,7 @@ function expandingMenu(numberOfElements, containerID, scrollPage) {
             }
         }
 
-		$(window).load(function() {
-	        panelinit();
-	    }); //END LOAD
-		
-    	$containerScope.find('.expandable-panel-heading').click(function() {           
+    	$containerScope.find('.expandable-panel-heading').click(function() {
             var obj = $(this).next();
             // Gets the objects ID.
             var objid = parseInt($(this).parent().attr('ID').substr(3,2));  
@@ -47,17 +43,22 @@ function expandingMenu(numberOfElements, containerID, scrollPage) {
             }
 
             // Check to see if we need to open panel.
-            if (parseInt(obj.css('margin-top')) <= (panelheight[objid]*-1)) {
+         //   alert('height: '+ parseInt(obj.css('margin-top')));
+            if (parseInt(obj.css('margin-top')) < 0) {
                 obj.clearQueue();
                 obj.stop();
                 openPanel($(this).parent());
             } else {
                 obj.clearQueue();
                 obj.stop();
-                closePanel($(this).parent());
+                closePanel($(this).parent(),panelHeight[objid]);
             }
-        });
-	}
+        }); // END CLICK
+
+		$(window).load(function() {
+		    panelinit();
+		}); //END LOAD
+	}); // END READY
 	
 	/**
 	 * Opens without an animation.
@@ -86,9 +87,10 @@ function expandingMenu(numberOfElements, containerID, scrollPage) {
 		var $iconElement = $panel.find('#expandable-panel-icon');
 		$iconElement.removeClass('icon-open');
 		$iconElement.addClass('icon-close');
-		$panel.find('.expandable-panel-content').animate({'margin-top':-panelHeight}, panelspeed);
+		$panel.find('.expandable-panel-content').animate({'margin-top':-panelHeight}, panelSpeed);
+		// TODO: set display to none after closing.
 		// TODO: add scrolling.
-        if (highlightopen == true) {
+        if (highlightOpen == true) {
         	$panel.find('.expandable-panel-heading').removeClass('header-active');
         }
 	}
@@ -100,9 +102,9 @@ function expandingMenu(numberOfElements, containerID, scrollPage) {
 		var $iconElement = $panel.find('#expandable-panel-icon');
 		$iconElement.removeClass('icon-close');
 		$iconElement.addClass('icon-open');
-		$panel.find('.expandable-panel-content').animate({'margin-top':0}, panelspeed);
+		$panel.find('.expandable-panel-content').animate({'margin-top':0}, panelSpeed);
 		// TODO: add scrolling.
-        if (highlightopen == true) {
+        if (highlightOpen == true) {
         	$panel.find('.expandable-panel-heading').addClass('header-active');
         }
 	}
