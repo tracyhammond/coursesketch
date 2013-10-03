bd5103e465358f78be4bbd9cc9f5861027cda3a6
dtracers <dtracers@gmail.com>
2013-10-03 13:08:07 -0500

    added more information
    TODO:
    Wrap all of the data in classes

--- coursesketchwebclient/js/persistant_data/class_data.js
+++ coursesketchwebclient/js/persistant_data/class_data.js
@@ -10,6 +10,15 @@ var upcomming_assignments =
 	 	 	new Date('Thu Aug 29 2013 11:24:27 GMT-0500 (CDT)'),
 	 	 	'completed'
 	 	],
+		[
+		 	'assignment',
+		 	['Truss Assignment2','Truss_Link2', 'Assignment_Truss_2_ID'],
+		 	'In this second assignment we will make something cool. YAYYYYYY',
+		 	'test/truss_thumb.png',
+		 	['Phyics','PhysicsLink','Class_Physics_ID'],
+		 	new Date('Thu Aug 29 2013 11:24:27 GMT-0500 (CDT)'),
+		 	'notStarted'
+		],
 	 	[
 	 	 	'assignment',
 	 	 	['Chemistry Assignment1','Chemistry_Link1', 'Assignment_Chemistry_1_ID'],
@@ -21,12 +30,30 @@ var upcomming_assignments =
  	 	],
 	 	[
 	 	 	'assignment',
-	 	 	['Truss Assignment2','Truss_Link2', 'Assignment_Truss_2_ID'],
-	 	 	'In this second assignment we will make something cool. YAYYYYYY',
-	 	 	'test/truss_thumb.png',
-	 	 	['Phyics','PhysicsLink','Class_Physics_ID'],
-	 	 	new Date('Thu Aug 29 2013 11:24:27 GMT-0500 (CDT)'),
-	 	 	'notStarted'
+	 	 	['Chemistry Assignment2','Chemistry_Link2', 'Assignment_Chemistry_2_ID'],
+	 	 	'n this assignment we will do a chemistry diagram.',
+	 	 	'test/chemistry_thumb.png',
+	 	 	['Chemistry','ChemistryLink','Class_Chemistry_ID'],
+	 	 	new Date('Sun Sep 1 2013 11:24:27 GMT-0500 (CDT)'),
+	 	 	'studentStarted'
+	 	],
+	 	[
+	 	 	'assignment',
+	 	 	['Chemistry Assignment3','Chemistry_Link3', 'Assignment_Chemistry_3_ID'],
+	 	 	'n this assignment we will do a chemistry diagram.',
+	 	 	'test/chemistry_thumb.png',
+	 	 	['Chemistry','ChemistryLink','Class_Chemistry_ID'],
+	 	 	new Date('Sun Sep 1 2013 11:24:27 GMT-0500 (CDT)'),
+	 	 	'visible'
+	 	],
+	 	[
+	 	 	'assignment',
+	 	 	['Class Assignment1','Class_Link3', 'Assignment_Class_1_ID'],
+	 	 	'n this assignment we will do a class item.',
+	 	 	'test/class_thumb.png',
+	 	 	['Class 3','Class3Link','Class_Class_3_ID'],
+	 	 	new Date('Sun Sep 1 2013 11:24:27 GMT-0500 (CDT)'),
+	 	 	'notVisible'
  	 	],
 	];
 
@@ -90,8 +117,11 @@ function listMap() {
 // TODO change the keys to actually be unique
 function initialize() {
 	classAssignments.addObject('Class_Physics_ID',upcomming_assignments[0]);
-	classAssignments.addObject('Class_Physics_ID',upcomming_assignments[2]);
-	classAssignments.addObject('Class_Chemistry_ID',upcomming_assignments[1]);
+	classAssignments.addObject('Class_Physics_ID',upcomming_assignments[1]);
+	classAssignments.addObject('Class_Chemistry_ID',upcomming_assignments[2]);
+	classAssignments.addObject('Class_Chemistry_ID',upcomming_assignments[3]);
+	classAssignments.addObject('Class_Chemistry_ID',upcomming_assignments[4]);
+	classAssignments.addObject('Class_Class_3_ID',upcomming_assignments[5]);
 	assignmentProblems.addObject('Assignment_Truss_2_ID',questions[0]);
 }
 
