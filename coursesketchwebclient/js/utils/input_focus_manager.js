ca2317cf45d8a7eb15bd0ce761980553d86ef802
dtracers <dtracers@gmail.com>
2013-10-08 15:01:58 -0500

    made fix for text box issue.

--- coursesketchwebclient/js/utils/input_focus_manager.js
+++ coursesketchwebclient/js/utils/input_focus_manager.js
@@ -5,13 +5,11 @@ function checkEnter(element) {
 }
 
 function makeEditable(element) {
-	element.readOnly='';
 	var name = element.className;
 	element.className = name.replace('notEditing','editing');
 }
 
 function makeUnEditable(element) {
-	element.readOnly='true';
 	var name = element.className;
 	element.className = name.replace('editing','notEditing');
 }
