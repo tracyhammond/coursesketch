b09b6fb059a4de759d8cf5ac183c676e4bd7166e
dtracers <dtracers@gmail.com>
2013-10-08 11:21:02 -0500

    The home page is now completly embedded in an iframe

--- coursesketchwebclient/js/utils/loadingManager.js
+++ coursesketchwebclient/js/utils/loadingManager.js
@@ -1,3 +1,6 @@
+/**
+ * Creates a recursive set of functions that are all called onload
+ */
 function addLoadEvent(func) {
   var oldonload = window.onload;
   if (typeof window.onload != 'function') {
@@ -13,15 +16,34 @@ function addLoadEvent(func) {
 }
 
 /**
+ * Creates a recursive set of functions that are all called onload
+ *
+ * This one is given a loading scope
+ */
+function addScopedLoadEvent(scope, func) {
+  var oldonload = scope.onload;
+  if (typeof scope.onload != 'function') {
+	  scope.onload = func;
+  } else {
+	  scope.onload = function() {
+      if (oldonload) {
+        oldonload();
+      }
+      func();
+    }
+  }
+}
+
+/**
  * A class that handles the dynamic loading of files.
  */
 function DynamicFileLoader() {
-
+	this.scope = document;
 	this.loadFile = function loadFile(filename, filetype, callBack) {
 		var fileref = this.createFile(filename, filetype, callBack);
 
 		if (fileref) {
-			document.getElementsByTagName("head")[0].appendChild(fileref);
+			this.scope.getElementsByTagName("head")[0].appendChild(fileref);
 		}
 	}
 
@@ -31,12 +53,12 @@ function DynamicFileLoader() {
 		}
 		var fileref = false;
 		if (filetype=="js"){ //if filename is a external JavaScript file
-			fileref=document.createElement('script');
+			fileref=this.scope.createElement('script');
 			fileref.setAttribute("type","text/javascript");
 			fileref.setAttribute("src", filename);
 		}
 		else if (filetype=="css"){ //if filename is an external CSS file
-			fileref=document.createElement("link");
+			fileref=this.scope.createElement("link");
 			fileref.setAttribute("rel", "stylesheet");
 			fileref.setAttribute("type", "text/css");
 			fileref.setAttribute("href", filename);
@@ -54,7 +76,7 @@ function DynamicFileLoader() {
 	this.removeFile = function removefile(filename, filetype){
 		var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none"; //determine element type to create nodelist from
 		var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none"; //determine corresponding attribute to test for
-		var allsuspects=document.getElementsByTagName(targetelement);
+		var allsuspects=this.scope.getElementsByTagName(targetelement);
 		for (var i=allsuspects.length; i>=0; i--) { //search backwards within nodelist for matching elements to remove
 			if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1) {
 				allsuspects[i].parentNode.removeChild(allsuspects[i]); //remove element by calling parentNode.removeChild()
@@ -69,7 +91,7 @@ function DynamicFileLoader() {
 	this.replaceFile = function replaceFile(oldfilename, newfilename, filetype){
 		var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none"; //determine element type to create nodelist using
 		var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none"; //determine corresponding attribute to test for
-		var allsuspects=document.getElementsByTagName(targetelement);
+		var allsuspects=this.scope.getElementsByTagName(targetelement);
 		for (var i=allsuspects.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
 			if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(oldfilename)!=-1){
 				var newelement=createjscssfile(newfilename, filetype);
@@ -82,7 +104,7 @@ function DynamicFileLoader() {
 
 var loader = new DynamicFileLoader();
 loader.loadFile("css/jquery/jquery.mobile-1.0rc2.min.css",'css');
-loader.loadFile("css/home.css",'css');
+loader.loadFile("css/main.css",'css');
 
 addLoadEvent(function () {
 	if(document.getElementById('menuBar')) {
