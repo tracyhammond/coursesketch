d13600af9e5ff244ea3d48b21c419190b34422ca
dtracers <dtracers@gmail.com>
2013-10-09 20:25:46 -0500

    Now you can login correct and it will redirect you! :)
    Now we need to just find a way to set the login information so if the
    page is refreshed re-logging in is not needed.  (maybe a cookie?) not
    sure what the safest way to do this is.

--- CourseSketchWebServer/src/connection/LoginChecker.java
+++ CourseSketchWebServer/src/connection/LoginChecker.java
@@ -1,19 +1,37 @@
 package connection;
 
+import protobuf.srl.request.Message.LoginInformation;
 import protobuf.srl.request.Message.Request;
+import protobuf.srl.request.Message.Request.MessageType;
 
 public class LoginChecker {
-	public static boolean checkLogin(Request req) {
+	/* package-private */ static boolean checkLogin(Request req) {
 		// Check log in information
 		// return failed log in information on failure
 		if(req.getRequestType() == Request.MessageType.LOGIN) {
 			String name = req.getLogin().getUsername();
 			String password = req.getLogin().getPassword();
 			System.out.println("USERNAME: " + name +"\nPASSWORD: " + password);
-			if((name.equalsIgnoreCase("matt") && password.equalsIgnoreCase("japan"))) {
+			if( (name.equalsIgnoreCase("matt") && password.equalsIgnoreCase("japan"))) {
 				return true;
 			}
 		}
 		return false;
+	}
+	
+	/* package-private */ static Request createResponse(Request req, boolean success) {
+		Request.Builder requestBuilder = Request.newBuilder();
+		requestBuilder.setRequestType(MessageType.LOGIN);
+		
+		// Create the Login Response.
+		LoginInformation.Builder loginBuilder = LoginInformation.newBuilder();
+		loginBuilder.setUsername(req.getLogin().getUsername());
+		loginBuilder.setIsLoggedIn(success);
+		
+		// Add login info.
+		requestBuilder.setLogin(loginBuilder.build());
+		
+		// Build and send.
+		return requestBuilder.build();
 	}
 }
