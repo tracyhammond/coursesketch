a30786dade80d559882a4bf761b512da6dd4972b
dtracers <dtracers@gmail.com>
2013-10-09 23:42:00 -0500

    gained the ability to edit the parent url to add redirects to it as needed.
    Also the appengine tried to make you login by default.

--- coursesketchwebclient/main.py
+++ coursesketchwebclient/main.py
@@ -19,7 +19,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.redirect('/main.html',permanent=True)
+        self.redirect('/index.html',permanent=True)
         #self.redirect('http://www.percussionstudio.org',permanent=True)
 
 
