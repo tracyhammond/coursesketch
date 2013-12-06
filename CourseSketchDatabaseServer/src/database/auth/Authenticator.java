97552d5f31ed9e5366a2951f2b3ebccec653d983
dtracers <dtracers@gmail.com>
2013-12-05 20:26:13 -0600

    added an auth finder
    Former-commit-id: f7061cd3ee71a86a4195c5fc6b124882957e721f

--- CourseSketchDatabaseServer/src/database/auth/Authenticator.java
+++ CourseSketchDatabaseServer/src/database/auth/Authenticator.java
@@ -1,5 +1,29 @@
 package database.auth;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+
 public class Authenticator {
 
+	/**
+	 * Checks to see if a user is allowed within the current groupList.
+	 *
+	 * looks up the groupId if there are groupId associated with this groupList
+	 * @param userId the userId within the program that is trying to be authenticated
+	 * @param groupList
+	 * @return
+	 */
+	public static boolean checkAuthentication(DB dbs, String userId, String[] groupList) {
+		DBCollection new_user = dbs.getCollection("User_Group");
+		for (String group: groupList) {
+			if (group.startsWith("group:")) {
+				group.substring(6); // should be correct?
+				new_user.find("?", group.substring(6));
+			} else {
+				if (group.equals(userId))
+					return true;
+			}
+		}
+		return false;
+	}
 }
