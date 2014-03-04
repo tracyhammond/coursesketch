7d94f5a5a43b71bb270747356ae09d71cc12f18d
Matthew Dillard <7395051@gmail.com>
2014-03-04 12:54:44 -0600

    deepgreentest v2
    forgot to save the changes I made

--- CourseSketchRecognitionServer/src/main/deepgreentest.java
+++ CourseSketchRecognitionServer/src/main/deepgreentest.java
@@ -1,19 +1,26 @@
 package main;
 
+import java.util.List;
+
+import org.ladder.core.sketch.IStroke;
 import org.ladder.recognition.constraint.domains.DomainDefinition;
-import org.ladder.recognition.constraint.domains.io.DomainDefinitionInputDOM;
+//import org.ladder.recognition.constraint.domains.io.DomainDefinitionInputDOM;
 
 import edu.tamu.deepGreen.recognition.DeepGreenRecognizer;
 
 public class deepgreentest {
-	public static void main() {
-		String domainFile = "../LadderDomains/domainDescriptions/domains/COA.xml";
+	public static void recognize(DomainDefinition domainDef, List<IStroke> strokes) {
+		//String domainFile = "../LadderDomains/domainDescriptions/domains/COA.xml";
 		
 		try {
-			DomainDefinition domainDef = new DomainDefinitionInputDOM()
-					.readDomainDefinitionFromFile(domainFile);
+			//DomainDefinition domainDef = new DomainDefinitionInputDOM()
+				//	.readDomainDefinitionFromFile(domainFile);
 			DeepGreenRecognizer recognizer = new DeepGreenRecognizer(domainDef);
 			
+			for(IStroke s : strokes)
+				recognizer.addStroke(s);
+			
+			recognizer.recognize();
 		} catch (Exception e) {
 			System.err.print("ERROR: " + e.getMessage());
 		}
