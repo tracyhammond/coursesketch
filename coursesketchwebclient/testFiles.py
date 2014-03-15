dbf3c7a73fc4de69c29b162601bdf805a76f1a90
dtracers <dtracers@gmail.com>
2014-03-14 21:40:21 -0500

    got the testList working again
    To view test files just point course sketch at:
    /testList (then chose the appropriate folder and the correct element)
    It is even stylized!

--- coursesketchwebclient/testFiles.py
+++ coursesketchwebclient/testFiles.py
@@ -18,25 +18,52 @@ import webapp2
 import os
 
 class MainPage(webapp2.RequestHandler):
+	
+	
+	def searchTestFiles(self):
+		fileList = []
+		directory = 'html/test'
+		absPath = os.path.abspath(directory)
+		self.response.write('looking at files in directory ' + directory + ' <br>')
+		counter = 1
+		for r, d, f in os.walk(directory):
+			self.response.write('<div class="row">')
+			self.response.write('<b>' + r + '</b><br><br>')
+			
+			self.response.write('<a href="#hide' + str(counter) + '" class="hide myButton" id="hide' + str(counter) + '">Expand</a>')
+			self.response.write('<a href="#show' + str(counter) + '" class="show myButton" id="show' + str(counter) + '">Collapse</a><div class="list">')
+			self.response.write('<ul>')
+			for files in f:
+				if files.endswith(".html"):
+					fileNameWithPath = os.path.join(r,files);
+					fileName = str(files);
+					self.response.write('<li><a class="testFile" href="' + fileNameWithPath + '" target="_blank">'+ fileName +'</a></li>')
+					#fileList[len(fileList):] = [os.path.join(r,files)]
+			self.response.write('</ul>')
+			self.response.write('</div></div>')
+			counter = counter + 1
 
-    def get(self):
-        fileList = self.searchTestFiles()
-        for file in fileList:
-            self.response.write('<a href="' + file + '">'+ file +'</a><br>')
-        #self.redirect('http://www.percussionstudio.org',permanent=True)
-
-
-        #    template = JINJA_ENVIRONMENT.get_template('index.html')
-        #    self.response.write(template.render())
-
-    def searchTestFiles(self):
-        fileList = []
-        directory = 'html/test'
-        for r,d,f in os.walk(directory):
-            for files in f:
-                if files.endswith(".html"):
-                    fileList[len(fileList):] = [os.path.join(r,files)]
-        return fileList
+		
+	def visitPath(arg, firname, names):
+		self.response.write('<a href="' + fileName + '">'+ fileName +'</a><br>')
+	
+	def get(myOwn):
+		myOwn.response.write('<style type="text/css">')
+		myOwn.response.write('b {margin-bottom:5px;}')
+		myOwn.response.write('.row { vertical-align: top; height:auto !important; background:#eeeeee; display:inline-block; padding:5px; border-style:ridge; border-color:#dddddd; border-width:2px;}')
+		myOwn.response.write(' .list {display:none; } .show {display: none; } .hide:target + .show {display: inline; } ')
+		myOwn.response.write('.hide:target {display: none; } .hide:target ~ .list {display:inline; } @media print { .hide, .show { display: none; } }')
+		myOwn.response.write('.myButton {border-style:ridge; border-color:#dddddd; border-width:2px; border-radius:4px; background-color:#FFFFFF; padding:2px;}')
+		myOwn.response.write('.myButton:hover {background-color:#DDDDDD;}')
+		myOwn.response.write('a {text-decoration:none; color:#000000;} ')
+		myOwn.response.write('li {padding:2px;}')
+		myOwn.response.write('.testFile {text-decoration:none; color:#000000;}')
+		myOwn.response.write('.testFile:hover {color:#ff0000;} ')
+		myOwn.response.write(' </style>')
+		myOwn.response.write('List of test files to use<br>')
+		myOwn.searchTestFiles()
+        #for file in fileList:
+		#	self.response.write('<a href="' + file + '">'+ file +'</a><br>')
 
 app = webapp2.WSGIApplication([('/testList', MainPage)], debug=True)
 
