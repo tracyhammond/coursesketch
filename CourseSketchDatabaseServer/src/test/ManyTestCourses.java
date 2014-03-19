789de05b585ea9ff0ebbb0ae432dfddd08f92ea3
dtracers <dtracers@gmail.com>
2014-03-19 12:17:20 -0500

    CHanged the many test courses to insert a local course into the database!

--- CourseSketchDatabaseServer/src/test/ManyTestCourses.java
+++ CourseSketchDatabaseServer/src/test/ManyTestCourses.java
@@ -2,10 +2,12 @@ package test;
 
 import protobuf.srl.school.School.SrlCourse;
 import protobuf.srl.school.School.SrlPermission;
+import database.DatabaseAccessException;
 import database.institution.Institution;
+import database.user.UserClient;
 
 public class ManyTestCourses {
-	public static void testCourses() {
+	public static void testCourses() throws DatabaseAccessException {
 		String[] name = new String[]{"CourseSketch 101"};
 		String[] descsription = new String[]{"Hi Welcome to CourseSketch, you have automatically been enrolled in this tutorial."
 				+ " To expand the description of a class click the down arrow."};
@@ -19,17 +21,19 @@ public class ManyTestCourses {
 
 			testBuilder.setAccessPermission(permissions.build());
 			System.out.println(testBuilder.toString());
-	
+
 			// testing inserting course
 				System.out.println("INSERTING COURSE");
 				String courseId = Institution.mongoInsertCourse("0aeee914-3411-6e12-8012-50ab6e769496-6eff24dba01bc332", testBuilder.buildPartial());
 				System.out.println("INSERTING COURSE SUCCESSFULT");
 				System.out.println(courseId);
-			//	ManyTestAssignments.testAssignments(courseId);
+				ManyTestAssignments.testAssignments(courseId);
 		}
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws DatabaseAccessException {
+		new Institution(false); // makes the database point locally
+		new UserClient(false); // makes the database point locally
 		testCourses();
 	}
 }
