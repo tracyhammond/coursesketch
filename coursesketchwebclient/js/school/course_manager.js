724c49c84354acfbb82cab621c168ca59b439acf
andrewking3rd <andrewking3@me.com>
2014-03-21 00:02:17 -0500

    Instructor side functions
    Instructor side functions to add new courses (in progress)
    
    
    Former-commit-id: 92bda10e243a039734556806d153bbfd440d4097

--- coursesketchwebclient/js/school/course_manager.js
+++ coursesketchwebclient/js/school/course_manager.js
@@ -181,12 +181,32 @@ function replaceIframe(src) {
 
 function addNewCourse() { // Functionality to allow for adding of courses by instructors
 	
-	var courseEdit = document.getElementById('addNewCourse');
-	courseEdit.innerHTML = '<textarea rows="4" cols="12">Please enter your courses description right here! </textarea>';
+	/**var courseEdit = document.getElementById('addNewCourse');
+	courseEdit.innerHTML = '<textarea rows="4" cols="12">Please enter your courses description right here! </textarea>';**/
+	var courseEdit1 = document.getElementById('addNewCourse_title');
+	courseEdit1.innerHTML = '<textarea rows="1" cols="36">Enter your course&rsquo;s title here! </textarea>';
 
-
-
+	var courseEdit2 = document.getElementById('addNewCourse_desc');
+	courseEdit2.innerHTML = '<textarea rows="6" cols="36">Enter your course&rsquo;s description here! </textarea>';
+	
+	var courseEdit3 = document.getElementById('newCourseForm');
+	courseEdit3.innerHTML = '<input id="newCourseTitle" type="text" name="newCourseTitle" maxlength="100"><br/><input id="newCourseDesc" type="text" name="newCourseDesc" maxlength="1000" style="width: 300px;"><br/><input type="submit" value="Submit">';
+	
 }
+
+function testCourseAdder(){
+								
+	var course_title = document.getElementById("newCourseTitle").value;
+	var course_desc = document.getElementById("newCourseDesc").value;
+	
+	alert(course_title + '<br>' + course_desc);
+	/**
+	return course_title;
+	return course_desc;
+	**/
+													
+	}
+
 
 var courseSelectionManager = new clickSelectionManager();
 var assignmentSelectionManager = new clickSelectionManager();
