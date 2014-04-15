eb13ddcee8272d4c92fbc09265e6ae034a361a44
dtracers <dtracers@gmail.com>
2014-04-15 02:16:51 -0500

    Created a semi server version for jetty

--- jettyBlank/src/jettyMultiConnection/WrapperConnection.java
+++ jettyBlank/src/jettyMultiConnection/WrapperConnection.java
@@ -1,51 +1,50 @@
 package jettyMultiConnection;
 
+import java.nio.ByteBuffer;
+import java.util.concurrent.CountDownLatch;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.StatusCode;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 
 /**
  * Basic Echo Client Socket
  */
-@WebSocket(maxTextMessageSize = 64 * 1024)
+@WebSocket(maxIdleTime = 30 * 60 * 60) // 30 minutes
 public class WrapperConnection {
 
-    private final CountDownLatch closeLatch;
- 
-    @SuppressWarnings("unused")
+	private final CountDownLatch closeLatch;
+
     private Session session;
  
-    public WrapperConnection() {
-        this.closeLatch = new CountDownLatch(1);
+	public WrapperConnection() {
+    	this.closeLatch = new CountDownLatch(1);
     }
- 
-    public boolean awaitClose(int duration, TimeUnit unit) throws InterruptedException {
-        return this.closeLatch.await(duration, unit);
-    }
- 
+
     @OnWebSocketClose
     public void onClose(int statusCode, String reason) {
         System.out.printf("Connection closed: %d - %s%n", statusCode, reason);
         this.session = null;
         this.closeLatch.countDown();
     }
- 
+
     @OnWebSocketConnect
     public void onConnect(Session session) {
         System.out.printf("Got connect: %s%n", session);
         this.session = session;
-        try {
-            Future<Void> fut;
-            fut = session.getRemote().sendStringByFuture("Hello");
-            fut.get(2, TimeUnit.SECONDS);
-            fut = session.getRemote().sendStringByFuture("Thanks for the conversation.");
-            fut.get(2, TimeUnit.SECONDS);
-            session.close(StatusCode.NORMAL, "I'm done");
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
     }
- 
+
     @OnWebSocketMessage
-    public void onMessage(String msg) {
-        System.out.printf("Got msg: %s%n", msg);
+    public void onMessage(Session session, byte[] data, int offset, int length) {
+    	onMessage(ByteBuffer.wrap(data));
     }
+
+    /**
+	 * Accepts messages and sends the request to the correct server and holds minimum client state.
+	 */
+    public void onMessage(ByteBuffer buffer) {
+	}
 }
