3dec15c3660c8020b3c3ce7df268c69d69898946
dtracers <dtracers@gmail.com>
2014-04-17 13:04:45 -0500

    Fixed error

--- jettyBlank/src/jettyMultiConnection/ConnectionWrapper.java
+++ jettyBlank/src/jettyMultiConnection/ConnectionWrapper.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
