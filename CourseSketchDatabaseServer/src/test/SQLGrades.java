5efe172a50885a72474acec4bbedfb8b1012e591
dwhatley19 <dwhatley@tamu.edu>
2014-04-17 13:34:25 -0500

    SQL setter functions *READ DESC*
    Date Setter NOT WORKING YET
    Needs to be commnented
    
    
    Former-commit-id: e58502979a2e54e675bb3c25acf14337595ba6e7

--- CourseSketchDatabaseServer/src/test/SQLGrades.java
+++ CourseSketchDatabaseServer/src/test/SQLGrades.java
@@ -8,9 +8,9 @@ import java.util.ArrayList;
 //import com.mysql.jdbc.Connection;
 
 public class SQLGrades {
-	private Date timeSubmitted = null;
-	private String comment = "";
-	private float grade = 0;
+//	private Date timeSubmitted = null;
+//	private String comment = "";
+//	private float grade = 0;
 	private Connection connG;
 	private Statement gradeStmt;
 	
@@ -23,10 +23,11 @@ public class SQLGrades {
 		} catch(Exception e) { e.printStackTrace(); }
 	}
 	
-	public Date getDate(String experimentId) {
+	public java.sql.Date getDate(String experimentId) {
 		try {
 			String gradeQ = "select * from problem_grades where problem_id='" + experimentId + "'";
 			ResultSet results = gradeStmt.executeQuery(gradeQ);
+			results.next();
 			return results.getDate("date_graded");
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -38,6 +39,7 @@ public class SQLGrades {
 		try {
 			String gradeQ = "select * from problem_grades where problem_id='" + experimentId + "'";
 			ResultSet results = gradeStmt.executeQuery(gradeQ);
+			results.next();
 			return results.getFloat("grade");
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -57,8 +59,39 @@ public class SQLGrades {
 		}
 	}
 	
+	public void setComment(String experimentId, String comment) {
+		try {
+			String gradeQ = "UPDATE problem_grades SET comment='" + comment + "' where problem_id='" + experimentId + "'";
+			gradeStmt.execute(gradeQ);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setGrade(String experimentId, float grade) {
+		try {
+			String gradeQ = "UPDATE problem_grades SET grade=" + grade + " where problem_id='" + experimentId + "'";
+			gradeStmt.execute(gradeQ);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setDate(String experimentId, java.sql.Date date) {
+		try {
+			String gradeQ = "UPDATE problem_grades SET date_graded=" + date + " where problem_id='" + experimentId + "'";
+			gradeStmt.execute(gradeQ);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		SQLGrades g = new SQLGrades();
 		System.out.println(g.getComment("5348558ae4b062975ab9d8d2"));
+		System.out.println(g.getDate("5348558ae4b062975ab9d8d2"));
+		g.setComment("5348558ae4b062975ab9d8d2", "Scooby-Dooby-Doo!!!!! Where Are You!!!");
+		g.setGrade("5348558ae4b062975ab9d8d2", (float)5.343);
+		g.setDate("5348558ae4b062975ab9d8d2", new java.sql.Date(1234567890));
 	}
 }
