7011932243ce9f6cb1c69adde20ca7976dddb020
dwhatley19 <dwhatley@tamu.edu>
2014-04-17 14:03:56 -0500

    SQL settter functions update
    Added comments adn fixed problem with date settter

--- CourseSketchDatabaseServer/src/test/SQLGrades.java
+++ CourseSketchDatabaseServer/src/test/SQLGrades.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 //import com.mysql.jdbc.*;
 //import com.mysql.jdbc.Connection;
 
+// SQL test with getters and setters
 public class SQLGrades {
 //	private Date timeSubmitted = null;
 //	private String comment = "";
@@ -14,20 +15,23 @@ public class SQLGrades {
 	private Connection connG;
 	private Statement gradeStmt;
 	
+	// Constructor
 	public SQLGrades() {
 		try {
+			// Create Query and Statement
+			// use in all getters/setters
 			connG = DriverManager.getConnection("jdbc:mysql://srl03.tamu.edu/grades?" +
 			                    "user=srl&password=sketchrec");
 			gradeStmt = connG.createStatement();
-			// course -> assignments -> problems
 		} catch(Exception e) { e.printStackTrace(); }
 	}
 	
-	public java.sql.Date getDate(String experimentId) {
+	public java.sql.Date getParseDate(String experimentId) {
 		try {
-			String gradeQ = "select * from problem_grades where problem_id='" + experimentId + "'";
+			// select the date
+			String gradeQ = "select date_graded from problem_grades where problem_id='" + experimentId + "'";
 			ResultSet results = gradeStmt.executeQuery(gradeQ);
-			results.next();
+			results.next(); // required for non-null value
 			return results.getDate("date_graded");
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -37,9 +41,10 @@ public class SQLGrades {
 	
 	public float getGrade(String experimentId) {
 		try {
-			String gradeQ = "select * from problem_grades where problem_id='" + experimentId + "'";
+			// select the grade
+			String gradeQ = "select grade from problem_grades where problem_id='" + experimentId + "'";
 			ResultSet results = gradeStmt.executeQuery(gradeQ);
-			results.next();
+			results.next(); // required for non-null value
 			return results.getFloat("grade");
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -49,9 +54,10 @@ public class SQLGrades {
 
 	public String getComment(String experimentId) {
 		try {
-			String gradeQ = "select * from problem_grades where problem_id='" + experimentId + "'";
+			// select the comment
+			String gradeQ = "select comment from problem_grades where problem_id='" + experimentId + "'";
 			ResultSet results = gradeStmt.executeQuery(gradeQ);
-			results.next();
+			results.next(); // required for non-null value
 			return results.getString("comment");
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -61,6 +67,7 @@ public class SQLGrades {
 	
 	public void setComment(String experimentId, String comment) {
 		try {
+			// update the comment with given submission id
 			String gradeQ = "UPDATE problem_grades SET comment='" + comment + "' where problem_id='" + experimentId + "'";
 			gradeStmt.execute(gradeQ);
 		} catch(Exception e) {
@@ -79,19 +86,21 @@ public class SQLGrades {
 	
 	public void setDate(String experimentId, java.sql.Date date) {
 		try {
-			String gradeQ = "UPDATE problem_grades SET date_graded=" + date + " where problem_id='" + experimentId + "'";
+			// SINGLE QUOTES!!!!!!!!!
+			String gradeQ = "UPDATE problem_grades SET date_graded='" + date.toString() + "' where problem_id='" + experimentId + "'";
 			gradeStmt.execute(gradeQ);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	// main function for testing..
 	public static void main(String[] args) {
 		SQLGrades g = new SQLGrades();
 		System.out.println(g.getComment("5348558ae4b062975ab9d8d2"));
-		System.out.println(g.getDate("5348558ae4b062975ab9d8d2"));
-		g.setComment("5348558ae4b062975ab9d8d2", "Scooby-Dooby-Doo!!!!! Where Are You!!!");
-		g.setGrade("5348558ae4b062975ab9d8d2", (float)5.343);
-		g.setDate("5348558ae4b062975ab9d8d2", new java.sql.Date(1234567890));
+		System.out.println(g.getParseDate("5348558ae4b062975ab9d8d2"));
+		g.setComment("5348558ae4b062975ab9d8d2", "Sfhgjffgkleddgnkjnkj");
+		g.setGrade("5348558ae4b062975ab9d8d2", (float)5.55);
+		g.setDate("5348558ae4b062975ab9d8d2", new java.sql.Date(1234567890000L));
 	}
 }
