ed6bb665f6509e0d3aa38ca3d0265e208d6fa0e1
dtracers <dtracers@gmail.com>
2014-09-02 14:55:06 -0500

    Moved the jars to a different folder

--- globalJars/copyJars.sh
+++ globalJars/copyJars.sh
@@ -1,5 +1,5 @@
-scp AnswerServer*.jar hammond@srl04.tamu.edu:
-scp DatabaseServer*.jar hammond@srl04.tamu.edu:
-scp LoginServer*.jar hammond@srl02.tamu.edu:
-scp SubmissionServer*.jar hammond@srl02.tamu.edu:
-scp ProxyServer*.jar hammond@srl01.tamu.edu:
+scp serverJars/AnswerServer*.jar hammond@srl04.tamu.edu:
+scp serverJars/DatabaseServer*.jar hammond@srl04.tamu.edu:
+scp serverJars/LoginServer*.jar hammond@srl02.tamu.edu:
+scp serverJars/SubmissionServer*.jar hammond@srl02.tamu.edu:
+scp serverJars/ProxyServer*.jar hammond@srl01.tamu.edu:
