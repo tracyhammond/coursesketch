8e072c0236aa9b4efa7f07d05d0eb2e9d5004fc5
dtracers <dtracers@gmail.com>
2014-09-07 21:10:55 -0500

    Refactored Time manager to convert to check style
    Former-commit-id: dd8aa840345381e50346733e8772d026beedb3e5

--- CourseSketchProjects/jettyBlank/src/main/java/connection/TimeManager.java
+++ CourseSketchProjects/jettyBlank/src/main/java/connection/TimeManager.java
@@ -7,86 +7,167 @@ import org.joda.time.DateTime;
 
 import protobuf.srl.request.Message.Request;
 
-public class TimeManager {
+/**
+ * Manages possible differences in time between the servers.
+ *
+ * The time is calculates in 6 steps:
+ * <ol>
+ * <li>The client asks the server for the true time</li>
+ * <li>The server responds and the client makes an adjustment</li>
+ * <li>The client then uses that adjustment to ask for latency</li>
+ * <li>The server then checks the time sent by the client compares it to its
+ * time and assumes the round trip took the same amount of time. This value is
+ * then sent back to the client</li>
+ * <li>The client receives the latency and uses that plus the original time
+ * difference to calculate the total time difference.</li>
+ * </ol>
+ * @author gigemjt
+ *
+ */
+public final class TimeManager {
 
-	private final static String SEND_TIME_TO_CLIENT_MSG = "1";
-	private final static String CLIENT_REQUEST_LATENCY_MSG = "2";
-	private final static String SEND_LATENCY_TO_CLIENT_MSG = "3";
+    /**
+     * A message representing the state to send to the client.
+     *
+     * Specifically this one is about sending a time message to the client.
+     */
+    private static final String SEND_TIME_TO_CLIENT_MSG = "1";
 
-	private static long latency = 0;
-	private static long timeDifferance = 0;
-	private static long totalTimeDifferance = 0;
-	static ActionListener listen;
+    /**
+     * A message representing the state to send to the client.
+     *
+     * Specifically this one is about the client requesting the latency.
+     */
+    private static final String CLIENT_REQUEST_LATENCY_MSG = "2";
 
-	public static void setTimeEstablishedListener(ActionListener list) {
-		listen = list;
-	}
+    /**
+     * A message representing the state to send to the client.
+     *
+     * Specifically this one is about sending the latency to the client.
+     */
+    private static final String SEND_LATENCY_TO_CLIENT_MSG = "3";
 
-	public static long getSystemTime() {
-		return DateTime.now().getMillis() + totalTimeDifferance;
-	}
 
-	public static Request serverSendTimeToClient() {
-		
-		Request.Builder req = Request.newBuilder();
-		req.setRequestType(Request.MessageType.TIME);
-		req.setMessageTime(getSystemTime());
-		req.setResponseText(SEND_TIME_TO_CLIENT_MSG); // Server sending client 'true' time
-		return req.build();
-	}
+    /**
+     * The initial time difference received from the server.
+     *
+     * This is a transient value and is not thread safe.
+     */
+    private static long timeDifference = 0;
 
-	public static Request clientReciveTimeDiff(Request req) {
-		long startCounter = getSystemTime();
-		System.out.println("Server Recieved Time");
-		timeDifferance = req.getMessageTime() - getSystemTime();
-		System.out.println("server time:"+MilltoDate(req.getMessageTime()));
-		System.out.println("proxy time:"+MilltoDate(DateTime.now().getMillis()));
-		Request.Builder rsp = Request.newBuilder();
-		rsp.setRequestType(Request.MessageType.TIME);
-		rsp.setMessageTime(req.getMessageTime()+(getSystemTime()-startCounter));
-		rsp.setResponseText(CLIENT_REQUEST_LATENCY_MSG);
+    /**
+     * The difference between the master server and the client server.
+     */
+    private static long totalTimeDifference = 0;
 
-		return rsp.build();
-	}
+    /**
+     * A listener that is called every time that latency is received and finished calculating.
+     */
+    private static ActionListener listen;
 
-	public static Request decodeRequest(Request req) {
-		if (req.getResponseText().equals(SEND_TIME_TO_CLIENT_MSG)){ //client
-			return clientReciveTimeDiff(req);
-		} else if (req.getResponseText().equals(CLIENT_REQUEST_LATENCY_MSG)) { //server
-			return serverSendLatencyToClient(req);
-		} else if (req.getResponseText().equals(SEND_LATENCY_TO_CLIENT_MSG)) { //client
-			return clientReciveLatency(req);
-		}
-		return null;
-	}
+    /**
+     * Sets a listener that is called when the final time is established.
+     * @param list The listener is called with an empty ActionEvent
+     */
+    public static void setTimeEstablishedListener(final ActionListener list) {
+        listen = list;
+    }
 
-	public static Request serverSendLatencyToClient(Request req) {
-		long latency = getSystemTime()-req.getMessageTime();
+    /**
+     * @return the adjusted system time based off of data from the master server.
+     */
+    public static long getSystemTime() {
+        return DateTime.now().getMillis() + totalTimeDifference;
+    }
+    /**
+     * @return Creates a request that sends the current time to the client. Request contains 'true' time.
+     */
+    public static Request serverSendTimeToClient() {
 
-		Request.Builder rsp = Request.newBuilder();
+        Request.Builder req = Request.newBuilder();
+        req.setRequestType(Request.MessageType.TIME);
+        req.setMessageTime(getSystemTime());
+        // Server sending client  'true' time
+        req.setResponseText(SEND_TIME_TO_CLIENT_MSG);
+        return req.build();
+    }
 
-		rsp.setRequestType(Request.MessageType.TIME);
-		rsp.setMessageTime(latency/2);
-		rsp.setResponseText(SEND_LATENCY_TO_CLIENT_MSG); 
-		return rsp.build();
-	}
+    /**
+     * Receives the time of the server and calculates the time difference.
+     *
+     * Then creates a response asking the server to calculate the latency of the response.
+     * @param req Contains the master server time.
+     * @return A {@link Request} asking for the latency of the message itself.
+     */
+    private static Request clientReceiveTimeDiff(final Request req) {
+        final long startCounter = getSystemTime();
+        System.out.println("Server Recieved Time");
+        timeDifference = req.getMessageTime() - getSystemTime();
+        System.out.println("server time:" + milltoDate(req.getMessageTime()));
+        System.out.println("proxy time:" + milltoDate(DateTime.now().getMillis()));
+        final Request.Builder rsp = Request.newBuilder();
+        rsp.setRequestType(Request.MessageType.TIME);
+        rsp.setMessageTime(req.getMessageTime() + (getSystemTime() - startCounter));
+        rsp.setResponseText(CLIENT_REQUEST_LATENCY_MSG);
 
-	public static Request clientReciveLatency(Request req) {
-		latency = req.getMessageTime();
-		totalTimeDifferance=timeDifferance+latency;
-		if (listen != null) {
-			listen.actionPerformed(new ActionEvent(req, 0, null));
-		}
-		System.out.println("Proxy Recived Time\nTotal Time Diff:"+totalTimeDifferance);
-		return null;
-	}
+        return rsp.build();
+    }
 
-	@SuppressWarnings("unused")
-	private static long DatetoMill(DateTime dt) {
-		return dt.getMillis();
-	}
+    /**
+     * Receives a request and handles it correctly based on the sent in values.
+     * @param req The input request that is given.
+     * @return A request if there is one to be sent or null if there is no response.
+     */
+    public static Request decodeRequest(final Request req) {
+        // client
+        if (req.getResponseText().equals(SEND_TIME_TO_CLIENT_MSG)) {
+            return clientReceiveTimeDiff(req);
+        // server
+        } else if (req.getResponseText().equals(CLIENT_REQUEST_LATENCY_MSG)) {
+            return serverSendLatencyToClient(req);
+        // client
+        } else if (req.getResponseText().equals(SEND_LATENCY_TO_CLIENT_MSG)) {
+            return clientReceiveLatency(req);
+        }
+        return null;
+    }
 
-	private static DateTime MilltoDate(long mils) {
-		return new DateTime(mils);
-	}
+    /**
+     * @param req Contains the slightly adjusted time sent from the client.
+     * @return Creates a request that contains the computed latency
+     */
+    private static Request serverSendLatencyToClient(final Request req) {
+        final long latency = getSystemTime() - req.getMessageTime();
+
+        Request.Builder rsp = Request.newBuilder();
+
+        rsp.setRequestType(Request.MessageType.TIME);
+        rsp.setMessageTime(latency / 2);
+        rsp.setResponseText(SEND_LATENCY_TO_CLIENT_MSG);
+        return rsp.build();
+    }
+
+    /**
+     * Computes the final time difference given the latency.
+     * @param req Contains the latency sent from the server.
+     * @return null
+     */
+    private static Request clientReceiveLatency(final Request req) {
+        long latency = req.getMessageTime();
+        totalTimeDifference = timeDifference + latency;
+        if (listen != null) {
+            listen.actionPerformed(new ActionEvent(req, 0, null));
+        }
+        System.out.println("Proxy Recived Time\nTotal Time Diff:" + totalTimeDifference);
+        return null;
+    }
+
+    /**
+     * Given milliseconds return a {@link DateTime}.
+     * @param mils milliseconds as a number
+     * @return {@link DateTime}
+     */
+    private static DateTime milltoDate(final long mils) {
+        return new DateTime(mils);
+    }
 }
