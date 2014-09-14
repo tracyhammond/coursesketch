fd99fbde51d181050abb9c49a6e4e1b5010c5701
Devin Tuchsen <devint1@icloud.com>
2014-09-14 17:19:54 -0500

    Checkstyle fixes for connection package in LoginServer

--- CourseSketchProjects/CourseSketchLoginServer/src/main/java/connection/ConnectionState.java
+++ CourseSketchProjects/CourseSketchLoginServer/src/main/java/connection/ConnectionState.java
@@ -1,43 +1,42 @@
 package connection;
 
 public class ConnectionState {
-/***
- * testing first push
- */
-	private String key;
-	private boolean isLoggedIn = false;
-	private boolean isInstructor = false; // flagged true if correct login and is instructor
-	private int previousMessageType = 0;
-	private int loginTries = 0;
-	
-	public ConnectionState(String key) {
-		this.key = key;
-	}
-	
-	public boolean equals(Object obj) {
-		if(!(obj instanceof ConnectionState)) {
-		return false;
-		}
-		return ((ConnectionState)obj).key == this.key;
-	}
-	
-	public int hashCode() {
-		return key.hashCode();
-	}
-	
-	public String getKey() {
-		return key;
-	}
-	
-	public boolean isLoggedIn() {
-		return isLoggedIn;
-	}
 
-	public void addTry() {
-		loginTries++;
-	}
-	
-	public int getTries() {
-		return loginTries;
-	}
+    private String key;
+    private boolean isLoggedIn = false;
+    private boolean isInstructor = false; // flagged true if correct login and
+                                          // is instructor
+    private int previousMessageType = 0;
+    private int loginTries = 0;
+
+    public ConnectionState(final String stateKey) {
+        this.key = stateKey;
+    }
+
+    public final boolean equals(final Object obj) {
+        if (!(obj instanceof ConnectionState)) {
+            return false;
+        }
+        return ((ConnectionState) obj).key == this.key;
+    }
+
+    public final int hashCode() {
+        return key.hashCode();
+    }
+
+    public final String getKey() {
+        return key;
+    }
+
+    public final boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public final void addTry() {
+        loginTries++;
+    }
+
+    public final int getTries() {
+        return loginTries;
+    }
 }
