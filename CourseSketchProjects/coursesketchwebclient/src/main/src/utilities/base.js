f5aa75a66707cbcb2285371f9099ae65d59d3fda
dtracers <dtracers@gmail.com>
2014-09-15 03:04:10 -0500

    Updated Utilities
    These need to have code coverage on them too

--- CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/base.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/base.js
@@ -1,10 +1,11 @@
 /*
- * Ironically check to see if a function that checks to see if objects are undefined is undefined
+ * Ironically check to see if a function that checks to see if objects are
+ * undefined is undefined
  */
 if (typeof isUndefined === 'undefined') {
-	function isUndefined(object) {
-		return typeof object === "undefined";
-	}
+    function isUndefined(object) {
+        return typeof object === "undefined";
+    }
 }
 
 /**
@@ -24,17 +25,16 @@ if (typeof isUndefined === 'undefined') {
  * http://stackoverflow.com/a/8809472/2187510
  */
 if (isUndefined(generateUUID)) {
-	function generateUUID() {
-		var d = new Date().getTime();
-		var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
-				function(c) {
-					var r = (d + Math.random() * 16) % 16 | 0;
-					d = Math.floor(d / 16);
-					return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
-				});
-		return uuid;
-	}
-	;
+    function generateUUID() {
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
+        });
+        return uuid;
+    }
+    ;
 }
 
 /**
@@ -42,10 +42,10 @@ if (isUndefined(generateUUID)) {
  * 1st 1970.
  */
 if (isUndefined(createTimeStamp)) {
-	// Creates a time stamp every time this method is called.
-	function createTimeStamp() {
-		return new Date().getTime();
-	}
+    // Creates a time stamp every time this method is called.
+    function createTimeStamp() {
+        return new Date().getTime();
+    }
 }
 
 /**
@@ -63,20 +63,21 @@ if (isUndefined(createTimeStamp)) {
  * 
  * The scope is the target for the onload
  */
-function addScopedLoadEvent(scope, func) {
-	var oldonload = scope.onload;
-	if (typeof scope.onload != 'function') {
-		scope.onload = func;
-	} else {
-		scope.onload = function() {
-			if (oldonload) {
-				oldonload();
-			}
-			func();
-		};
-	}
+if (isUndefined(addScopedLoadEvent)) {
+    function addScopedLoadEvent(scope, func) {
+        var oldonload = scope.onload;
+        if (typeof scope.onload != 'function') {
+            scope.onload = func;
+        } else {
+            scope.onload = function() {
+                if (oldonload) {
+                    oldonload();
+                }
+                func();
+            };
+        }
+    }
 }
-
 /**
  * *************************************************************
  * 
@@ -93,24 +94,24 @@ function addScopedLoadEvent(scope, func) {
  * returns true if the object is a function.
  */
 if (isUndefined(isFunction)) {
-	function isFunction(object) {
-		return typeof (object) === 'function';
-	}
+    function isFunction(object) {
+        return typeof (object) === 'function';
+    }
 }
 
 /**
  * copies a property from parent scope to child scope
  */
 if (isUndefined(copyParentValues)) {
-	function copyParentValues(scope, propertyName, bindFunction) {
-		var object = this[propertyName];
-		// 
-		if (isFunction(object) && bindFunction) {
-			scope[propertyName] = object.bind(scope);
-		} else {
-			scope[propertyName] = this[propertyName];
-		}
-	}
+    function copyParentValues(scope, propertyName, bindFunction) {
+        var object = this[propertyName];
+        // 
+        if (isFunction(object) && bindFunction) {
+            scope[propertyName] = object.bind(scope);
+        } else {
+            scope[propertyName] = this[propertyName];
+        }
+    }
 }
 
 /**
@@ -123,40 +124,58 @@ if (isUndefined(copyParentValues)) {
  * *************************************************************
  */
 if (isUndefined(make2Digits)) {
-	function make2Digits(num) {
-		return ("0" + Number(num)).slice(-2);
-	}
+    function make2Digits(num) {
+        return ("0" + Number(num)).slice(-2);
+    }
 }
 
 if (isUndefined(getMillitaryFormattedDateTime)) {
-	function getMillitaryFormattedDateTime(dateTime) {
-		var date = make2Digits(dateTime.getMonth() + 1) + "-"
-				+ make2Digits(dateTime.getDate()) + "-"
-				+ dateTime.getFullYear();
-		var time = make2Digits(dateTime.getHours()) + ":"
-				+ make2Digits(dateTime.getMinutes());
-		return date + ' ' + time;
-	}
+    function getMillitaryFormattedDateTime(dateTime) {
+        var date = make2Digits(dateTime.getMonth() + 1) + "-" + make2Digits(dateTime.getDate()) + "-" + dateTime.getFullYear();
+        var time = make2Digits(dateTime.getHours()) + ":" + make2Digits(dateTime.getMinutes());
+        return date + ' ' + time;
+    }
 }
 
 if (isUndefined(getFormattedDateTime)) {
-	function getFormattedDateTime(dateTime) {
-		var date = make2Digits(dateTime.getMonth() + 1) + "-"
-				+ make2Digits(dateTime.getDate()) + "-"
-				+ dateTime.getFullYear();
-		var hours = dateTime.getHours();
-		var timeType = "AM";
-		if (dateTime.getHours() > 12) {
-			hours -= 12;
-		}
-		if (dateTime.getHours() >= 12) {
-			timeType = "PM";
-		}
-		if (dateTime.getHours() == 0) {
-			hours = 12;
-		}
-		var time = make2Digits(hours) + ":"
-				+ make2Digits(dateTime.getMinutes()) + timeType;
-		return date + ' ' + time;
-	}
+    function getFormattedDateTime(dateTime) {
+        var date = make2Digits(dateTime.getMonth() + 1) + "-" + make2Digits(dateTime.getDate()) + "-" + dateTime.getFullYear();
+        var hours = dateTime.getHours();
+        var timeType = "AM";
+        if (dateTime.getHours() > 12) {
+            hours -= 12;
+        }
+        if (dateTime.getHours() >= 12) {
+            timeType = "PM";
+        }
+        if (dateTime.getHours() == 0) {
+            hours = 12;
+        }
+        var time = make2Digits(hours) + ":" + make2Digits(dateTime.getMinutes()) + timeType;
+        return date + ' ' + time;
+    }
+}
+
+if (isUndefined(BaseException)) {
+    /**
+     * @Class Defines the base exception class that can be extended by all other
+     *        exceptions.
+     */
+    var BaseException = {
+        name : "BaseException",
+        /**
+         * The level defines how bad it is. level 5 is the okayest exception
+         * (with 6+ typically being ignored completely) and level 0 is the worst
+         * exception (with <0 being treated as 0).
+         */
+        level : 5,
+        message : "BaseException Thrown.\n Please subclass this to create a better exception.",
+        htmlMessage : "BaseException Thrown<br> Please subclass this to create a better exception.",
+        toString : function() {
+            return this.name + ": " + this.message + (this.specificMessage ? "\n" + this.specificMessage : "");
+        },
+        setMessage : function(messageValue) {
+            this.specificMessage = messageValue;
+        }
+    };
 }
