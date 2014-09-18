f70d9162b7472f7fb27d8ebb0de7c6e02a7bbf95
dtracers <dtracers@gmail.com>
2014-09-18 02:07:53 -0500

    Formatting using auto format
    Former-commit-id: 7132c20c6754ee7114017939f80704e0771b97ef

--- CourseSketchProjects/coursesketchwebclient/src/main/src/sketching/srl/objects/interpretation.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/sketching/srl/objects/interpretation.js
@@ -1,17 +1,18 @@
 /**
- **************************************************************
- *
- *
+ * *************************************************************
+ * 
+ * 
  * SRL_Interpretation SRL_Library
+ * 
  * @author Daniel Tan
  * 
- *
- *
- **************************************************************
+ * 
+ * 
+ * *************************************************************
  */
 
 function SRL_Interpretation(label, confidence, complexity) {
-	this.label = label;
-	this.confidence = confidence;
-	this.complexity = complexity;
+    this.label = label;
+    this.confidence = confidence;
+    this.complexity = complexity;
 }
