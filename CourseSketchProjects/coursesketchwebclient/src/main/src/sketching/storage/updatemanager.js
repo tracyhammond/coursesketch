7dfbd7193b60e545289845c7377be880f67eda6b
dtracers <dtracers@gmail.com>
2014-09-18 14:58:21 -0500

    Moved the sketch commands to a separate file

--- CourseSketchProjects/coursesketchwebclient/src/main/src/sketching/storage/updatemanager.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/sketching/storage/updatemanager.js
@@ -210,12 +210,12 @@ function UpdateManager(inputSketch, onError) {
 
                 // creates and inserts the first marker [update] -> [marker] ->
                 // [unreachable update]
-                var startingMarker = localScope.createMarker(false, PROTOBUF_UTIL.getMarkerClass().MarkerType.SPLIT, ""+splitDifference);
+                var startingMarker = localScope.createMarker(false, PROTOBUF_UTIL.getMarkerClass().MarkerType.SPLIT, "" + splitDifference);
                 updateList.splice(currentUpdateIndex, 0, PROTOBUF_UTIL.createUpdateFromCommands([ startingMarker ]));
 
                 // creates and inserts the second marker [unreachable update
                 // (probably undo or redo)] -> [marker] -> [index out of range]
-                var endingMarker = localScope.createMarker(false, PROTOBUF_UTIL.getMarkerClass().MarkerType.SPLIT, ""+(0 - splitDifference));
+                var endingMarker = localScope.createMarker(false, PROTOBUF_UTIL.getMarkerClass().MarkerType.SPLIT, "" + (0 - splitDifference));
                 updateList.push(PROTOBUF_UTIL.createUpdateFromCommands([ endingMarker ]));
 
                 // reset the information
@@ -416,7 +416,9 @@ function UpdateManager(inputSketch, onError) {
      * 
      * @param list
      *            The list that is will be added to the sketch
-     * @param percentBar The bar that will show these updates.  It is called with how much is left to be completed.
+     * @param percentBar
+     *            The bar that will show these updates. It is called with how
+     *            much is left to be completed.
      */
     this.setUpdateList = function(list, percentBar) {
         initializing = true;
@@ -467,213 +469,4 @@ function UpdateManager(inputSketch, onError) {
         this.addUpdate(update, false);
     };
 
-    /***************************************************************************
-     * METHODS FOR THE REDO AND UNDO ARE BELOW.
-     * 
-     * Each method is a prototype of the command or the update
-     **************************************************************************/
-    (function() {
-        /**
-         * Adds a stroke to this local sketch object.
-         * 
-         * @returns {boolean} true. This will always ask for the sketch to be
-         *          redrawn. TODO: change it so that it knows what sketch it is
-         *          associated with.
-         */
-        PROTOBUF_UTIL.getSrlCommandClass().addRedoMethod(PROTOBUF_UTIL.CommandType.ADD_STROKE, function() {
-            if (!this.decodedData) {
-                var stroke = PROTOBUF_UTIL.decodeProtobuf(this.commandData, parent.ProtoSrlStroke);
-                this.decodedData = SRL_Stroke.createFromProtobuf(stroke);
-            }
-            sketch.addObject(this.decodedData);
-            return true;
-        });
-
-        /**
-         * The undo method associated with adding a stroke to the sketch
-         * 
-         * @returns {boolean} true. This will always ask for the sketch to be
-         *          redrawn. TODO: change it so that it knows what sketch it is
-         *          associated with.
-         */
-        PROTOBUF_UTIL.getSrlCommandClass().addUndoMethod(PROTOBUF_UTIL.CommandType.ADD_STROKE, function() {
-            if (!this.decodedData) {
-                var stroke = PROTOBUF_UTIL.decodeProtobuf(this.commandData, parent.ProtoSrlStroke);
-                this.decodedData = SRL_Stroke.createFromProtobuf(stroke);
-            }
-            sketch.removeSubObjectById(this.decodedData.getId());
-            return true;
-        });
-
-        /**
-         * Adds a shape to this local sketch object.
-         * 
-         * @returns {boolean} false. This will never ask for the sketch to be
-         *          redrawn. TODO: change it so that it knows what sketch it is
-         *          associated with.
-         */
-        PROTOBUF_UTIL.getSrlCommandClass().addRedoMethod(PROTOBUF_UTIL.CommandType.ADD_SHAPE, function() {
-            if (!this.decodedData) {
-                var shape = PROTOBUF_UTIL.decodeProtobuf(this.commandData, parent.ProtoSrlShape);
-                this.decodedData = SRL_Shape.createFromProtobuf(shape);
-            }
-            sketch.addObject(this.decodedData);
-            return false;
-        });
-
-        /**
-         * Undoes adding a shape command which basically means it removes the
-         * shape
-         * 
-         * @returns {boolean} false. This will never ask for the sketch to be
-         *          redrawn. TODO: change it so that it knows what sketch it is
-         *          associated with.
-         */
-        PROTOBUF_UTIL.getSrlCommandClass().addUndoMethod(PROTOBUF_UTIL.CommandType.ADD_SHAPE, function() {
-            if (!this.decodedData) {
-                var shape = PROTOBUF_UTIL.decodeProtobuf(this.commandData, parent.ProtoSrlShape);
-                this.decodedData = SRL_Shape.createFromProtobuf(shape);
-            }
-            sketch.removeSubObjectById(this.decodedData.getId());
-            sketch.addObject(this.decodedData);
-            return false;
-        });
-
-        /**
-         * Removes an object from the sketch.
-         * 
-         * @returns {boolean} true. This will always ask for the sketch to be
-         *          redrawn. TODO: change it so that it knows what sketch it is
-         *          associated with.
-         */
-        PROTOBUF_UTIL.getSrlCommandClass().addRedoMethod(PROTOBUF_UTIL.CommandType.REMOVE_OBJECT, function() {
-            if (!this.decodedData || !isArray(this.decodedData)) {
-                this.decodedData = new Array();
-                var idChain = PROTOBUF_UTIL.decodeProtobuf(this.commandData, parent.IdChain);
-                this.decodedData[0] = idChain;
-            }
-            this.decodedData[1] = sketch.removeSubObjectByIdChain(this.decodedData[0].idChain);
-            return true;
-        });
-
-        /**
-         * Undoes removing an object from the sketch Removes an object from the
-         * sketch.
-         * 
-         * @returns {boolean} true. This will always ask for the sketch to be
-         *          redrawn. TODO: change it so that it knows what sketch it is
-         *          associated with.
-         */
-        PROTOBUF_UTIL.getSrlCommandClass().addUndoMethod(PROTOBUF_UTIL.CommandType.REMOVE_OBJECT, function() {
-            if (!this.decodedData || !isArray(this.decodedData)) {
-                this.decodedData = new Array();
-                var idChain = PROTOBUF_UTIL.decodeProtobuf(this.commandData, parent.IdChain);
-                this.decodedData[0] = idChain;
-            }
-            // sketch.addObject(this.decodedData);
-            // this.decodedData[1];
-            throw new "REMOVE_OBJECT undo not supported";
-            return true;
-        });
-
-        /**
-         * Moves shapes from one shape to another shape.
-         * 
-         * @returns {boolean} false. This will never ask for the sketch to be
-         *          redrawn. TODO: change it so that it knows what sketch it is
-         *          associated with.
-         */
-        PROTOBUF_UTIL.getSrlCommandClass().addRedoMethod(PROTOBUF_UTIL.CommandType.PACKAGE_SHAPE, function() {
-            if (isUndefined(this.decodedData) || (!this.decodedData)) {
-                this.decodedData = PROTOBUF_UTIL.decodeProtobuf(this.commandData, Action.ActionPackageShape);
-            }
-            this.decodedData.redo();
-            return false;
-        });
-
-        /**
-         * Moves shapes from one shape to another shape. But does the opposite
-         * as the redo package shape
-         * 
-         * @returns {boolean} false. This will never ask for the sketch to be
-         *          redrawn. TODO: change it so that it knows what sketch it is
-         *          associated with.
-         */
-        PROTOBUF_UTIL.getSrlCommandClass().addUndoMethod(PROTOBUF_UTIL.CommandType.PACKAGE_SHAPE, function() {
-            if (isUndefined(this.decodedData) || (!this.decodedData)) {
-                this.decodedData = PROTOBUF_UTIL.decodeProtobuf(this.commandData, Action.ActionPackageShape);
-            }
-            this.decodedData.undo();
-            return false;
-        });
-
-        /***********************************************************************
-         * MARKER SPECIFIC UPDATES.
-         **********************************************************************/
-
-        /**
-         * @returns the human readable name of the given marker type
-         */
-
-        PROTOBUF_UTIL.getMarkerClass().prototype.getCommandTypeName = function() {
-            switch (this.getType()) {
-                case this.MarkerType.SUBMISSION:
-                    return 'SUBMISSION';
-                case PROTOBUF_UTIL.CommandType.FEEDBACK:
-                    return 'FEEDBACK';
-                case PROTOBUF_UTIL.CommandType.SAVE:
-                    return 'SAVE';
-                case PROTOBUF_UTIL.CommandType.SPLIT:
-                    return 'SPLIT';
-                case PROTOBUF_UTIL.CommandType.CLEAR:
-                    return 'CLEAR';
-            }
-            return "NO_NAME # is: " + this.getCommandType();
-        };
-
-        /***********************************************************************
-         * Specific commands and their actions.
-         **********************************************************************/
-
-        /**
-         * Moves the shapes from the old container to the new container.
-         */
-        PROTOBUF_UTIL.getActionPackageShapeClass().prototype.redo = function() {
-            var oldContainingObject = !(this.oldContainerId) ? sketch : sketch.getSubObjectByIdChain(this.oldContainerId.getIdChain());
-            var newContainingObject = !(this.newContainerId) ? sketch : sketch.getSubObjectByIdChain(this.newContainerId.getIdChain());
-
-            if (oldContainingObject == newContainingObject)
-            // done moving to same place.
-            return;
-            for (var shapeIndex = 0; shapeIndex < this.shapesToBeContained.length; shapeIndex++) {
-                var shapeId = this.shapesToBeContained[shapeIndex];
-                var object = oldContainingObject.removeSubObjectById(shapeId);
-                newContainingObject.addSubObject(object);
-            }
-        };
-
-        /**
-         * Moves the shapes from the new container to the old container.
-         * 
-         * This is a reverse of the process used in redo.
-         */
-        PROTOBUF_UTIL.getActionPackageShapeClass().prototype.undo = function() {
-            var oldContainingObject = !(this.newContainerId) ? sketch : sketch.getSubObjectByIdChain(this.newContainerId.getIdChain());
-            var newContainingObject = !(this.oldContainerId) ? sketch : sketch.getSubObjectByIdChain(this.oldContainerId.getIdChain());
-
-            if (oldContainingObject == newContainingObject)
-            // done moving to same place.
-            return;
-
-            for (shapeId in this.shapesToBeContained) {
-                var object = oldContainingObject.removeSubObjectById(shapeId);
-                if (newContainerId) {
-                    newContainingObject.addSubObject(object);
-                } else {
-                    newContainingObject.addObject(object);
-                }
-            }
-        };
-
-    });// (PROTOBUF_UTIL.SrlUpdate, PROTOBUF_UTIL.SrlCommand, PROTOBUF_UTIL);
 }
