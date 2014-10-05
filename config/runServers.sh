45e6d150a7359f58a57f4da3b716a8b47baf13bd
dtracers <dtracers@gmail.com>
2014-10-05 12:12:02 -0500

    Added a way to run all servers and kill all servers

---
+++ config/runServers.sh
@@ -0,0 +1,20 @@
+screenName=('database' 'login' 'submission' 'answer' 'recognition' 'proxy')
+serverName=('Database' 'Login' 'Submission' 'AnswerChecker' 'Recognition' 'Proxy')
+cd ../CourseSketchProjects
+length=$(expr ${#screenName[@]} - 1)
+echo $length
+for (( i=0; i<=$length; i++ ))
+do
+	dir="CourseSketch${serverName[$i]}Server"
+	echo $dir
+	result=($(find $dir -name '*-run.jar') )
+	if [ -n "$result" ]; then
+		fullPath="$result"
+		echo $fullPath
+		command1="java -jar $fullPath"
+		screen -d -m -S "${screenName[$i]}" bash -c "$command1"
+		echo "attach: screen -r ${screenName[$i]}"
+	fi
+done
+
+echo "To leave a running screen: ctrl-a d"
