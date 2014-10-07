43be7d208717fed1888f72a4b1a2062561a7ed07
dtracers <dtracers@gmail.com>
2014-10-07 00:55:30 -0500

    test utilities for testing fake pages
    Mocked the database and the connection and the listener in hope that
    testing full webpages will be much easier.

---
+++ CourseSketchProjects/coursesketchwebclient/src/test/src/testUtilities/mockedObjects/mockedConnection.js
@@ -0,0 +1,6 @@
+(function() {
+    var connection = new Connection("http://localHost:8080", false, false);
+    connection.userId = "fakseUser1";
+    CourseSketch.connection = sinon.stub(connection);
+    CourseSketch.connectionPure = connection; // in case you need to replace the mock.
+})();
---
+++ CourseSketchProjects/coursesketchwebclient/src/test/src/testUtilities/mockedObjects/mockedIndex.js
@@ -0,0 +1,26 @@
+var CourseSketch = {}; // notice that this is not read only!
+if (typeof parent == "undefined" || parent == null) {
+    parent = {};
+}
+
+parent.CourseSketch = CourseSketch;
+
+CourseSketch.connection = false;
+CourseSketch.redirector = {};
+CourseSketch
+
+CourseSketch.reloadContent = function() {
+    throw "This function must be mocked to be called";
+};
+
+CourseSketch.redirectContent = function() {
+    throw "This function must be mocked to be called";
+};
+
+CourseSketch.connection = {};
+
+// notice that I defined it and made it not read only
+CourseSketch.PROTOBUF_UTIL = {};
+
+CourseSketch.dataListener = {};
+CourseSketch.dataManager = {};
