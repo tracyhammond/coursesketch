1a19456a1ef62eef8c2b43d1d350cf196b74bac6
dtracers <dtracers@gmail.com>
2014-10-08 00:19:00 -0500

    Now the entirety of the school item is created
    in its most simplistic form which is good enough for now.
    
    Now I need to work on replacing the school builder with these new sets
    of values.  Which should be like half as large
    
    
    Former-commit-id: 639f4c53613b4b743ff31bc0802d13f879ba4bf8

--- CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/templates/schoolItem/schoolItem.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/templates/schoolItem/schoolItem.js
@@ -1,6 +1,7 @@
 function SchoolItem() {
 
     var shadowRoot = undefined;
+    var isOverflow = false;
 
     /**
      * @param document
@@ -14,10 +15,123 @@ function SchoolItem() {
     this.initializeElement = function(document, templateClone) {
         shadowRoot = this.createShadowRoot();
         shadowRoot.appendChild(templateClone);
+        this.checkOverflow();
+        this.buildExpandEvent();
+        this.setUpEditButtons();
     };
 
-    function setOverflow() {
-        
+    /**
+     * Checks for overflow in an element.
+     */
+    this.checkOverflow = function() {
+        var descriptionHolder = shadowRoot.querySelector('.description');
+        var descriptionContent = shadowRoot.querySelector('.description content');
+
+        var nodes = descriptionContent.getDistributedNodes();
+        var element = nodes[0];
+        var text = element.textContent;
+
+        var widths = [ descriptionHolder.clientWidth, descriptionHolder.offsetWidth, descriptionHolder.scrollWidth, this.style.width,
+                this.style.maxwidth ];
+        var usedWidth = $(window).width();
+        for (var i = 0; i < widths.length; i++) {
+            var width = widths[i];
+            if (typeof width == "string") {
+                if (width.endsWith("px")) {
+                    width = width.substring(0, width.indexOf("px"));
+                    width = parseInt(width);
+                } else {
+                    break;
+                }
+            }
+            if (width && width < usedWidth) {
+                usedWidth = width;
+            }
+        }
+        var padding = 20; // left right padding is 10
+        isOverflow = checkTextOverflow(usedWidth - padding, text, $(element).css('font'));
+        return isOverflow;
+    };
+
+    /**
+     * Given an allowed width, text and font it returns true if the element is
+     * over 3 lines long.
+     */
+    function checkTextOverflow(widthAllowed, text, font) {
+        var totalWidth = getTextWidth(text, font);
+        var lines = totalWidth / widthAllowed;
+        var numberOfLines = Math.round(lines);
+        if (numberOfLines > 3) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Builds the event for what happens when expanding content.
+     */
+    this.buildExpandEvent = function() {
+        var paragraph = shadowRoot.querySelector('.description p');
+        if (isOverflow) {
+            $(paragraph).addClass("overflow");
+        }
+
+        var descriptionContent = shadowRoot.querySelector('.description content');
+
+        var nodes = descriptionContent.getDistributedNodes();
+        var contentElement = nodes[0];
+
+        var button = shadowRoot.querySelector('.description p + p');
+        var expanded = true;
+        $(button).click(function() {
+            if ($(button).hasClass('expand')) {
+                $(button).removeClass('expand');
+                $(button).addClass('contract');
+                $(contentElement).addClass('expandedContent');
+                $(paragraph).addClass('expandedContent');
+            } else {
+                $(button).removeClass('contract');
+                $(button).addClass('expand');
+                $(contentElement).removeClass('expandedContent');
+                $(paragraph).removeClass('expandedContent');
+            }
+        });
+    };
+
+    /**
+     * Sets up what happens when an edit button is clicked.
+     */
+    this.setUpEditButtons = function() {
+        if (isUndefined(this.dataset.instructor)) {
+            return;
+        }
+
+        var editingClass = 'currentlyEditing';
+        [].forEach.call(shadowRoot.querySelectorAll('.editButton'), function(element) {
+            var parentNode = element.parentNode;
+            var content = parentNode.querySelector('content');
+            var nodes = content.getDistributedNodes();
+            var contentElement = nodes[0];
+            var editorElement = getEditorElement(parentNode);
+            $(element).click(function() {
+                if ($(parentNode).hasClass(editingClass)) {
+                    $(parentNode).removeClass(editingClass);
+                    $(contentElement).removeClass(editingClass);
+                    parentNode.removeChild(editorElement);
+                    contentElement.textContent = editorElement.value;
+                } else {
+                    $(parentNode).addClass(editingClass);
+                    // makes the display = none
+                    $(contentElement).addClass(editingClass);
+                    editorElement.value = contentElement.textContent;
+                    parentNode.insertBefore(editorElement, element);
+                }
+            });
+        });
+    };
+
+    function getEditorElement(parentNode) {
+        return document.createElement('input');
     }
 }
 
