08ab5276c65d2f805585a442def32a375219615d
dtracers <dtracers@gmail.com>
2014-10-09 11:11:04 -0500

    moved the click selection manager into its own file
    Former-commit-id: f52c73ef4483b53f6ccc20ad6d7fb1299a461345

--- CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/templates/schoolItem/schoolItemBuilder.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/templates/schoolItem/schoolItemBuilder.js
@@ -58,7 +58,7 @@ function SchoolItemBuilder() {
 	}
 
 	this.centerItem = this.setCenterInDiv;
-	
+
 	/********
 	 * LOCAL VARIABLES
 	 ******/
@@ -478,40 +478,3 @@ function SchoolItemBuilder() {
 	};
 }
 
-function clickSelectionManager() {
-	this.selectedItems = [];
-	this.selectionClassName = ' selected_box';
-
-	this.addSelectedItem = function(id) {
-		this.selectedItems.push(id);
-		this.selectItem(id);
-	};
-
-	this.selectItem = function(id) {
-		document.getElementById(id).className+=this.selectionClassName;
-	};
-
-	this.clearItem = function(id) {
-		var toSelect = document.getElementById(id);
-		if(!toSelect) {
-			return;
-		}	
-		var className = toSelect.className;
-		var index = className.indexOf(this.selectionClassName);
-		var firstHalf = className.substring(0,index);
-		var secondHalf = className.substring(index + this.selectionClassName.length);
-		toSelect.className = firstHalf + secondHalf;
-	};
-
-	this.clearAllSelectedItems = function() {
-		for(var i = 0; i < this.selectedItems.length; i++) {
-			this.clearItem(this.selectedItems[i]);
-		}
-		// Clears array.
-		this.selectedItems = [];
-	};
-
-	this.isItemSelected = function(id) {
-		return this.selectedItems.indexOf(id) > -1;
-	};
-}
