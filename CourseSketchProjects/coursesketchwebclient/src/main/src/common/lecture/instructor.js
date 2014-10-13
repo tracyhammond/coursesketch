48b908b7bdc8551bb888f7f95d61c108459bc01f
devint1 <devint1@icloud.com>
2014-10-13 16:20:50 -0500

    Code clean
    Former-commit-id: f5794ef744c17a64068de3974a9e04e8942fd57e

--- CourseSketchProjects/coursesketchwebclient/src/main/src/common/lecture/instructor.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/common/lecture/instructor.js
@@ -1,21 +1,24 @@
- 
 var foo = [
-           {
-           "title":"this is a title",
-           "summary":"this is a summary"
-           }
-           ];
+	{
+		"title":"this is a title",
+		"summary":"this is a summary"
+	}
+];
+var courseSelected = function(evt) {
+	var item = $(evt.currentTarget);
+	var title = item[0].children[0];
+	$("#placeholder").text($(title).text());
+	$.each($(".list-item"), function() {
+		if($(this) != item)
+			$(this).removeClass("selected");
+	});
+	$(item).addClass("selected");
+}
+
 $(document).ready(function() {
-                  $.each($(".list-item"), function() {
-                         var children = $(this).children();
-                         if(typeof children[0] != "undefined")
-                         $(this).bind("click", function() {
-                                      $("#placeholder").text($(children[0]).text());
-                                      $.each($(".list-item"), function() {
-                                             $(this).removeClass("selected");
-                                             });
-                                      $(this).addClass("selected");
-                                      });
-                         });
-                  })
- 
+	$.each($(".list-item"), function() {
+		var children = $(this).children();
+		if(typeof children[0] != "undefined")
+		$(this).bind("click", children, courseSelected);
+	});
+})
