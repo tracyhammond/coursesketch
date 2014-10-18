fcd795bd2a918a44a559095b7973787b1afc56cf
dtracers <dtracers@gmail.com>
2014-10-18 01:45:20 -0500

    You can now run servers using this method.
    Kills all java programs though
    
    
    Former-commit-id: 7657d52be9bd39629c1cae7ba7863826b7b138ef

--- config/killServers.sh
+++ config/killServers.sh
@@ -1,16 +1,17 @@
-screenName=('database' 'login' 'submission' 'answer' 'recognition' 'proxy')
-serverName=('Database' 'Login' 'Submission' 'AnswerChecker' 'Recognition' 'Proxy')
+screenName=('database' 'login' 'submission' 'answer' 'recognition' 'proxy' 'mongo')
 cd ../CourseSketchProjects
 length=$(expr ${#screenName[@]} - 1)
 echo $length
 for (( i=0; i<=$length; i++ ))
 do
-	dir="CourseSketch${serverName[$i]}Server"
-	result=($(find $dir -name '*-run.jar') )
-	if [ -n "$result" ]; then
-		fullPath="$result"
-		command1="java -jar $fullPath"
-		screen -X -S "${screenName[$i]}" quit
-		echo "killing screen ${screenName[$i]}"
-	fi
+	#looks to see if there are multiple screens with the same name
+	for session in $(screen -ls | grep -o "[0-9]*\.${screenName[$i]}")
+	do
+		screen -X -S "$session" quit
+		echo "killing screen $session"
+	done
 done
+
+echo 'sorry java you are going bye bye'
+killall -9 java
+#screen -S test -p 0 -X stuff 'command\n'
