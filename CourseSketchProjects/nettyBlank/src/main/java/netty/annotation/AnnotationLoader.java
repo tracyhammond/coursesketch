5cbe15698ca2f36294b75e3f36dd77932d14c8a0
dtracers <dtracers@gmail.com>
2014-10-19 17:14:00 -0500

    implements abstract class
    Former-commit-id: e0d449101c99ad86423cff06176ceaa0b0e995e4

---
+++ CourseSketchProjects/nettyBlank/src/main/java/netty/annotation/AnnotationLoader.java
@@ -0,0 +1,24 @@
+package netty.annotation;
+
+import org.reflections.Reflections;
+
+import java.util.Set;
+
+/**
+ * Created by gigemjt on 10/19/14.
+ */
+public class AnnotationLoader {
+    public static void main(final String aargs[]) {
+        final Reflections reflections =
+                isProduction() ? Reflections.collect() : new Reflections("");
+
+        final Set<Class<?>> annotated =
+                reflections.getTypesAnnotatedWith(NettyWebSocket.class);
+
+        System.out.println(annotated);
+    }
+
+    public static boolean isProduction() {
+        return false;
+    }
+}
