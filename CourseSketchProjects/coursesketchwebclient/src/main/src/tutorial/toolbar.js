a634685af02dbe327de6f045a8dc877a2e0c71e1
mattrunyon <mattrunyon@verizon.net>
2014-10-20 17:05:49 -0500

    Toolbar for Tutorial Creation
    This makes the toolbar for tutorial creation into a custom element.
    Import "toolbarInclude" in the webpage and add
    <toolbar-creation></toolbar-creation> to add the tutorial toolbar
    anywhere. It is currently a table with buttons.

---
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/tutorial/toolbar.js
@@ -0,0 +1,10 @@
+function Toolbar() {
+    this.initializeElement = function(templateClone) {
+        var localScope = this;
+        shadowRoot = this.createShadowRoot();
+        shadowRoot.appendChild(templateClone);
+    };
+}
+
+Toolbar.prototype = Object.create(HTMLElement.prototype);
+
