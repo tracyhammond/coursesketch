be4b4fbdd7bba785b450285a37204ef19f7db499
AssumedBread52 <asanchez16756@gmail.com>
2014-10-29 16:42:16 -0500

    Attempt at lectureDataManager and proto done
    Former-commit-id: ea3fa68affc747b40924e75ce595e5a62c8263c4

--- CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/persistantData/specificManager/lectureDataManager.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/persistantData/specificManager/lectureDataManager.js
@@ -4,4 +4,90 @@ function LectureDataManager(parent, advanceDataListener, parentDatabase, sendDat
 	var Request = request
 	var localScope = parent;
 	var ByteBuffer = buffer;
+	
+	function setLecture (lecture, lectureCallback) {
+		database.putInLecture(lecture.id, lecture.toBase64(), function (e, request) {
+			if (lectureCallback) {
+				lectureCallback (e, request);
+			}
+		});
+	};
+	parent.setLecture = setLecture;
+	
+	function deleteLecture (lectureId, lectureCallback) {
+		database.deleteFromLectures (lectureId, function (e, request) {
+			if (lectureCallback) {
+				lectureCallback (e, request);
+			}
+		});
+	};
+	parent.deleteLecture = deleteLecture;
+	
+	function getLectureLocal (lectureId, lectureCallback) {
+		database.getFromLectures(lectureId, function (e, request, result) {
+			if (isUndefined(result) || isUndefined(result.data)) {
+				lectureCallback (undefined);
+			} else if (result.data == nonExistantValue) {
+				lectureCallback (nonExistantValue);
+			} else {
+				var bytes = ByteBuffer.fromBase64(result.data);
+				lectureCallback(CourseSketch.PROTOBUF_UTIL.getSrlLectureClass().decode(bytes));
+			}
+		});
+	}
+	parent.getLecture = getLecture;
+	
+	function getCourseLecture (lectureId, lectureCallback) {
+		getLectures([lectureId], function (lectureList) {
+			lectureCallback (lectureList[0]);
+		});
+	};
+	parent.getCourseLecture = getCourseLecture;
+	
+	function getCourseLectures (lectureIds, lectureCallback) {
+		if (isUndefined (lectureIds) || lectureIds == null || lectureIds.length == 0) {
+			lectureCallback (nonExistantValue);
+		}
+		var barrier = lectureIds.length;
+		var lecturesFound = [];
+		var lectureIdsNotFound = [];
+		for (var i = 0; i < lectureIds.length; i++) {
+			var currentLectureId = lectureIds[i];
+			function forLoopBlock (lectureId) {
+				getLectureLocal (lectureId, function (lecture) {
+					if (!isUndefined (lecture)) {
+						lecturesFound.push(lecture);
+					} else {
+						lectureIdsNotFound.push(lecture);
+					}
+					barrier -= 1;
+					if (barrier == 0) {
+						if (lectureIdsNotFound.length >= 1) {
+							advanceDataListener.setListener(Request.MessageType.DATA_REQUEST, CourseSketch.PROTOBUF_UTIL.ItemQuery.LECTURE, function (evt, item) {
+								var school = CourseSketch.PROTOBUF_UTIL.getSrlSchoolClass().decode(item.data);
+								var lecture = school.lectures[0];
+								if (isUndefined (lecture)) {
+									lectureCallback (lecture);
+									advanceDataListener.removeListener(Request.MessageType.DATA_REQUEST, CourseSketch.PROTOBUF_UTIL.ItemQuery.LECTURE);
+									return;
+								}
+								for (var i = 0; i < school.lectures.length; i++) {
+									localScope.setLecture(school.lectures[i]);
+									lecturesFound.push(school.lectures[i]);
+								}
+								lectureCallback (lecturesFound);
+								advanceDataListener.removeListener(Request.MessageType.DATA_REQUEST, CourseSketch.PROTOBUF_UTIL.ItemQuery.LECTURE);
+							});
+							sendData.sendDataRequest (CourseSketch.PROTOBUF_UTIL.ItemQuery.COURSE_PROBLEM, lectureIdsNotFound);
+						}
+						if (lectureList.length > 0) {
+							lectureCallback (lectureList);
+						}
+					}
+				});
+			}
+			forLoopBlock (currentLectureId);
+		}
+	};
+	parent.getCourseLectures = getCourseLectures;
 }
