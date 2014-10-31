1322896242b40bbbd2956794d2a5b4e9e65e51ee
dtracers <dtracers@gmail.com>
2014-10-31 12:28:56 -0500

    Fixed issues merge

--- CourseSketchProjects/jettyBlank/src/main/java/database/DatabaseStringConstants.java
+++ CourseSketchProjects/jettyBlank/src/main/java/database/DatabaseStringConstants.java
@@ -31,7 +31,9 @@ public final class DatabaseStringConstants {
     public static final String STATE_COLLECTION = "UserStates";
     public static final String GRADE_COLLECTION = "problem_submissions";
     public static final String UPDATE_COLLECTION = "User_Updates";
+    public static final String LOGIN_COLLECTION = "CourseSketchUsers";
     public static final String DATABASE = "institution";
+    public static final String LOGIN_DATABASE = "login";
 
     // meta fields
     public static final String NAME = "Name";
@@ -113,11 +115,19 @@ public final class DatabaseStringConstants {
     public static final String UPDATEID = "UpdateId";
     public static final String TIME = "Time";
 
+    // login specific
+    public static final String PASSWORD = "Password";
+    public static final String IS_DEFAULT_INSTRUCTOR = "IsInstructor";
+    public static final String INSTRUCTOR_ID = "InstructorId";
+    public static final String STUDENT_ID = "StudentId";
+    public static final String STUDENT_CLIENT_ID = "StudentClientId";
+    public static final String INSTRUCTOR_CLIENT_ID = "InstructorClientId";
+
     // user group specific
     public static final String USER_LIST = "UserList";
     public static final String GROUP_PREFIX = "group";
     public static final int GROUP_PREFIX_LENGTH = GROUP_PREFIX.length();
 
-	// mongo comands
-	public static final String SET_COMMAND = "$set";
+    // mongo comands
+    public static final String SET_COMMAND = "$set";
 }
