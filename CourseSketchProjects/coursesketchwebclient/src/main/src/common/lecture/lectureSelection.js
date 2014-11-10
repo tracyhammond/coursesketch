bd9aa79c575b7c6e608904ac7eba2407afa3d6eb
devint1 <devint1@icloud.com>
2014-11-10 17:33:25 -0600

    Add tests for lectureSelection and some changes from tests
    Former-commit-id: b6220b7e0c9414b4faeb94c31b64ed58c869462a

--- CourseSketchProjects/coursesketchwebclient/src/main/src/common/lecture/lectureSelection.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/common/lecture/lectureSelection.js
@@ -1,70 +1,67 @@
 (function() {
-	CourseSketch.lectureSelection.courseSelectionManager = undefined,
-	CourseSketch.lectureSelection.currentCourse = undefined,
-
-	/**
-	 * Renders a list of lectures to the screen.
-	 *
-	 * @param lectureList list of lectures to display
-	 */
-	CourseSketch.lectureSelection.displayLectures = function(lectureList) {
-		$("#placeholder").css({
-			display: "none"
-		});
-		var add = $("#add").clone();
-		var schoolItemBuilder = new SchoolItemBuilder();
-		schoolItemBuilder.setList(lectureList).setShowDate(false).build(document.querySelector("#col2>.content"));
-		$("#col2>.content").prepend(add);
-		$("#add").bind("click", CourseSketch.lectureSelection.addLecture);
-		$("#add").css({
-			display: "inline-block"
-		});
-	},
-
-	/**
-	 * Called when a course is selected. Updates selection and gets lectures for the course.
-	 *
-	 * @param course course object of the selected element
-	 */
-	CourseSketch.lectureSelection.courseSelected = function(course) {
-		var courseid = course.id;
-		this.currentCourse = course.id;
-		CourseSketch.dataManager.getCourseLectures(course.lectureList, CourseSketch.lectureSelection.displayLectures);
-		CourseSketch.lectureSelection.courseSelectionManager.clearAllSelectedItems();
-		CourseSketch.lectureSelection.courseSelectionManager.addSelectedItem(document.getElementById(courseid));
-		CourseSketch.dataManager.getCourse(courseid, function(course) {
-			CourseSketch.dataManager.getCourseLectures(course.lectureList, CourseSketch.lectureSelection.displayLectures);
-		});
-	},
-
-	/**
-	 * Adds a new lecture to the currently selected course.
-	 *
-	 * @param evt event from click (or other) action
-	 */
-	CourseSketch.lectureSelection.addLecture = function(evt) {
-		$("#col2>.content").append("<span class=\"lecture\"><div class=\"title\">TITLE</div><div class=\"summary\">Untitled Lecture</div></span>");
-	    var lecture=CourseSketch.PROTOBUF_UTIL.Lecture();
-	    lecture.courseId = currentCourse;
-	    lecture.title = "Untitled Lecture";
-	    lecture.id = generateUUID();
-	    lecture.description = "N/A";
-	    CourseSketch.dataManager.insertLecture(lecture );
-	    console.log("finished adding to course "+ currentCourse);
-	    
-	},
-
-	/**
-	 * Renders a list of courses to the screen.
-	 *
-	 * @param courseList list of courses to display
-	 */
-	CourseSketch.lectureSelection.showCourses = function(courseList) {
-		var schoolItemBuilder = new SchoolItemBuilder();
-		schoolItemBuilder.setList(courseList).setShowDate(false).setBoxClickFunction(this.courseSelected).build(document.querySelector("#col1>.content"));
-	}
-
 	$(document).ready(function() {
+		CourseSketch.lectureSelection.courseSelectionManager = undefined,
+		CourseSketch.lectureSelection.currentCourse = undefined,
+
+		/**
+		 * Renders a list of lectures to the screen.
+		 *
+		 * @param lectureList list of lectures to display
+		 */
+		CourseSketch.lectureSelection.displayLectures = function(lectureList) {
+			$("#placeholder").css({
+				display: "none"
+			});
+			var add = $("#add").clone();
+			var schoolItemBuilder = new SchoolItemBuilder();
+			schoolItemBuilder.setList(lectureList).setShowDate(false).build(document.querySelector("#col2>.content"));
+			$("#col2>.content").prepend(add);
+			$("#add").bind("click", CourseSketch.lectureSelection.addLecture);
+			$("#add").css({
+				display: "inline-block"
+			});
+		},
+
+		/**
+		 * Called when a course is selected. Updates selection and gets lectures for the course.
+		 *
+		 * @param course course object of the selected element
+		 */
+		CourseSketch.lectureSelection.courseSelected = function(course) {
+			var courseid = course.id;
+			this.currentCourse = course.id;
+			CourseSketch.dataManager.getCourseLectures(course.lectureList, CourseSketch.lectureSelection.displayLectures);
+			CourseSketch.lectureSelection.courseSelectionManager.clearAllSelectedItems();
+			CourseSketch.lectureSelection.courseSelectionManager.addSelectedItem(document.getElementById(courseid));
+		},
+
+		/**
+		 * Adds a new lecture to the currently selected course.
+		 *
+		 * @param evt event from click (or other) action
+		 */
+		CourseSketch.lectureSelection.addLecture = function(evt) {
+			$("#col2>.content").append("<span class=\"lecture\"><div class=\"title\">TITLE</div><div class=\"summary\">Untitled Lecture</div></span>");
+		    var lecture=CourseSketch.PROTOBUF_UTIL.Lecture();
+		    lecture.courseId = currentCourse;
+		    lecture.title = "Untitled Lecture";
+		    lecture.id = generateUUID();
+		    lecture.description = "N/A";
+		    CourseSketch.dataManager.insertLecture(lecture );
+		    console.log("finished adding to course "+ currentCourse);
+		    
+		},
+
+		/**
+		 * Renders a list of courses to the screen.
+		 *
+		 * @param courseList list of courses to display
+		 */
+		CourseSketch.lectureSelection.showCourses = function(courseList) {
+			CourseSketch.lectureSelection.schoolItemBuilder = new SchoolItemBuilder();
+			CourseSketch.lectureSelection.schoolItemBuilder.setList(courseList).setShowDate(false).setBoxClickFunction(this.courseSelected).build(document.querySelector("#col1>.content"));
+		}
+	
 		CourseSketch.lectureSelection.courseSelectionManager = new clickSelectionManager();
 		var loadCourses = function(courseList) {
 		    /* (waitingIcon.isRunning()) {
