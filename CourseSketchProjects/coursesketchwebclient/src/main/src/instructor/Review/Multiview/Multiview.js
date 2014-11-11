5612dccf094d7291990e6222c9f815dba1920ae1
joshuazodda18 <chrome@neo.tamu.edu>
2014-11-11 14:32:32 -0600

    worknig template
    Former-commit-id: 81650117d40057d64791fdcff7bddbc1e0a3e7b0

--- CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/Review/Multiview/Multiview.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/Review/Multiview/Multiview.js
@@ -4,28 +4,32 @@ function Mvsketch(){
 
 
 
-this.initializeElement = function(templateClone){
-    shadowRoot = this.createShadowRoot();
-    shadowRoot.appendChild(templateClone);
+    this.initializeElement = function(templateClone){
+        shadowRoot = this.createShadowRoot();
+        shadowRoot.appendChild(templateClone);
 
-}
-function correct(){
-    gradevalue = 'correct';
-    var changing = document.getElementById("outer");
-    changing.className="outercorrect";
-}
-function wrong(){
-    gradevalue = 'wrong';
-    var changing = document.getElementById("outer");
-    changing.className="outerwrong";
-}
-function redo(){
-    gradevalue = 'redo';
-    var changing = document.getElementById("outer");
-    changing.className="outerredo";
-    //later will have a function to call that brings up a text box for commments
-}
+    };
+    function correct(){
+        gradevalue = 'correct';
+        var changing = document.getElementById("outer");
+        changing.className="outercorrect";
+    }
+    function wrong(){
+        gradevalue = 'wrong';
+        var changing = document.getElementById("outer");
+        changing.className="outerwrong";
+    }
+    function redo(){
+        gradevalue = 'redo';
+        var changing = document.getElementById("outer");
+        changing.className="outerredo";
+        //later will have a function to call that brings up a text box for commments
+    }
 
 
-}
+   // function getData(){
+       // var sketches = new Array();
 
+};
+
+Mvsketch.prototype = Object.create(HTMLElement.prototype);
