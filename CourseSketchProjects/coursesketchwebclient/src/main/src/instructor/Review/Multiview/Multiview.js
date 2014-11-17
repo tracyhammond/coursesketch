bc521d6a7ea2fe269b28e93f66d14cd31c693766
joshuazodda18 <chrome@neo.tamu.edu>
2014-11-17 16:30:54 -0600

    update fixed style
    fixed the errors David pointed out and deleted the test file

--- CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/Review/Multiview/Multiview.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/Review/Multiview/Multiview.js
@@ -1,31 +1,40 @@
-function Mvsketch(){
+/*
+* the Mvsketch function handles all the action that can take place
+* in the multiview units
+*/
+
+
+function Mvsketch() {
     var shadowRoot = undefined;
     var gradevalue = undefined;
 
 
-
-    this.initializeElement = function(templateClone){
+/*
+* this creates the shadow root and attches it to the object in question
+*/
+    this.initializeElement = function(templateClone) {
         shadowRoot = this.createShadowRoot();
         shadowRoot.appendChild(templateClone);
 
         shadowRoot.querySelector(".correctButton").onclick = correct;
         shadowRoot.querySelector(".wrongButton").onclick = wrong;
     };
-    function correct(){
+
+    /*
+    * marks the sketch at correct and changes the background to outercorrect
+    */
+    function correct() {
         gradevalue = 'correct';
         shadowRoot.querySelector("#outer").className='outerCorrect';
     }
-    function wrong(){
+    /*
+    * marks the sketch as wrong and changes the background to outerwrong
+    */
+    function wrong() {
         gradevalue = 'wrong';
         shadowRoot.querySelector("#outer").className='outerWrong';
     }
-  //  function redo(){
-       // gradevalue = 'redo';
-       // shadowRoot.querySelector(".outer").backgroundColor='green';
-        //later will have a function to call that brings up a text box for commments
-   // }
-    
 
-};
+}
 
 Mvsketch.prototype = Object.create(HTMLElement.prototype);
