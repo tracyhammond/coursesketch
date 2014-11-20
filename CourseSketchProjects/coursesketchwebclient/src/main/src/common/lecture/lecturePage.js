7ca611cd20580dc280d581f4950160c3e661b419
devint1 <devint1@icloud.com>
2014-11-19 18:08:49 -0600

    Do some stuff for the lecturePage
    Former-commit-id: 8ec095cee3b538a8e94f78fc560619fe6c586c18

--- CourseSketchProjects/coursesketchwebclient/src/main/src/common/lecture/lecturePage.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/common/lecture/lecturePage.js
@@ -1,7 +1,32 @@
-CourseSketch.Lecture = {
-	
-}
+(function() {
+    $(document).ready(function() {
+        CourseSketch.lecturePage = [];
+        CourseSketch.lecturePage.lecture = CourseSketch.dataManager.getState("currentLecture");
+        CourseSketch.dataManager.clearStates();
 
-$(document).ready(function() {
-	
-});
+        /**
+         * Adds a new text box to the current lecture slide.
+         */
+        CourseSketch.lecturePage.newTextBox = function() {
+            var textbox = document.createElement('text-box-creation');
+	    document.querySelector("#slide-content").appendChild(textbox);
+        }
+
+        CourseSketch.lecturePage.addSlideToDom = function(slideIndex) {
+            $("#slides>.content").append("<span class=\"slide-thumb\">" + slideIndex + "</span>");
+        }
+
+	CourseSketch.lecturePage.newSlide = function() {
+            CourseSketch.lecturePage.lecture.slides.push([]);
+            CourseSketch.lecturePage.addSlideToDom(CourseSketch.lecturePage.lecture.slides.length);
+        }
+
+        CourseSketch.lecturePage.displaySlides = function() {
+            for(var i = 0; i < CourseSketch.lecturePage.lecture.slides.length; ++i) {
+                CourseSketch.lecturePage.addSlideToDom(i);
+            }
+        }
+
+        //CourseSketch.lecturePage.displaySlides();
+    });
+})();
