85d725c5f358cd8131affa8801dbe0d95ab199f1
joshuazodda18 <chrome@neo.tamu.edu>
2014-11-20 14:41:49 -0600

    updated  multiviewMain page and names for files
    Former-commit-id: ab90988c71e6057dfcf05a9aeb597e36ab1ea43f

--- CourseSketchProjects/coursesketchwebclient/src/test/src/instructor/Review/Mv_instructor_view_fake_page.js
+++ CourseSketchProjects/coursesketchwebclient/src/test/src/instructor/Review/Mv_instructor_view_fake_page.js
@@ -1,35 +1,30 @@
-
-
-
-
-//var sketchList = new Array;
-
-
-function getSketchs( callback ){
-    CourseSketch.dataManager.getAllExperiments(CourseSketch.problemNavigator.getCurrentProblemId(), function(sketchList) {
-         if (isUndefined(sketchList)) {
-			if (element.isRunning()) {
-				element.finishWaiting();
+(function() {
+	function getSketchs( callback ){
+	    CourseSketch.dataManager.getAllExperiments(CourseSketch.problemNavigator.getCurrentProblemId(), function(sketchList) {
+	         if (isUndefined(sketchList)) {
+				if (element.isRunning()) {
+					element.finishWaiting();
+				}
+				return;
+			} 
+			if (!isUndefined(callback)) {
+				callback(sketchList);
 			}
-			return;
-		} 
-		if (!isUndefined(callback)) {
-			callback(sketchList);
-		}
-    });
-}
-
-
-function createMvSketch(array){
-	for(var i = 0; i < array.length(); i++){
-		var newelem = document.createElement('mv-sketch');
-		document.quereySeletor("sketch-area").appendChild(newelem);
-		newelem.setUpdateList(getUList(array, i))
+	    });
 	}
-}
 
-function getUList(array, index){
-	return CourseSketch.PROTOBUF_UTIL.decodeProtobuf(
-		array[index].getSubmission().getUpdateList(),
-		CourseSketch.PROTOBUF_UTIL.getSrlUpdateListClass());
-};
+
+	function createMvSketch(array) {
+		for(var i = 0; i < array.length(); i++){
+			var newelem = document.createElement('mv-sketch');
+			document.quereySeletor("sketch-area").appendChild(newelem);
+			newelem.setUpdateList(getUList(array, i));
+		}
+	}
+
+	function getUList(array, index) {
+		return CourseSketch.PROTOBUF_UTIL.decodeProtobuf(
+			array[index].getSubmission().getUpdateList(),
+			CourseSketch.PROTOBUF_UTIL.getSrlUpdateListClass());
+	}
+})();
