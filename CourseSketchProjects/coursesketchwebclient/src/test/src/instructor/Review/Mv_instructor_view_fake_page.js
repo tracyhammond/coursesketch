ee91ceac2326533e5ac8630cb82a293a8f2825b4
joshuazodda18 <chrome@neo.tamu.edu>
2014-11-20 14:29:22 -0600

    sketch creation function and sub-functions

---
+++ CourseSketchProjects/coursesketchwebclient/src/test/src/instructor/Review/Mv_instructor_view_fake_page.js
@@ -0,0 +1,35 @@
+
+
+
+
+//var sketchList = new Array;
+
+
+function getSketchs( callback ){
+    CourseSketch.dataManager.getAllExperiments(CourseSketch.problemNavigator.getCurrentProblemId(), function(sketchList) {
+         if (isUndefined(sketchList)) {
+			if (element.isRunning()) {
+				element.finishWaiting();
+			}
+			return;
+		} 
+		if (!isUndefined(callback)) {
+			callback(sketchList);
+		}
+    });
+}
+
+
+function createMvSketch(array){
+	for(var i = 0; i < array.length(); i++){
+		var newelem = document.createElement('mv-sketch');
+		document.quereySeletor("sketch-area").appendChild(newelem);
+		newelem.setUpdateList(getUList(array, i))
+	}
+}
+
+function getUList(array, index){
+	return CourseSketch.PROTOBUF_UTIL.decodeProtobuf(
+		array[index].getSubmission().getUpdateList(),
+		CourseSketch.PROTOBUF_UTIL.getSrlUpdateListClass());
+};
