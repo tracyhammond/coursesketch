20bb132c7c5f2b6d9cb601d4e647cf8ed05b87fb
devint1 <devint1@icloud.com>
2014-11-23 16:23:36 -0600

    Resizing of lecture slide area, fix race condition

--- CourseSketchProjects/coursesketchwebclient/src/main/src/common/lecture/lecturePage.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/common/lecture/lecturePage.js
@@ -1,42 +1,61 @@
 (function() {
-        CourseSketch.lecturePage = [];
-        if(isUndefined(CourseSketch.lecturePage.lecture)) {
-		CourseSketch.lecturePage.lecture = CourseSketch.dataManager.getState("currentLecture");
-		CourseSketch.dataManager.clearStates();
-	}
+    CourseSketch.lecturePage = [];
 
-        /**
-         * Adds a new text box to the current lecture slide.
-         */
-        CourseSketch.lecturePage.newTextBox = function() {
-            var textbox = document.createElement('text-box-creation');
-	    document.querySelector("#slide-content").appendChild(textbox);
+    /**
+     * Adds a new text box to the current lecture slide.
+     */
+    CourseSketch.lecturePage.newTextBox = function() {
+        var textbox = document.createElement('text-box-creation');
+    document.querySelector("#slide-content").appendChild(textbox);
+    }
+
+    CourseSketch.lecturePage.addSlideToDom = function(slideIndex) {
+        var cssWidth = "calc(10vw + "
+            + (CourseSketch.lecturePage.lecture.slides.length * 10.8)
+            + "vw + 200px)"
+        $("#slides>.content").css({
+            width: cssWidth
+        });
+        $("#slides>.content").append("<span class=\"slide-thumb\">" + slideIndex + "</span>");
+    }
+
+    CourseSketch.lecturePage.newSlide = function() {
+        var slide = CourseSketch.PROTOBUF_UTIL.LectureSlide();
+        slide.id = generateUUID();
+        slide.lectureId = CourseSketch.lecturePage.lecture.id;
+        slide.unlocked = true;
+        var finishGetCourse = function(lecture) {
+            
+            // TODO: This needs to be changed once insertSlide works!!!
+            //CourseSketch.lecturePage.lecture = lecture;
+            CourseSketch.lecturePage.lecture.slides.push(slide);
+            
+            CourseSketch.lecturePage.addSlideToDom(CourseSketch.lecturePage.lecture.slides.length);
         }
-
-        CourseSketch.lecturePage.addSlideToDom = function(slideIndex) {
-            $("#slides>.content").append("<span class=\"slide-thumb\">" + slideIndex + "</span>");
+        var finishInsert = function(lecture) {
+            CourseSketch.dataManager.getCourseLecture(CourseSketch.lecturePage.lecture.id, finishGetCourse, finishGetCourse);
         }
+        CourseSketch.dataManager.insertSlide(slide, finishInsert, finishInsert);
+    }
 
-	CourseSketch.lecturePage.newSlide = function() {
-            var slide = CourseSketch.PROTOBUF_UTIL.LectureSlide();
-            slide.id = generateUUID();
-            slide.lectureId = CourseSketch.lecturePage.lecture.id;
-            slide.unlocked = true;
-            var finishGetCourse = function(lecture) {
-                CourseSketch.lecturePage.lecture = lecture;
-                CourseSketch.lecturePage.addSlideToDom(CourseSketch.lecturePage.lecture.slides.length);
-            }
-            var finishInsert = function(lecture) {
-                CourseSketch.dataManager.getCourseLecture(CourseSketch.lecturePage.lecture.id, finishGetCourse, finishGetCourse);
-            }
-            CourseSketch.dataManager.insertSlide(slide, finishInsert, finishInsert);
+    CourseSketch.lecturePage.displaySlides = function() {
+        for(var i = 0; i < CourseSketch.lecturePage.lecture.slides.length; ++i) {
+            CourseSketch.lecturePage.addSlideToDom(i);
         }
+    }
 
-        CourseSketch.lecturePage.displaySlides = function() {
-            for(var i = 0; i < CourseSketch.lecturePage.lecture.slides.length; ++i) {
-                CourseSketch.lecturePage.addSlideToDom(i);
-            }
-        }
-
+    if (CourseSketch.dataManager.isDatabaseReady() && isUndefined(CourseSketch.lecturePage.lecture)) {
+        CourseSketch.lecturePage.lecture = CourseSketch.dataManager.getState("currentLecture");
+        CourseSketch.dataManager.clearStates();
         CourseSketch.lecturePage.displaySlides();
+    } else {
+        var intervalVar = setInterval(function() {
+        if (CourseSketch.dataManager.isDatabaseReady() && isUndefined(CourseSketch.lecturePage.lecture)) {
+            clearInterval(intervalVar);
+                CourseSketch.lecturePage.lecture = CourseSketch.dataManager.getState("currentLecture");
+        CourseSketch.dataManager.clearStates();
+                CourseSketch.lecturePage.displaySlides();
+        }
+        }, 100);
+    }
 })();
