dc6d2ff1ed744e19d85b9e9d88bfa05e766219a3
joshuazodda18 <chrome@neo.tamu.edu>
2014-12-01 16:18:21 -0600

    added comments

--- CourseSketchProjects/coursesketchwebclient/src/test/src/instructor/Review/Mv_instructor_view_fake_page.js
+++ CourseSketchProjects/coursesketchwebclient/src/test/src/instructor/Review/Mv_instructor_view_fake_page.js
@@ -13,7 +13,10 @@
 	    });
 	}
 
-
+	/*
+	 * creates a multiview sketch panel and attaches it to the grading area 
+	 * this can be done dynamically
+	 */
 	function createMvSketch(array) {
 		for(var i = 0; i < array.length(); i++){
 			var newelem = document.createElement('mv-sketch');
@@ -21,7 +24,9 @@
 			newelem.setUpdateList(getUList(array, i));
 		}
 	}
-
+	/*
+	 * gets a specific set of sketch data to be used in the multiview sketch panel
+	 */
 	function getUList(array, index) {
 		return CourseSketch.PROTOBUF_UTIL.decodeProtobuf(
 			array[index].getSubmission().getUpdateList(),
