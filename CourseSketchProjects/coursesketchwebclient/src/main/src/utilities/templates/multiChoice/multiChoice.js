3487a014b12421ad6d358a3cea8108a2bacbc8f2
Devin Tuchsen <devint1@icloud.com>
2014-12-06 20:05:11 -0600

    Multi-choice actually does stuff
    At least on the UI, still need to be able to load/save all of the data

--- CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/templates/multiChoice/multiChoice.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/templates/multiChoice/multiChoice.js
@@ -1,5 +1,82 @@
 function MultiChoice() {
     /**
+     * Removes an answer choice from this multiple choice element.
+     * @param event the event that triggered this function
+     * @param answer the answer element to be removed
+     */
+    this.removeAnswer = function(event, answer) {
+        this.shadowRoot.querySelector("#answer-choices").removeChild(answer);
+    }
+
+    /**
+     * Marks an answer choice as correct.
+     * @param event the event that triggered this function
+     * @param answer the answer element to set as the correct answer
+     */
+    this.setCorrectAnswer = function(event, answer) {
+        var answerChoices = this.shadowRoot.querySelectorAll(".answer-choice");
+        for(var i = 0; i < answerChoices.length; ++i) {
+            answerChoices[i].querySelector(".correct").textContent = "";
+        }
+        answer.querySelector(".correct").textContent = "✔";
+    }
+
+    /**
+     * Adds an answer choice to this multiple choice element.
+     * @param event the event that triggered this function
+     */
+    this.addAnswer = function(event) {
+        localScope = this;
+
+        // Set up the parent
+        var answer = document.createElement("div");
+        var lastAnswer = this.shadowRoot.querySelector("#answer-choices").lastChild;
+        answer.className = "answer-choice";
+        if(isUndefined(lastAnswer) || lastAnswer.className !== "answer-choice") {
+            answer.id = "1";
+        } else {
+            answer.id = parseInt(lastAnswer.id) + 1;
+        }
+
+        // Radio button
+        var radio = document.createElement("input");
+        radio.className = "radio";
+        radio.type = "radio";
+        radio.name = "answer";
+        radio.disabled = true;
+        answer.appendChild(radio);
+
+        // Need a newline after radio or things don't display properly
+        var text = document.createTextNode("\n");
+        answer.appendChild(text);
+
+        // Radio label (will be an input for instructors)
+        var label = document.createElement("input");
+        label.className = "label";
+        answer.appendChild(label);
+
+        // Correct check box
+        var correct = document.createElement("span");
+        correct.className = "correct";
+        correct.onclick = function(event) {
+            localScope.setCorrectAnswer(event, answer);
+        }
+        answer.appendChild(correct);
+
+        // Close icon
+        var close = document.createElement("span");
+        close.className = "close";
+        close.textContent = "×";
+        close.onclick = function(event) {
+            localScope.removeAnswer(event, answer);
+        }
+        answer.appendChild(close);
+
+        // Now that we are done creating the answer choice, add it
+        this.shadowRoot.querySelector("#answer-choices").appendChild(answer);
+    }
+
+    /**
      * @param templateClone {node} is a clone of the custom HTML Element for the text box
      * Makes the exit button close the box and enables dragging
      */
@@ -7,6 +84,11 @@ function MultiChoice() {
         localScope = this; // This sets the variable to the level of the custom element tag
         shadowRoot = this.createShadowRoot();
         shadowRoot.appendChild(templateClone);
+
+        // Bind addAnswer to click
+        localScope.shadowRoot.querySelector("#add").onclick = function(event) {
+            localScope.addAnswer(event);
+        }
     }
 
     this.saveData = function(event) {
@@ -15,7 +97,7 @@ function MultiChoice() {
         // Populate data in the proto object
         // mcProto.setQuestionText(this.shadowRoot.querySelector('#text').textContent);
 
-        // If the textbox does not have an id, then a command has not been created for the textbox
+        // If the multi-choice item does not have an id, then a command has not been created for the multi-choice item
         if ((isUndefined(this.id) || this.id == null || this.id == "")) {
             this.command = CourseSketch.PROTOBUF_UTIL.createBaseCommand(CourseSketch.PROTOBUF_UTIL.CommandType.CREATE_QUESTION,true);
         }
