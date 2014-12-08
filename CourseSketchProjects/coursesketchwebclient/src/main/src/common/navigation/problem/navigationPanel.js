9edf8842dd2bd93a19520bc5d1542692e1fa9a32
dtracers <dtracers@gmail.com>
2014-12-08 00:07:19 -0600

    More work on the navigation panel and making it pretty

--- CourseSketchProjects/coursesketchwebclient/src/main/src/common/navigation/problem/navigationPanel.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/common/navigation/problem/navigationPanel.js
@@ -1,57 +1,75 @@
+/**
+ * The custom element for navigating a problem.
+ * @attribute loop {existence} If this property exist the navigator will loop.  (Setting the navigator overrides this property).
+ * @attribute assignment_id {string} uses the given value as the assignment id inside the navigator.
+ * @attribute index {number} if the value exist then this is the number used to define the current index.
+ */
 function NavigationPanel() {
 
-var scope = document;
-		parent.problemNavigator.addCallback(function(nav) {
-			scope.getElementById("selectionBoxNumber").innerHTML = nav.getCurrentNumber();
-			// set span state
-			var button = scope.getElementById("buttonNext");
-			if (nav.hasNext()) {
+    /**
+     * Sets up the navigator so that the buttons will be b
+     */
+    function setUpNavigator() {
+        this.navigator.addCallback(function(nav) {
+            this.shadowRoot.querySelector("#selectionBoxNumber").textContent = nav.getCurrentNumber();
+            // set span state
+            setUpButtons(nav);
+            var totalNumber = nav.getLength();
+            if (totalNumber) {
+                this.shadowRoot.querySelector("#totalNumber").textContent = "out of " + totalNumber;
+            }
+            // TODO: change this to strip out bad HTML code
+            this.shadowRoot.querySelector("#problemPanel").innerHTML = '<p>' + parent.problemNavigator.getProblemText() + '</p>';
+        }.bind(this));
 
-				button.onclick = function(){parent.problemNavigator.gotoNext();};
-			} else {
-				button.onclick = function(){};
-			}
-			button = scope.getElementById("buttonPrev");
-			if (nav.hasPrevious()) {
-				button.onclick = function(){parent.problemNavigator.gotoPrevious();};
-			} else {
-				button.onclick = function(){};
-			}
-			var totalNumber = nav.getLength();
-			if (totalNumber) {
-				scope.getElementById("totalNumber").innerHTML = "out of " + totalNumber;
-			}
-			// TODO: change this to strip out bad HTML code
-			scope.getElementById("problemPanel").innerHTML = '<p>' + parent.problemNavigator.getProblemText() + '</p>';
-		});
+        setUpButtons(this.navigator);
+    }
 
-		/*
-		window.onresize = function() {
-			var navWidth = document.getElementById("navPanel").offsetWidth;
-			var navHeight = document.getElementById("navPanel").offsetHeight;
-			var textWidth = document.getElementById("panelWrapper").offsetWidth - navWidth;
-			document.getElementById("problemPanel").style.width = (textWidth - 15) +"px";
-			document.getElementById("problemPanel").style.height = (navHeight -15) +"px";
-		}
-		*/
+    function setUpButtons(nav) {
+        var button = this.shadowRoot.querySelector("#buttonNext");
+        if (nav.hasNext()) {
+            button.onclick = function() {nav.gotoNext();};
+            button.disabled = false;
+        } else {
+            button.onclick = function() {};
+            button.disabled = true;
+        }
+        button = scope.getElementById("buttonPrev");
+        if (nav.hasPrevious()) {
+            button.onclick = function() {nav.gotoPrevious();};
+            button.disabled = false;
+        } else {
+            button.onclick = function() {};
+            button.disabled = true;
+        }
+    }
+        /*
+        window.onresize = function() {
+            var navWidth = document.getElementById("navPanel").offsetWidth;
+            var navHeight = document.getElementById("navPanel").offsetHeight;
+            var textWidth = document.getElementById("panelWrapper").offsetWidth - navWidth;
+            document.getElementById("problemPanel").style.width = (textWidth - 15) +"px";
+            document.getElementById("problemPanel").style.height = (navHeight -15) +"px";
+        }
+        */
 
-	/**
-	 * @param templateClone {node} is a clone of the custom HTML Element for the text box
-	 * Makes the exit button close the box and enables dragging
-	 */
-	this.initializeElement = function(templateClone) {
-		localScope = this; // This sets the variable to the level of the custom element tag
-		shadowRoot = this.createShadowRoot();
-		shadowRoot.appendChild(templateClone);
+    /**
+     * @param templateClone {node} is a clone of the custom HTML Element for the text box
+     * Makes the exit button close the box and enables dragging
+     */
+    this.initializeElement = function(templateClone) {
+        localScope = this; // This sets the variable to the level of the custom element tag
+        shadowRoot = this.createShadowRoot();
+        shadowRoot.appendChild(templateClone);
 
-		if (isUndefined(this.navigator)) {
-			this.navigator = new ProblemNavigator
-		}
-	}
+        if (isUndefined(this.navigator)) {
+            this.navigator = new ProblemNavigator(this.dataset.assignment_id, !isUndefined(this.dataset.loop), this.dataset.index);
+        }
+    }
 
-	this.setNavigator = function(navPanel) {
-		this.navigator = navPanel;
-	}
+    this.setNavigator = function(navPanel) {
+        this.navigator = navPanel;
+    }
 }
 
 NavigationPanel.prototype = Object.create(HTMLElement.prototype);
