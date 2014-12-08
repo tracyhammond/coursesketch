51741eab14d85b5c94e8592db1947f9eb1755af6
dtracers <dtracers@gmail.com>
2014-12-07 23:50:38 -0600

    Added some code to the navigator
    Former-commit-id: 825335dc9969c86aa0870fa64459b23bbcae8a5e

--- CourseSketchProjects/coursesketchwebclient/src/main/src/common/navigation/problem/navigationPanel.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/common/navigation/problem/navigationPanel.js
@@ -1,3 +1,5 @@
+function NavigationPanel() {
+
 var scope = document;
 		parent.problemNavigator.addCallback(function(nav) {
 			scope.getElementById("selectionBoxNumber").innerHTML = nav.getCurrentNumber();
@@ -23,6 +25,7 @@ var scope = document;
 			scope.getElementById("problemPanel").innerHTML = '<p>' + parent.problemNavigator.getProblemText() + '</p>';
 		});
 
+		/*
 		window.onresize = function() {
 			var navWidth = document.getElementById("navPanel").offsetWidth;
 			var navHeight = document.getElementById("navPanel").offsetHeight;
@@ -30,3 +33,25 @@ var scope = document;
 			document.getElementById("problemPanel").style.width = (textWidth - 15) +"px";
 			document.getElementById("problemPanel").style.height = (navHeight -15) +"px";
 		}
+		*/
+
+	/**
+	 * @param templateClone {node} is a clone of the custom HTML Element for the text box
+	 * Makes the exit button close the box and enables dragging
+	 */
+	this.initializeElement = function(templateClone) {
+		localScope = this; // This sets the variable to the level of the custom element tag
+		shadowRoot = this.createShadowRoot();
+		shadowRoot.appendChild(templateClone);
+
+		if (isUndefined(this.navigator)) {
+			this.navigator = new ProblemNavigator
+		}
+	}
+
+	this.setNavigator = function(navPanel) {
+		this.navigator = navPanel;
+	}
+}
+
+NavigationPanel.prototype = Object.create(HTMLElement.prototype);
