0a7f20ea6babd61fc60c2ed52f1dad463b6e941c
dtracers <dtracers@gmail.com>
2014-12-08 01:21:29 -0600

    DONT MERGE
    This will get merged in when we are done with the legacy website.
    
    
    Former-commit-id: d360284f9a0aa411b8de65d8c87cb92f7133baea

--- CourseSketchProjects/coursesketchwebclient/src/js/test/buttonsData.js
+++
@@ -1,7 +0,0 @@
-﻿
-new ButtonObject("Math", ["Angle", "Cosine", "Tangent", "Factorial", "Exponent"]);
-new ButtonObject("English", ["Italics", "Capitals", "Bold", "Factorial", "Exponent"]);
-new ButtonObject("Physics", ["Angle", "Cosine", "Tangent", "Factorial", "Exponent"]);
-new ButtonObject("Chemistry", ["Angle", "Cosine", "Tangent", "Factorial", "Exponent"]);
-new ButtonObject("Computer", ["Angle", "Cosine", "Tangent", "Factorial", "Exponent"]);
-new ButtonObject("Arts", ["Angle", "Cosine", "Tangent", "Factorial", "Exponent"]);
--- CourseSketchProjects/coursesketchwebclient/src/js/test/textBox.js
+++
@@ -1,7 +0,0 @@
-﻿function ButtonObject(name, array) {
-    this.buttonName = name;
-    this.subButtons = array;
-    
-
-
-}
