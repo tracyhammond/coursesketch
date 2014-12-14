3b3cae9c92774e0cc208fcadfc642899d4fb3fb3
dtracers <dtracers@gmail.com>
2014-12-14 17:28:01 -0600

    Moved javascript to a different file

---
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/student/search/classSearch.js
@@ -0,0 +1,106 @@
+(function() {
+$(document).ready(function() {
+        var request = CourseSketch.PROTOBUF_UTIL.DataRequest();
+        var item = CourseSketch.PROTOBUF_UTIL.ItemRequest();
+        item.query = CourseSketch.PROTOBUF_UTIL.ItemQuery.COURSE_SEARCH;
+        request.items = [item];
+        CourseSketch.connection.sendRequest(parent.connection.createRequestFromData(request, CourseSketch.PROTOBUF_UTIL.getRequestClass().MessageType.DATA_REQUEST));
+    });
+    var localDoc = document;
+    var courseList1 = new Array();
+    var courseList2 = new Array();
+    var courseRightSide = {};
+    var courseProtoMap = {};
+    var schoolSet = SchoolBuilder.SrlSchool;
+    var schoolItemBuilder = new SchoolItemBuilder();
+    var setTimeVar;
+    (function() {
+        schoolItemBuilder.setWidth('large').centerItem(true);
+        schoolItemBuilder.showImage = false; // till we have images actually working!
+        schoolItemBuilder.setBoxClickFunction(courseClickerFunction);
+    })();
+
+    parent.dataListener.setListener(Request.MessageType.DATA_REQUEST, QueryBuilder.ItemQuery.COURSE_SEARCH, function(evt, item) {
+        var school = schoolSet.decode(item.data);
+        var courseList = school.courses;
+        var idList = [];
+        for(var i = 0; i < courseList.length; i++) {
+            courseProtoMap[courseList[i].id] = courseList[i];
+            if (i % 2 == 0) {
+                courseList1.push(courseList[i]);
+                courseRightSide[courseList[i].id] = false;
+            } else {
+                courseList2.push(courseList[i]);
+                courseRightSide[courseList[i].id] = true;
+            }
+        }
+        schoolItemBuilder.setList(courseList1).build("class_list_column1");
+        schoolItemBuilder.setList(courseList2).build("class_list_column2");
+        localDoc.getElementById("loadingIcon").style.display="none";
+    });
+
+    parent.dataListener.setErrorListener(function(msg){
+        localDoc.getElementById("loadingIcon").innerHTML = '<h1>error loading data</h1> <p>' + msg.getResponseText() + '</p>';
+        clearTimeout(setTimeVar);
+    });
+
+    parent.dataListener.setListener(Request.MessageType.DATA_REQUEST, QueryBuilder.ItemQuery.REGISTER, function(evt, item) {
+        alert("User is already registered for this course");
+        clearTimeout(setTimeVar);
+    });
+
+    function courseClickerFunction(course) {
+        var id = course.id;
+        var element = localDoc.getElementById(id);
+        var width = element.offsetWidth/2;
+        var moveAmount = width + "px";
+        //console.log("style "  + element.style.marginLeft);
+        if (element.style.marginLeft == "" || element.style.marginLeft == "0px") {
+            if (courseRightSide[id]) {
+                moveAmount = "" + moveAmount;
+            } else {
+                moveAmount = "-" + moveAmount;
+            }
+            var button = localDoc.createElement('button');
+            button.setAttribute("id", "button"+id);
+            button.onclick = function() {
+                registerClass(id);
+                setTimeVar = setTimeout(function () { alert("Your have successfully registered") }, 3000);
+            }
+            button.textContent = "Register";
+            button.style.position = "absolute";
+            if (courseRightSide[id]) {
+                button.style.left = element.offsetLeft + "px";
+            } else {
+                button.style.left = (element.offsetLeft + element.offsetWidth - width/2) + "px";
+            }
+            button.style.top = element.offsetHeight/2 + element.offsetTop + "px";
+            //localDoc.appendChild(button);
+            localDoc.getElementById("registerButton").appendChild(button);
+            $("#" + id).animate({
+                marginLeft: moveAmount,
+                }, 300, function () {
+
+            });
+        } else {
+            $("#" + id).animate({
+                marginLeft: "0px",
+                }, 300, function () {
+                    localDoc.getElementById("registerButton").removeChild(localDoc.getElementById("button" + id));
+            });
+        }
+    }
+    function registerClass(id) {
+        var request = new QueryBuilder.DataSend();
+        var item = new QueryBuilder.ItemSend();
+        item.query = QueryBuilder.ItemQuery.REGISTER;
+        item.data = courseProtoMap[id].toArrayBuffer();
+        request.items = [item];
+        parent.connection.sendRequest(parent.connection.createRequestFromData(request, Request.MessageType.DATA_INSERT));
+        $("#" + id).animate({
+            marginLeft: "0px",
+            }, 300, function () {
+                localDoc.getElementById("registerButton").removeChild(localDoc.getElementById("button" + id));
+        });
+}
+});
