108febb8311e6471eeff56cb1ea1554dc967af26
dtracers <dtracers@gmail.com>
2014-12-23 04:16:15 -0600

    working on adding the sketch surface
    created a function that will attempt to set up the sketch surface for
    doing sketching things!

--- CourseSketchProjects/coursesketchwebclient/src/main/src/student/experiment/experiment.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/student/experiment/experiment.js
@@ -15,4 +15,27 @@
         });
     });
 
+    function loadSketch(navigator) {
+        var sketchSurface = document.createElement("sketch-surface");
+        var element = new WaitScreenManager().setWaitType(WaitScreenManager.TYPE_PERCENT).build();
+        document.getElementById("percentBar").appendChild(element);
+        element.startWaiting();
+
+        CourseSketch.dataManager.getSubmission(navigator.getCurrentProblemId(), function(submission) {
+            if (isUndefined(submission)) {
+                if (element.isRunning()) {
+                    element.finishWaiting();
+                }
+                return;
+            }
+
+            var updateList = ProtoUpdateCommandBuilder.SrlUpdateList.decode(submission.getUpdateList());
+            //console.log(updateList);
+            commandManager.setUpdateList(updateList.getList(), element);
+            updateList = null;
+            element = null;
+            //console.log(submission);
+        });
+        document.getElementById("problemPanel").appendChild(sketchSurface);
+    }
 })();
