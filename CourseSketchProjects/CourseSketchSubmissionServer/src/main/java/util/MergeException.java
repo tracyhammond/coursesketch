8e15e3968ef7998752633eb54e46e9104734ed3d
dtracers <dtracers@gmail.com>
2014-12-28 16:13:51 -0600

    Added an exception
    Former-commit-id: e2f2c9c46705412effae0ef6f8aebf6485328354

---
+++ CourseSketchProjects/CourseSketchSubmissionServer/src/main/java/util/MergeException.java
@@ -0,0 +1,10 @@
+package util;
+
+/**
+ * Created by gigemjt on 12/28/14.
+ */
+public class MergeException extends Exception {
+    public MergeException(final String s) {
+        super(s);
+    }
+}
