b244e73c50c72b7611907626399dc167baa88554
dtracers <dtracers@gmail.com>
2014-12-28 16:08:21 -0600

    Updating to latest version of the socket system.
    Former-commit-id: eb2d7fddbba6b14b79f25adbe1b03fa81547ea8e

--- CourseSketchProjects/CourseSketchSubmissionServer/src/main/java/connection/DataClientWebSocket.java
+++ CourseSketchProjects/CourseSketchSubmissionServer/src/main/java/connection/DataClientWebSocket.java
@@ -1,17 +1,15 @@
 package connection;
 
-import java.net.URI;
-import java.nio.ByteBuffer;
-
 import coursesketch.server.base.ClientWebSocket;
 import coursesketch.server.base.ServerWebSocketHandler;
-import coursesketch.server.base.ServerWebSocketHandler.Decoder;
-
+import coursesketch.server.interfaces.AbstractServerWebSocketHandler;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
-
 import protobuf.srl.request.Message.Request;
 import utilities.ConnectionException;
 import utilities.TimeManager;
+
+import java.net.URI;
+import java.nio.ByteBuffer;
 
 
 /** This example demonstrates how to create a websocket connection to a server. Only the most important callbacks are overloaded. */
@@ -29,12 +27,12 @@ public class DataClientWebSocket extends ClientWebSocket {
 	 */
 	@Override
 	public void onMessage(final ByteBuffer buffer) {
-		final Request req = Decoder.parseRequest(buffer);
+		final Request req = AbstractServerWebSocketHandler.Decoder.parseRequest(buffer);
 		if (req.getRequestType() == Request.MessageType.TIME) {
 			final Request rsp = TimeManager.decodeRequest(req);
 			if (rsp != null) {
 				try {
-					this.parentManager.send(rsp, req.getSessionInfo(), DataClientWebSocket.class);
+					this.getParentManager().send(rsp, req.getSessionInfo(), DataClientWebSocket.class);
 				} catch (ConnectionException e) {
 					e.printStackTrace();
 				}
