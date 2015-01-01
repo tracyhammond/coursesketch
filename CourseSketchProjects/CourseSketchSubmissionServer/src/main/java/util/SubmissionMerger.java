7e277bb65961e880e7adc8dbcea1a25fe0989e01
dtracers <dtracers@gmail.com>
2015-01-01 03:26:12 -0600

    Added some authentication and added undo redo merging
    Adding tests tomorrow!
    
    
    Former-commit-id: aec1165b7b0de636fd5ace87901021055b73eac6

--- CourseSketchProjects/CourseSketchSubmissionServer/src/main/java/util/SubmissionMerger.java
+++ CourseSketchProjects/CourseSketchSubmissionServer/src/main/java/util/SubmissionMerger.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class SubmissionMerger {
     private Commands.SrlUpdateList databaseList, clientList;
     private boolean isModerator;
+
     public SubmissionMerger(final Commands.SrlUpdateList databaseList, final Commands.SrlUpdateList clientList) {
         this.databaseList = databaseList;
         this.clientList = clientList;
@@ -23,6 +24,7 @@ public class SubmissionMerger {
      * True if you are a moderator.
      * Moderators can only do certain things.
      * We do not check if you are an admin or a user as that is handled externally.
+     *
      * @param isMod
      */
     public void setIsModerator(final boolean isMod) {
@@ -34,26 +36,34 @@ public class SubmissionMerger {
      *
      * There are three cases:
      * <ul>
-     *     <li>An undo has happened after a save has happened.  This will insert a marker into an older part of the list.</li>
-     *     <li>A switch sketch occurs in the middle of another system. (which in case you must be the moderator or admin).</li>
-     *     <li>The client list is just longer than the database list.</li>
+     * <li>An undo has happened after a save has happened.  This will insert a marker into an older part of the list.</li>
+     * <li>A switch sketch occurs in the middle of another system. (which in case you must be the moderator or admin).</li>
+     * <li>The client list is just longer than the database list.</li>
      * </ul>
      */
     public Commands.SrlUpdateList merge() throws MergeException {
-        List<Commands.SrlUpdate> result = merge(databaseList.getListList(), clientList.getListList());
-        Commands.SrlUpdateList.Builder build = Commands.SrlUpdateList.newBuilder();
+        final List<Commands.SrlUpdate> result = merge(databaseList.getListList(), clientList.getListList());
+        final Commands.SrlUpdateList.Builder build = Commands.SrlUpdateList.newBuilder();
         return build.addAllList(result).build();
     }
 
-    private List<Commands.SrlUpdate> merge(List<Commands.SrlUpdate> database, List<Commands.SrlUpdate> client) throws MergeException {
-        int differentIndex = Checksum.indexOfDifference(database, client);
+    private List<Commands.SrlUpdate> merge(final List<Commands.SrlUpdate> database, final List<Commands.SrlUpdate> client) throws MergeException {
+        final int differentIndex = Checksum.indexOfDifference(database, client);
         if (differentIndex == -1) {
             return database;
         }
 
         // if the only difference is that one is longer than we return the entire client list as no modification has occurred.
-        if (differentIndex == database.size()) {
+        if (differentIndex == database.size() && !isModerator) {
             return client;
+        } else if (isModerator) {
+            final Commands.SrlUpdate differentUpdate = client.get(differentIndex);
+            if (differentUpdate.getCommands(0).getCommandType() == Commands.CommandType.SWITCH_SKETCH
+                    || differentUpdate.getCommands(0).getCommandType() == Commands.CommandType.CREATE_SKETCH) {
+                // They just added the feedback at the end of the sketch.  We will allow moderators to perform this action.
+                return client;
+            }
+            throw new MergeException("Moderator is not allowed to change the list in this way");
         }
 
         // something weird happened so we quit and don't modify.
@@ -65,14 +75,15 @@ public class SubmissionMerger {
         final Commands.SrlUpdate differentUpdate = client.get(differentIndex);
 
         // switch sketch!
-        if (differentUpdate.getCommands(0).getCommandType() == Commands.CommandType.SWITCH_SKETCH || differentUpdate.getCommands(0).getCommandType() == Commands.CommandType.CREATE_SKETCH ) {
+        if (differentUpdate.getCommands(0).getCommandType() == Commands.CommandType.SWITCH_SKETCH
+                || differentUpdate.getCommands(0).getCommandType() == Commands.CommandType.CREATE_SKETCH) {
             final String startingSketch = getPreviousSketchId(database, differentIndex - 1);
             if (startingSketch == null) {
                 throw new MergeException("Switch sketch inserted before parent sketch was created!");
             }
             final int endingIndex = getMatchingSketchId(client, differentIndex + 1, startingSketch);
             if (endingIndex == -1) {
-                throw new MergeException("Client list does not switch back.");
+                throw new MergeException("new additions do not switch back to the old sketch!");
             }
             final List<Commands.SrlUpdate> result = new ArrayList<>();
             final List<Commands.SrlUpdate> listForSecondMerge = new ArrayList<>();
@@ -87,28 +98,64 @@ public class SubmissionMerger {
             result.addAll(secondHalfOfMerge);
             return result;
         }
+
+        // switch sketch!
+        if (differentUpdate.getCommands(0).getCommandType() == Commands.CommandType.MARKER) {
+            final Commands.SrlCommand command = differentUpdate.getCommands(0);
+            Commands.Marker marker = null;
+            try {
+                marker = Commands.Marker.parseFrom(command.getCommandData());
+            } catch (InvalidProtocolBufferException e) {
+                throw new MergeException("List contains data in an invalid format.");
+            }
+            if (marker.getType() != Commands.Marker.MarkerType.SPLIT) {
+                throw new MergeException("List contains an unsupported marker type");
+            }
+            final int endingIndex = Integer.parseInt(marker.getOtherData());
+            if (endingIndex <= -1) {
+                throw new MergeException("Split marker is not correctly formatted.");
+            }
+
+            if (endingIndex + differentIndex + 1 <= database.size()) {
+                throw new MergeException("Split marker added at invalid position database list extends after marker");
+            }
+
+            if (isModerator) {
+                throw new MergeException("Moderator is not allowed to change the list in this way");
+            }
+
+            final List<Commands.SrlUpdate> result = new ArrayList<>();
+            result.addAll(database.subList(0, differentIndex));
+
+            final List<Commands.SrlUpdate> merged = merge(database.subList(differentIndex, database.size()),
+                    client.subList(differentIndex + 1, differentIndex + endingIndex));
+            result.addAll(merged);
+            result.addAll(client.subList(differentIndex + endingIndex, client.size()));
+            return result;
+        }
         return database;
     }
 
     /**
      * Goes back until a switch sketch or create sketch is found and returns that id.
+     *
      * @param updates
      * @param startIndex
      * @return The id of the starting sketch.
      */
-    private String getPreviousSketchId(List<Commands.SrlUpdate> updates, int startIndex) {
+    private String getPreviousSketchId(final List<Commands.SrlUpdate> updates, final int startIndex) {
         for (int i = startIndex; i >= 0; i--) {
-            Commands.SrlCommand command = updates.get(i).getCommands(0);
+            final Commands.SrlCommand command = updates.get(i).getCommands(0);
             if (command.getCommandType() == Commands.CommandType.SWITCH_SKETCH) {
                 try {
-                    Commands.IdChain id = Commands.IdChain.parseFrom(command.getCommandData());
+                    final Commands.IdChain id = Commands.IdChain.parseFrom(command.getCommandData());
                     return id.getIdChain(0);
                 } catch (InvalidProtocolBufferException e) {
                     e.printStackTrace();
                 }
             } else if (command.getCommandType() == Commands.CommandType.CREATE_SKETCH) {
                 try {
-                    Commands.ActionCreateSketch createSketch = Commands.ActionCreateSketch.parseFrom(command.getCommandData());
+                    final Commands.ActionCreateSketch createSketch = Commands.ActionCreateSketch.parseFrom(command.getCommandData());
                     return createSketch.getSketchId().getIdChain(0);
                 } catch (InvalidProtocolBufferException e) {
                     e.printStackTrace();
@@ -120,13 +167,14 @@ public class SubmissionMerger {
 
     /**
      * Goes back until a switch sketch or create sketch is found and returns that id.
+     *
      * @param updates
      * @param startIndex
      * @return The id of the starting sketch.
      */
-    private int getMatchingSketchId(List<Commands.SrlUpdate> updates, int startIndex, String matchingSketchId) {
+    private int getMatchingSketchId(final List<Commands.SrlUpdate> updates, final int startIndex, final String matchingSketchId) {
         for (int i = startIndex; i < updates.size(); i++) {
-            Commands.SrlCommand command = updates.get(i).getCommands(0);
+            final Commands.SrlCommand command = updates.get(i).getCommands(0);
             if (command.getCommandType() == Commands.CommandType.SWITCH_SKETCH) {
                 try {
                     Commands.IdChain id = Commands.IdChain.parseFrom(command.getCommandData());
@@ -138,7 +186,7 @@ public class SubmissionMerger {
                 }
             } else if (command.getCommandType() == Commands.CommandType.CREATE_SKETCH) {
                 try {
-                    Commands.ActionCreateSketch createSketch = Commands.ActionCreateSketch.parseFrom(command.getCommandData());
+                    final Commands.ActionCreateSketch createSketch = Commands.ActionCreateSketch.parseFrom(command.getCommandData());
                     if (createSketch.getSketchId().getIdChain(0).equals(matchingSketchId)) {
                         return i;
                     }
