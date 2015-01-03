f276b8066c74c4fb78cb385c30ab7a11bdd62e81
dtracers <dtracers@gmail.com>
2015-01-02 23:59:09 -0600

    changed tabs to spaces
    Former-commit-id: 2d88cbef2e5877153ddff3e2d2bcb41f1c16a7bf

--- CourseSketchProjects/CourseSketchSubmissionServer/src/main/java/connection/SubmissionServlet.java
+++ CourseSketchProjects/CourseSketchSubmissionServer/src/main/java/connection/SubmissionServlet.java
@@ -15,29 +15,33 @@ import coursesketch.server.interfaces.MultiConnectionManager;
 @SuppressWarnings("serial")
 public class SubmissionServlet extends ServerWebSocketInitializer {
 
-	/**
-	 * Creates a GeneralConnectionServlet.
-	 * @param timeoutTime The time it takes before a connection times out.
-	 * @param isSecure True if the connection is allowing SSL connections.
-	 * @param connectLocally True if the server is connecting locally.
-	 */
-	public SubmissionServlet(final long timeoutTime, final boolean isSecure, final boolean connectLocally) {
-		super(timeoutTime, isSecure, connectLocally);
-	}
-
-	@Override
-	public final ServerWebSocketHandler createServerSocket() {
-    	return new SubmissionServerWebSocketHandler(this);
+    /**
+     * Creates a GeneralConnectionServlet.
+     *
+     * @param timeoutTime
+     *         The time it takes before a connection times out.
+     * @param isSecure
+     *         True if the connection is allowing SSL connections.
+     * @param connectLocally
+     *         True if the server is connecting locally.
+     */
+    public SubmissionServlet(final long timeoutTime, final boolean isSecure, final boolean connectLocally) {
+        super(timeoutTime, isSecure, connectLocally);
     }
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <br>
-	 * We do not need to manage multiple connections so we might as well just make it return null.
-	 */
-	@Override
-	public final MultiConnectionManager createConnectionManager(final boolean connectLocally, final boolean secure) {
-		return new SubmissionConnectionManager(this.getServer(), connectLocally, secure);
-	}
+    @Override
+    public final ServerWebSocketHandler createServerSocket() {
+        return new SubmissionServerWebSocketHandler(this);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <br>
+     * We do not need to manage multiple connections so we might as well just make it return null.
+     */
+    @Override
+    public final MultiConnectionManager createConnectionManager(final boolean connectLocally, final boolean secure) {
+        return new SubmissionConnectionManager(this.getServer(), connectLocally, secure);
+    }
 }
