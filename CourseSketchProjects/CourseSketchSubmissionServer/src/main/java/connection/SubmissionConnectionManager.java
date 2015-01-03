1c5985ce39ad7fbc92052a629a05125fc14f47ed
dtracers <dtracers@gmail.com>
2015-01-02 23:58:12 -0600

    Fixed check style issues

--- CourseSketchProjects/CourseSketchSubmissionServer/src/main/java/connection/SubmissionConnectionManager.java
+++ CourseSketchProjects/CourseSketchSubmissionServer/src/main/java/connection/SubmissionConnectionManager.java
@@ -4,18 +4,39 @@ import coursesketch.server.interfaces.AbstractServerWebSocketHandler;
 import coursesketch.server.interfaces.MultiConnectionManager;
 import utilities.ConnectionException;
 
-public class SubmissionConnectionManager extends MultiConnectionManager {
+/**
+ * A manager for holding all of the connections that were created.
+ *
+ * @author gigemjt
+ */
+public final class SubmissionConnectionManager extends MultiConnectionManager {
 
-	public SubmissionConnectionManager(AbstractServerWebSocketHandler parent, boolean connectType, boolean secure) {
-		super(parent, connectType, secure);
-	}
+    /**
+     * Port number.
+     */
+    private static final int PORT = 8885;
 
-	@Override
-	public void connectServers(AbstractServerWebSocketHandler serv) {
-		try {
-			createAndAddConnection(serv, isConnectionLocal(), "srl04.tamu.edu", 8885, isSecure(), DataClientWebSocket.class);
-		} catch (ConnectionException e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * Creates a default {@link MultiConnectionManager}.
+     *
+     * @param parent  The server that is using this object.
+     * @param isLocal True if the connection should be for a local server instead of
+     *                 a remote server.
+     * @param isSecure  True if the connections should be isSecure.
+     */
+    public SubmissionConnectionManager(final AbstractServerWebSocketHandler parent, final boolean isLocal, final boolean isSecure) {
+        super(parent, isLocal, isSecure);
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    @Override
+    public void connectServers(final AbstractServerWebSocketHandler serv) {
+        try {
+            createAndAddConnection(serv, isConnectionLocal(), "srl04.tamu.edu", PORT, isSecure(), DataClientWebSocket.class);
+        } catch (ConnectionException e) {
+            e.printStackTrace();
+        }
+    }
 }
