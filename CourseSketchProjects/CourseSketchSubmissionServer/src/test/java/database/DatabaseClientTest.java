f7678b09467d22ab606aaefdc6fdb7de1f9c0e30
dtracers <dtracers@gmail.com>
2015-01-04 05:40:27 -0600

    50% coverage! making progress!
    Former-commit-id: 903558863982a6fb54144f61313be7ffe1ce2fd3

--- CourseSketchProjects/CourseSketchSubmissionServer/src/test/java/database/DatabaseClientTest.java
+++ CourseSketchProjects/CourseSketchSubmissionServer/src/test/java/database/DatabaseClientTest.java
@@ -4,9 +4,7 @@ import com.github.fakemongo.junit.FongoRule;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.mongodb.DB;
 import coursesketch.server.interfaces.AbstractServerWebSocketHandler;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import protobuf.srl.commands.Commands;
@@ -23,12 +21,35 @@ public class DatabaseClientTest {
     @Rule
     public FongoRule fongoRule = new FongoRule();
 
-    @Before
-    public void setUp() throws Exception {
+    public static Commands.SrlUpdateList createSimpleDatabaseListWithSaveMarker(long submissionTime) {
+        Commands.SrlUpdateList fakeList = SubmissionMergerTest.createSimpleDatabaseList(100);
+        return addSaveMarker(fakeList, submissionTime);
     }
 
-    @After
-    public void tearDown() throws Exception {
+    /**
+     * Adds a save marker to the end of the list so that it has an update time that is the same as the submission time.
+     *
+     * @param fakeList
+     * @param submissionTime
+     * @return
+     */
+    public static Commands.SrlUpdateList addSaveMarker(Commands.SrlUpdateList fakeList, long submissionTime) {
+        Commands.SrlUpdateList.Builder newList = Commands.SrlUpdateList.newBuilder(fakeList);
+
+        // create a new marker with the save list item.
+        final Commands.SrlUpdate.Builder saveUpdate = Commands.SrlUpdate.newBuilder();
+        saveUpdate.setUpdateId(AbstractServerWebSocketHandler.Encoder.nextID().toString());
+        saveUpdate.setTime(submissionTime);
+
+        final Commands.Marker saveMarker = Commands.Marker.newBuilder().setType(Commands.Marker.MarkerType.SAVE).build();
+        final Commands.SrlCommand saveCommand = Commands.SrlCommand.newBuilder().setCommandType(Commands.CommandType.MARKER).setCommandId(
+                AbstractServerWebSocketHandler.Encoder.nextID().toString()).setCommandData(saveMarker.toByteString())
+                .setIsUserCreated(false).build();
+
+        saveUpdate.addCommands(saveCommand);
+
+        newList.addList(saveUpdate);
+        return newList.build();
     }
 
     @Test
@@ -113,24 +134,84 @@ public class DatabaseClientTest {
         Assert.assertNotEquals(clientUpdate.getTime(), lastUpdate.getTime());
     }
 
-    public static Commands.SrlUpdateList createSimpleDatabaseListWithSaveMarker(long submissionTime) {
-        Commands.SrlUpdateList fakeList = SubmissionMergerTest.createSimpleDatabaseList(100);
-        Commands.SrlUpdateList.Builder newList = Commands.SrlUpdateList.newBuilder(fakeList);
+    @Test
+    public void testUpdateListMergesCorrectly() throws DatabaseAccessException {
 
-        // create a new marker with the save list item.
-        final Commands.SrlUpdate.Builder saveUpdate = Commands.SrlUpdate.newBuilder();
-        saveUpdate.setUpdateId(AbstractServerWebSocketHandler.Encoder.nextID().toString());
-        saveUpdate.setTime(submissionTime);
+        // once you have a DB instance, you can interact with it
+        // just like you would with a real one.
+        DB db = fongoRule.getDB();
+        DatabaseClient client = getMockedVersion(db);
 
-        final Commands.Marker saveMarker = Commands.Marker.newBuilder().setType(Commands.Marker.MarkerType.SAVE).build();
-        final Commands.SrlCommand saveCommand = Commands.SrlCommand.newBuilder().setCommandType(Commands.CommandType.MARKER).setCommandId(
-                AbstractServerWebSocketHandler.Encoder.nextID().toString()).setCommandData(saveMarker.toByteString())
-                .setIsUserCreated(false).build();
+        // round 1
+        Submission.SrlSubmission.Builder build = Submission.SrlSubmission.newBuilder();
+        final Commands.SrlUpdateList original = createSimpleDatabaseListWithSaveMarker(200);
+        build.setUpdateList(original);
+        Submission.SrlExperiment expected = getFakeExperiment("User1", build.build());
+        String id = DatabaseClient.saveExperiment(expected, 200, client);
 
-        saveUpdate.addCommands(saveCommand);
+        // round 2
+        Submission.SrlSubmission.Builder secondList = Submission.SrlSubmission.newBuilder();
+        secondList.setUpdateList(SubmissionMergerTest.createSimpleDatabaseListInsertSketchAt(original, 2, 300));
+        Submission.SrlExperiment secondSubmission = getFakeExperiment("User1", secondList.build());
+        String secondId = DatabaseClient.saveExperiment(secondSubmission, 200, client);
 
-        newList.addList(saveUpdate);
-        return newList.build();
+        Assert.assertEquals(null, secondId);
+        // get experiment
+        Submission.SrlExperiment result = DatabaseClient.getExperiment(id, client);
+        Assert.assertEquals(secondSubmission, result);
+    }
+
+    @Test
+    public void testTextSubmissionSavesCorrectly() throws DatabaseAccessException {
+
+        // once you have a DB instance, you can interact with it
+        // just like you would with a real one.
+        DB db = fongoRule.getDB();
+        DatabaseClient client = getMockedVersion(db);
+
+        final String textAnswer = "TEXT ANSWER";
+
+        // round 1
+        Submission.SrlSubmission.Builder build = Submission.SrlSubmission.newBuilder();
+        build.setTextAnswer(textAnswer);
+        Submission.SrlExperiment expected = getFakeExperiment("User1", build.build());
+        String id = DatabaseClient.saveExperiment(expected, 200, client);
+
+        // get experiment
+        Submission.SrlExperiment result = DatabaseClient.getExperiment(id, client);
+        String resultAnswer = result.getSubmission().getTextAnswer();
+
+        Assert.assertEquals(textAnswer, resultAnswer);
+    }
+
+    @Test
+    public void testTextSubmissionUpdatesCorrectly() throws DatabaseAccessException {
+
+        // once you have a DB instance, you can interact with it
+        // just like you would with a real one.
+        DB db = fongoRule.getDB();
+        DatabaseClient client = getMockedVersion(db);
+
+        final String textAnswer = "TEXT ANSWER";
+        final String textAnswer2 = "TEXT ANSWER2";
+
+        // round 1
+        Submission.SrlSubmission.Builder build = Submission.SrlSubmission.newBuilder();
+        build.setTextAnswer(textAnswer);
+        Submission.SrlExperiment expected = getFakeExperiment("User1", build.build());
+        String id = DatabaseClient.saveExperiment(expected, 200, client);
+
+        // round 2
+        Submission.SrlSubmission.Builder secondList = Submission.SrlSubmission.newBuilder();
+        secondList.setTextAnswer(textAnswer2);
+        Submission.SrlExperiment secondSubmission = getFakeExperiment("User1", secondList.build());
+        String secondId = DatabaseClient.saveExperiment(secondSubmission, 200, client);
+
+        // get experiment
+        Submission.SrlExperiment result = DatabaseClient.getExperiment(id, client);
+        String resultAnswer = result.getSubmission().getTextAnswer();
+
+        Assert.assertEquals(textAnswer2, resultAnswer);
     }
 
     public Submission.SrlExperiment getFakeExperiment(String userId, Submission.SrlSubmission sub) {
