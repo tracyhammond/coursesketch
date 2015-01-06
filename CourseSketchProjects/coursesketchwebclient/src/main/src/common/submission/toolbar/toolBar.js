bc7b38d6adf48158e7895c71e039cdd5d7555d42
dtracers <dtracers@gmail.com>
2015-01-06 03:32:46 -0600

    Added css and fixed issues.
    It appears that this problem toolbar is ready to be added for use.

--- CourseSketchProjects/coursesketchwebclient/src/main/src/common/submission/toolbar/toolBar.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/common/submission/toolbar/toolBar.js
@@ -11,19 +11,19 @@ function ProblemToolBar() {
     };
 }
 
-ToolBar.prototype.setSubmitCallback = function(submitCallback) {
+ProblemToolBar.prototype.setSubmitCallback = function(submitCallback) {
     this.shadowRoot.querySelect("#submission").onclick = submitCallback;
 };
 
-ToolBar.prototype.setSaveCallback = function(saveCallback) {
+ProblemToolBar.prototype.setSaveCallback = function(saveCallback) {
     this.shadowRoot.querySelect("#save").onclick = saveCallback;
 };
 
-ToolBar.prototype.setUndoCallback = function(undoCallback) {
+ProblemToolBar.prototype.setUndoCallback = function(undoCallback) {
     this.shadowRoot.querySelect("#undo").onclick = undoCallback;
 };
 
-ToolBar.prototype.setRedoCallback = function(redoCallback) {
+ProblemToolBar.prototype.setRedoCallback = function(redoCallback) {
     this.shadowRoot.querySelect("#redo").onclick = redoCallback;
 };
 
@@ -31,11 +31,13 @@ ToolBar.prototype.setRedoCallback = function(redoCallback) {
  * Returns an image element that can be added to the tool bar.
  * It will have the custom load functions and click functions
  */
-ToolBar.prototype.createButton = function(imgLocation, onclickFunction, onloadFunction) {
+ProblemToolBar.prototype.createButton = function(imgLocation, onclickFunction, onloadFunction) {
     var element = document.createElement("img");
     element.src = img;
     element.onclick = onclickFunction;
     element.onload = onloadFunction;
     element.className = ".specific_button";
     return createButton;
-}
+};
+
+ProblemToolBar.prototype = Object.create(HTMLDialogElement.prototype);
