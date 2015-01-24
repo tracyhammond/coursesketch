dfd55aee54d3c8b5bdf96f1a19b4fe0b4cf1620d
dtracers <dtracers@gmail.com>
2015-01-23 19:15:30 -0600

    Adding new assignment.

--- CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/homePage/create.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/homePage/create.js
@@ -23,6 +23,30 @@
         CourseSketch.dataManager.updateCourse(srlCourse);
     };
 
+    /**
+     * Function to be called when a lecture has finished editing.
+     *
+     * @param attributeChanged
+     *            the name of the protobuf attribute that changed
+     * @param oldValue
+     *            the attribute's old value
+     * @param newValue
+     *            the attribute's new value
+     * @param element
+     *            protobuf element that has been edited
+     */
+    courseManagement.assignmentEndEdit = function(attributeChanged, oldValue, newValue, element) {
+        var keyList = newValue.keys();
+        var srlCourse = element.schoolItemData;
+        console.log(srlCourse);
+        for (var key of keyList) {
+            console.log(key);
+            srlCourse[key] = newValue.get(key);
+        }
+        console.log(srlCourse);
+        CourseSketch.dataManager.updateAssignment(srlCourse);
+    };
+
     courseManagement.commonShowCourses = courseManagement.showCourses;
 
     /**
@@ -40,6 +64,27 @@
             var schoolItem = children[i];
             if (schoolItem.nodeName == "SCHOOL-ITEM") {
                 schoolItem.setEditCallback(courseManagement.courseEndEdit);
+            }
+        }
+    };
+
+    courseManagement.commonShowAssignments = courseManagement.showAssignments;
+
+    /**
+     * Overwrote the old show courses to add some edit capabilities.
+     */
+    courseManagement.showAssignments = function(courseList) {
+        courseManagement.commonShowAssignments(courseList);
+        var children = document.getElementById('assignment_list_column').children;
+        if (children.length <= 0) {
+            hideButton("problem_button")
+        } else {
+            showButton("problem_button");
+        }
+        for (var i = 0; i < children.length; i++) {
+            var schoolItem = children[i];
+            if (schoolItem.nodeName == "SCHOOL-ITEM") {
+                schoolItem.setEditCallback(courseManagement.assignmentEndEdit);
             }
         }
     };
@@ -79,8 +124,45 @@
                 courseManagement.showCourses(localCourseList);
             });
         });
-    }
+    };
 
+    /**
+     * Creates a new assignment with default values.
+     * and adds it to the database.
+     */
+    courseManagement.addNewAssignment = function addNewAssignment() {
+        var course = document.querySelector("#class_list_column .selectedBox");
+        var waitingIcon = CourseSketch.courseManagement.waitingIcon;
+        document.getElementById('assignment_list_column').appendChild(waitingIcon);
+        CourseSketch.courseManagement.waitingIcon.startWaiting();
+        // by instructors
+        var assignment = CourseSketch.PROTOBUF_UTIL.SrlAssignment();
+        // course.id = "Course_01";
+        assignment.name = "Insert name";
+        assignment.description = "Insert description";
+        // course.semester = "Should be in format: '_F13' (_F = Fall, Sp =
+        // Spring, Su = Summer) ";
+        // course.accessDate = "mm/dd/yyyy";
+        // course.closeDate = "mm/dd/yyyy";
+        CourseSketch.dataManager.getAllCourses(function(courseList) {
+            var localCourseList = courseList;
+            if (courseList instanceof CourseSketch.DatabaseException) {
+                // we are cool because we are adding a new one.
+                localCourseList = [];
+            }
+            var firstCourse = undefined;
+            CourseSketch.dataManager.insertCourse(course, function(course) {
+                firstCourse = course;
+                localCourseList.push(course);
+                courseManagement.showCourses(localCourseList);
+            }, function(course) {
+                if (waitingIcon.isRunning()) {
+                    waitingIcon.finishWaiting();
+                }
+                courseManagement.showCourses(localCourseList);
+            });
+        });
+    };
 
     /**
      * sets an element (should be a button) with the given id to be visible.
