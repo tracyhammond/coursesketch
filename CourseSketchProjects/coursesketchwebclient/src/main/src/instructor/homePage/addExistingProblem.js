25f7d915155ef15e6d495ffb39116cb6181b8736
dtracers <dtracers@gmail.com>
2015-01-30 22:45:34 -0600

    Started work on the creation of existing problems.
    Former-commit-id: fc8cd833062a524cb236e371076c328c937ef9b8

---
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/homePage/addExistingProblem.js
@@ -0,0 +1,9 @@
+validateFirstRun(document.currentScript);
+
+(function() {
+    var courseManagement = CourseSketch.courseManagement;
+
+    CourseSketch.courseManagement.chooseExistingProblem = function() {
+        var dialog = document.createElement('dialog');
+    };
+})();
