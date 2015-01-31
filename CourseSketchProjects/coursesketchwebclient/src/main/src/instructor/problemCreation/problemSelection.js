9518b615a6e98422c68ef4a5cfd07b42428031be
dtracers <dtracers@gmail.com>
2015-01-30 22:45:34 -0600

    Started work on the creation of existing problems.

---
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/problemCreation/problemSelection.js
@@ -0,0 +1,16 @@
+function ProblemSelectionPanel() {
+    // if you are an admin of the assignment we will allow you to view problems.
+    this.loadProblems = function(courseId, assignmentId) {
+        CourseSketch.PROTOBUF_UTIL.
+    };
+}
+
+ProblemSelectionPanel.prototype = setCanceledCallback = function(callback) {
+    this.canceledCallback = callback;
+};
+
+ProblemSelectionPanel.prototype = setAcceptCallback = function(callback) {
+    this.acceptCallback = callback;
+};
+
+ProblemSelectionPanel.prototype = Object.create(HTMLDialogElement.prototype);
