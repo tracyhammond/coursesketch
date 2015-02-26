d84b806ba039f71da225b8600260e2be361fd3f8
neaboo822 <ss1234534@hotmail.com>
2015-02-26 12:29:32 -0600

    Buttons on PlaybackTag
    added buttons pictures

--- CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/templates/voicePlayback/voicePlayback.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/templates/voicePlayback/voicePlayback.js
@@ -3,7 +3,17 @@ function VoicePlayback() {
         var localScope = this;
         shadowRoot = this.createShadowRoot();
         shadowRoot.appendChild(templateClone);
+
+        this.shadowRoot.querySelector("#play-btn").onclick = function(){
+            localScope.shadowRoot.querySelector("#pause-btn").style.display = "block";
+            localScope.shadowRoot.querySelector("#play-btn").style.display = "none";
+        }
+        this.shadowRoot.querySelector("#pause-btn").onclick = function(){
+            localScope.shadowRoot.querySelector("#play-btn").style.display = "block";
+            localScope.shadowRoot.querySelector("#pause-btn").style.display = "none";
+        }
     }
+
 }
 
 VoicePlayback.prototype = Object.create(HTMLElement.prototype);
