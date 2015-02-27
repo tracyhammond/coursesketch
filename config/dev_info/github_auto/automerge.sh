c6f46a0484152b9f2a3b397244bdce518a88317a
mattrunyon <mattrunyon@tamu.edu>
2015-02-26 22:42:46 -0600

    Update script to return user to their original branch and apply their stash
    Former-commit-id: 312319a6864d696977962ab61be8d145b438214b

--- config/dev_info/github_auto/automerge.sh
+++ config/dev_info/github_auto/automerge.sh
@@ -1,11 +1,13 @@
 git stash
-git checkout mergeScript
+oldbranch=$(git rev-parse --abbrev-ref HEAD)
+git checkout master
 git pull
 branches="branchNames"
 while IFS= read -r line
 do
 git checkout "$line"
-git merge mergeScript
+git merge master
 git push origin "$line"
 done <"$branches"
-git checkout mergeScript
+git checkout "$oldbranch"
+git stash apply
