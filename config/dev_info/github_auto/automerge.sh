70dd66f96b08719743ef59cfafa336976c8f9254
dtracers <dtracers@gmail.com>
2015-02-26 18:16:29 -0600

    Updated the merge script

--- config/dev_info/github_auto/automerge.sh
+++ config/dev_info/github_auto/automerge.sh
@@ -2,9 +2,10 @@ git stash
 git checkout mergeScript
 git pull
 branches="branchNames"
+cd ..
 while IFS= read - r line
 do
 git checkout "$line"
-git merge master
+git merge mergeScript
 done <"$file"
-git checkout serverIpAddresses
+git checkout mergeScriptTest
