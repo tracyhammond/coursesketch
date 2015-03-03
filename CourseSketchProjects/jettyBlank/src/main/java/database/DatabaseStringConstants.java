cd1da325b232000436e54cfe1c564e64f9a15fba
hounds34 <hounds34@tamu.edu>
2015-03-03 12:03:09 -0600

    Updated Proto & DB for script
    Bank problem script added in school.proto , appropriate DB added except
    update
    
    
    Former-commit-id: 8d2da68557319dffcbbac9e4538de80bb169e7c5

--- CourseSketchProjects/jettyBlank/src/main/java/database/DatabaseStringConstants.java
+++ CourseSketchProjects/jettyBlank/src/main/java/database/DatabaseStringConstants.java
@@ -103,6 +103,7 @@ public final class DatabaseStringConstants {
     public static final String SOURCE = "Source";
     public static final String QUESTION_TYPE = "QuestionType";
     public static final String KEYWORDS = "OtherKeywords";
+    public static final String SCRIPT = "Script";
 
     // state specific
     public static final String STATE_PUBLISHED = "Published"; // Instructor has finished construction of school item
