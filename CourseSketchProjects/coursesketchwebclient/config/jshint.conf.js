571aa7064a346ec3e2b00152d9d4c1afd8146771
dtracers <dtracers@gmail.com>
2015-03-03 14:46:52 -0600

    Got check style on javascript!
    The actual errors themselves will be fixed in a different branch

---
+++ CourseSketchProjects/coursesketchwebclient/config/jshint.conf.js
@@ -0,0 +1,20 @@
+{
+  // Options
+  "maxparams": 3,
+  "indent": true,
+  "camelcase": true,
+  "eqeqeq": true,
+  "forin": true,
+  "immed": true,
+  "latedef": true,
+  "noarg": true,
+  "noempty": true,
+  "nonew": true,
+
+  /*
+   * Globals
+   */
+  "globals": {
+     "$": false
+  }
+}
