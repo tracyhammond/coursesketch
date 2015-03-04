fcea58fef74c0f01334dea0083f422bfafa21d64
hounds34 <hounds34@tamu.edu>
2015-03-03 20:07:43 -0600

    Script Execution Baseline functionality
    Added function signature for executeScript (does nothing) in
    scriptExecutor.js
    Added call to this function in mvGrading, experiment.
    scripExecutor.js loaded by mvgrading.html, experiment.html

---
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/common/scriptExecution/scriptExecutor.js
@@ -0,0 +1,11 @@
+validateFirstRun(document.currentScript);
+
+/**
+ * This function parses and executes the script passed in
+ * @param script
+ *        {string}
+ */
+
+function executeScript(script) {
+    //todo: design and code functionality
+}
--- CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/review/mvGrading.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/review/mvGrading.js
@@ -125,7 +125,9 @@ validateFirstRun(document.currentScript);
         }
 
         var problemScript = navigator.getProblemScript();
-        // todo: run the script
+        if (problemScript != "") {
+            executeScript(problemScript);
+        }
 
         parentPanel.problemIndex = navigator.getCurrentNumber();
         parentPanel.setProblemType(problemType);
