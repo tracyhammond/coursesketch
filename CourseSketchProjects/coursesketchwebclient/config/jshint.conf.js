8e12e703720c3f6124dd401b9dc16ca37cffa72c
mattrunyon <mattrunyon@tamu.edu>
2015-03-04 17:55:33 -0600

    Added suppression of errors we don't care about. Specifically initializing a variable to undefined (it doesn't like this. We do) and defining functions in a block (again it doesn't like this. We need it for some parts)

--- CourseSketchProjects/coursesketchwebclient/config/jshint.conf.js
+++ CourseSketchProjects/coursesketchwebclient/config/jshint.conf.js
@@ -1,4 +1,5 @@
 {
+  // Look at http://jslinterrors.com/ for more details on errors and to find how to suppress certain errors
   // Options
   "maxparams": 3,
   "indent": false,
@@ -16,6 +17,8 @@
   "unused" : true,
   "maxlen" : "150",
   "maxcomplexity" : "10",
+  "-W080" : false, // Suppresses "there is no need to initialize a variable to undefined" error
+  "-W082" : false, // Suppresses "functions should not be defined in a block" error
 
   /*
    * Globals
