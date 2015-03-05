4c5739cd2f52b4cc858df9a9f566715e816b5a1c
dtracers <dtracers@gmail.com>
2015-03-05 03:10:33 -0600

    Finished adding jscs
    Former-commit-id: 14c3a1c54d8b48bfb13bb7ad7b1a8b0c433a9368

--- CourseSketchProjects/coursesketchwebclient/Gruntfile.js
+++ CourseSketchProjects/coursesketchwebclient/Gruntfile.js
@@ -9,19 +9,21 @@ module.exports = function(grunt) {
         jshint: {
             options: {
                 jshintrc: 'config/jshint.conf.json',
-                ignores : ['src/main/src/utilities/libraries/**/*.js', 'src/test/src/testUtilities/**/*.js'],
+                ignores: [ 'src/main/src/utilities/libraries/**/*.js', 'src/test/src/testUtilities/**/*.js' ],
                 globals: {
                     module: true
                 },
                 reporter:'jslint',
                 reporterOutput: 'target/jshint.xml'
             },
-            files: ['Gruntfile.js', 'src/main/src/**/*.js','src/test/src/**/*.js']
+            files: [ 'Gruntfile.js', 'src/main/src/**/*.js', 'src/test/src/**/*.js', '!src/main/src/utilities/libraries/**/*.js',
+                    '!src/test/src/testUtilities/**/*.js' ]
         },
         jscs: {
-            src: "<%= jshint.files %>",
+            src: '<%= jshint.files %>',
             options: {
                 config: 'config/jscs.conf.json',
+                reporterOutput: 'target/jscsReport.txt'
             }
         },
         connect: {
@@ -31,24 +33,24 @@ module.exports = function(grunt) {
                 debug: true
             },
             rules: [
-               {from: '^/src/(?!test)(.*)$', to: '/src/main/src/$1'},
-               {from: '^/test(.*)$', to: '/src/test/src$1', redirect: "permanent"},
-               {from: '^/other(.*)$', to: 'src/main/resources/other/$1'}
+               { from: '^/src/(?!test)(.*)$', to: '/src/main/src/$1' },
+               { from: '^/test(.*)$', to: '/src/test/src$1', redirect: "permanent" },
+               { from: '^/other(.*)$', to: 'src/main/resources/other/$1' }
             ],
             development: {
                 options: {
-                    middleware: function (connect, options) {
+                    middleware: function(connect, options) {
                         var middlewares = [];
 
                         // RewriteRules support
                         middlewares.push(rewriteRulesSnippet);
 
                         if (!Array.isArray(options.base)) {
-                            options.base = [options.base];
+                            options.base = [ options.base ];
                         }
 
                         var directory = options.directory || options.base[options.base.length - 1];
-                        options.base.forEach(function (base) {
+                        options.base.forEach(function(base) {
                             // Serve static files.
                             middlewares.push(connect.static(base));
                         });
@@ -66,12 +68,12 @@ module.exports = function(grunt) {
                 httpBase: 'http://localhost:9001',
                 timeout: 2000
             },
-            all: ['src/test/src/**/*Test.html']
+            all: [ 'src/test/src/**/*Test.html' ]
         },
     });
 
     // sets up tasks relating to starting the server
-    grunt.registerTask('server', function (target) {
+    grunt.registerTask('server', function(target) {
         grunt.task.run([
             'configureRewriteRules',
             'connect:development'
@@ -79,11 +81,19 @@ module.exports = function(grunt) {
     });
 
     // sets up tasks related to testing
-    grunt.registerTask('test', function (target) {
-            grunt.task.run([
-                'server',
-                'qunit'
-            ]);
-        });
-    grunt.registerTask('default', ['jscs', 'test', 'jshint']);
+    grunt.registerTask('test', function(target) {
+        grunt.task.run([
+            'server',
+            'qunit'
+        ]);
+    });
+
+    // sets up tasks related to checkstyle
+    grunt.registerTask('checkstyle', function(target) {
+        grunt.task.run([
+            'jscs',
+            'jshint'
+        ]);
+    });
+    grunt.registerTask('default', [ 'checkstyle', 'test' ]);
 };
