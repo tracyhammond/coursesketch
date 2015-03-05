0aeac401afbeba40d5e893fc10b602ca59b4b913
dtracers <dtracers@gmail.com>
2015-03-05 00:19:19 -0600

    getting server to run

--- CourseSketchProjects/coursesketchwebclient/Gruntfile.js
+++ CourseSketchProjects/coursesketchwebclient/Gruntfile.js
@@ -1,4 +1,7 @@
 module.exports = function(grunt) {
+    grunt.loadNpmTasks("grunt-jscs");
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-connect-rewrite');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.initConfig({
@@ -7,15 +10,60 @@ module.exports = function(grunt) {
                 jshintrc: 'config/jshint.conf.js',
                 ignores : ['src/main/src/utilities/libraries/**/*.js', 'src/test/src/testUtilities/**/*.js'],
                 globals: {
-                  module: true
-                }
+                    module: true
+                },
+                reporter:'jslint',
+                reporterOutput: 'target/jshint.xml'
             },
-            //files: ['Gruntfile.js', 'src/main/src/**/*.js','src/test/src/**/*.js'],
-            files: ['Gruntfile.js']
+            files: ['Gruntfile.js', 'src/main/src/**/*.js','src/test/src/**/*.js']
+        },
+        jscs: {
+            src: "src/main/src/**/*.js",
+            options: {
+                config: "config/jscs.conf.js",
+            }
+        },
+        connect: {
+            options: {
+                port: 9001,
+                hostname: 'localhost',
+                debug: true
+            },
+            rules: [
+                // Internal rewrite
+                {from: '^/src/(?!test)(.*)$', to: '/src/main/src/$1'},
+                // Internal rewrite
+                {from: '^/test/(.*)$', to: 'src/test/src/$1'},
+                // Internal rewrite
+                {from: '^/other(.*)$', to: 'src/main/resources/other/$1'}
+            ],
         },
         qunit: {
-            files: ['src/test/src/**/*.html']
-        }
+            options: {
+                httpBase: 'http://localhost:9001',
+                urls: [
+                  'http://localhost:9001/test/instructor/homePage/courseManagementTest.html',
+                  'http://localhost:9001/test/instructor/homePage/NOURL.html',
+                ]
+            },
+            all: ['src/test/src/**/*.html']
+        },
     });
-    grunt.registerTask('default', ['jshint', 'qunit']);
+
+    // sets up tasks relating to starting the server
+    grunt.registerTask('server', function (target) {
+        grunt.task.run([
+            'configureRewriteRules',
+            'connect'
+        ]);
+    });
+
+    // sets up tasks related to testing
+    grunt.registerTask('test', function (target) {
+            grunt.task.run([
+                'server',
+                'qunit'
+            ]);
+        });
+    grunt.registerTask('default', ['test', 'jshint']);
 };
---
+++ CourseSketchProjects/coursesketchwebclient/config/jscs.conf.js
@@ -0,0 +1,30 @@
+{
+    "requireCurlyBraces": [ "if", "else", "for", "while", "do" ],
+    "requireSpaceAfterKeywords": [ "if", "else", "for", "while", "do", "switch"],
+    "requireSpacesInFunctionExpression": {
+        "beforeOpeningCurlyBrace": true
+    },
+    "disallowSpacesInFunctionExpression": {
+        "beforeOpeningRoundBrace": true
+    },
+    "requireMultipleVarDecl": true,
+    "requireSpacesInsideObjectBrackets": "all",
+    "requireSpacesInsideArrayBrackets": "all",
+    "disallowLeftStickedOperators": [ "?", "-", "/", "*", "=", "==", "===", "!=", "!==", ">", ">=", "<", "<=" ],
+    "disallowRightStickedOperators": [ "?", "/", "*", ":", "=", "==", "===", "!=", "!==", ">", ">=", "<", "<="],
+    "requireSpaceBeforeBinaryOperators": ["+", "-", "/", "*", "=", "==", "===", "!=", "!=="],
+    "requireSpaceAfterBinaryOperators": ["+", "-", "/", "*", "=", "==", "===", "!=", "!=="],
+    "disallowSpaceAfterPrefixUnaryOperators": ["++", "--", "+", "-"],
+    "disallowSpaceBeforePostfixUnaryOperators": ["++", "--"],
+    "requireRightStickedOperators": [ "!" ],
+    "requireLeftStickedOperators": [ "," ],
+    "disallowKeywords": [ "with" ],
+    "disallowMultipleLineBreaks": true,
+    "disallowKeywordsOnNewLine": [ "else" ],
+    "requireLineFeedAtFileEnd": true,
+    "disallowSpaceAfterObjectKeys": true,
+    "validateLineBreaks": "LF",
+
+
+    "excludeFiles": ["src/main/src/utilities/libraries/**", "src/test/src/testUtilities/**"]
+}
