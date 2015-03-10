e611c343d0eac42b598b8490069ff65c8169fde4
Raegan Lucero <raegano@tamu.edu>
2015-03-10 11:59:55 -0500

    Got playback working!
    Former-commit-id: b17e669996ec422583528708f166473aa5595231

--- CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/templates/voicePlayback/voicePlayback.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/templates/voicePlayback/voicePlayback.js
@@ -3,14 +3,33 @@ function VoicePlayback() {
         var localScope = this;
         shadowRoot = this.createShadowRoot();
         shadowRoot.appendChild(templateClone);
+        var surface = document.body.querySelector("sketch-surface");
+        var graphics = surface.graphics;
+        var updateManager = surface.getUpdateManager();
 
         this.shadowRoot.querySelector("#play-btn").onclick = function(){
             localScope.shadowRoot.querySelector("#pause-btn").style.display = "block";
             localScope.shadowRoot.querySelector("#play-btn").style.display = "none";
+            playMe();
         }
         this.shadowRoot.querySelector("#pause-btn").onclick = function(){
             localScope.shadowRoot.querySelector("#play-btn").style.display = "block";
             localScope.shadowRoot.querySelector("#pause-btn").style.display = "none";
+        }
+
+        function playMe() {
+            var graphics = surface.graphics;
+            var updateList = surface.getUpdateList();
+            var copyList = [];
+            for (var i = 0; i < updateList.length; i++) {
+                copyList.push(updateList[i]);
+            }
+            var updateManager = surface.getUpdateManager();
+            updateManager.clearUpdates(false);
+
+            var playBack = new Playback(copyList, updateManager, graphics);
+            updateManager.addPlugin(playBack);
+            playBack.playNext();
         }
     }
 
