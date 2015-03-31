0dc0b6f9b2cd0f049025df94b468c63051b68470
hounds34 <hounds34@tamu.edu>
2015-03-31 12:45:39 -0500

    Setup script execution basics
    Untested, committing so that I can merge in from master before i
    continue
    
    
    Former-commit-id: 846e0400e5856c9a328d807a34e52725727f9226

--- CourseSketchProjects/coursesketchwebclient/src/main/src/common/scriptExecution/scriptExecutor.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/common/scriptExecution/scriptExecutor.js
@@ -1,11 +1,31 @@
 validateFirstRun(document.currentScript);
 
 /**
- * This function parses and executes the script passed in
+ * This function parses and executes the script that is passed in.
  * @param script
  *        {string}
  */
 
 function executeScript(script) {
-    //todo: design and code functionality
+
+    var scriptWorker = new jailed.DynamicPlugin(script, api);
+}
+
+/**
+ * This var is passed to the jailed plugin to define what functions from this
+ *  script the plugin can call when executing 3rd party code.
+ */
+
+var api = {
+    debugLog: debugLog
+}
+
+/**
+ *This function adds the ability to call console.log() to the api
+ * @param text
+ *        {string}
+ */
+
+function debugLog(text) {
+    console.log(text);
 }
