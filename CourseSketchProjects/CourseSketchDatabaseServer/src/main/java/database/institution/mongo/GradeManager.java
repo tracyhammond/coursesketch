a1ac1254f320a36ce9441b132a03389ae9018dc3
mattrunyon <mattrunyon@tamu.edu>
2015-04-02 21:29:01 -0500

    Finished getAllGradesInstructor method
    Former-commit-id: 3f6cb647b03856c52c29e3dea352a037e3319c39

--- CourseSketchProjects/CourseSketchDatabaseServer/src/main/java/database/institution/mongo/GradeManager.java
+++ CourseSketchProjects/CourseSketchDatabaseServer/src/main/java/database/institution/mongo/GradeManager.java
@@ -10,24 +10,29 @@ import database.auth.AuthenticationException;
 import database.auth.Authenticator;
 import protobuf.srl.grading.Grading.GradeHistory;
 import protobuf.srl.grading.Grading.ProtoGrade;
+import protobuf.srl.utils.Util;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static database.DatabaseStringConstants.ASSIGNMENT_ID;
-import static database.DatabaseStringConstants.COMMENTS;
+import static database.DatabaseStringConstants.COMMENT;
 import static database.DatabaseStringConstants.COURSE_COLLECTION;
 import static database.DatabaseStringConstants.COURSE_ID;
 import static database.DatabaseStringConstants.COURSE_PROBLEM_ID;
 import static database.DatabaseStringConstants.CURRENT_GRADE;
+import static database.DatabaseStringConstants.EXTERNAL_GRADE;
+import static database.DatabaseStringConstants.GRADED_DATE;
 import static database.DatabaseStringConstants.GRADE_COLLECTION;
 import static database.DatabaseStringConstants.GRADE_HISTORY;
+import static database.DatabaseStringConstants.GRADE_VALUE;
 import static database.DatabaseStringConstants.USER_ID;
+import static database.DatabaseStringConstants.WHO_CHANGED;
 
 /**
  * Created by Matt on 3/29/2015.
  */
-public class GradeManager {
+public final class GradeManager {
 
     /**
      * Private constructor.
@@ -35,6 +40,24 @@ public class GradeManager {
     private GradeManager() {
     }
 
+    /**
+     * Gets all grades for a certain course.
+     *
+     * @param authenticator
+     *         The object that is performing authentication.
+     * @param dbs
+     *         The database that the grades are being retrieved from.
+     * @param courseId
+     *         The course that the grades are being retrieved for.
+     * @param userId
+     *         The user that is requesting the grades. Only users with admin access can get all grades.
+     * @return The list of ProtoGrades for the course. Each ProtoGrade is an individual assignment grade for an individual student.
+     *         Sorting should be done by whoever implements this method.
+     * @throws AuthenticationException
+     *         Thrown if the user did not have the authentication to get the grades.
+     * @throws DatabaseAccessException
+     *         Thrown if grades are not found in the database.
+     */
     public static List<ProtoGrade> getAllCourseGradesInstructor(final Authenticator authenticator, final DB dbs, final String courseId,
             final String userId) throws AuthenticationException, DatabaseAccessException {
         final DBCollection gradeCollection = dbs.getCollection(GRADE_COLLECTION);
@@ -52,36 +75,90 @@ public class GradeManager {
             throw new AuthenticationException(AuthenticationException.INVALID_PERMISSION);
         }
 
-        List<ProtoGrade> grades = new ArrayList<>();
+        final List<ProtoGrade> grades = new ArrayList<>();
         while (cursor.hasNext()) {
             grades.add(buildProtoGrade(cursor.next()));
         }
 
-
+        return grades;
     }
 
-    private static ProtoGrade buildProtoGrade(DBObject grade) {
+    /**
+     * @param grade
+     *         Mongo DBObject representing one assignment grade for one student.
+     * @return ProtoObject representing one assignment grade for one student.
+     * @throws DatabaseAccessException
+     *         Thrown if courseId or userId are not found in the DBObject.
+     */
+    private static ProtoGrade buildProtoGrade(final DBObject grade) throws DatabaseAccessException {
+        if (!grade.containsField(COURSE_ID)) {
+            throw new DatabaseAccessException("Missing required field: courseId");
+        }
+        if (!grade.containsField(USER_ID)) {
+            throw new DatabaseAccessException("Missing required field: userId");
+        }
+
         final ProtoGrade.Builder protoGrade = ProtoGrade.newBuilder();
         protoGrade.setCourseId(grade.get(COURSE_ID).toString());
         protoGrade.setUserId(grade.get(USER_ID).toString());
-        protoGrade.setAssignmentId(grade.get(ASSIGNMENT_ID).toString());
+
+        if (grade.containsField(ASSIGNMENT_ID)) {
+            protoGrade.setAssignmentId(grade.get(ASSIGNMENT_ID).toString());
+        }
+
         if (grade.containsField(COURSE_PROBLEM_ID)) {
             protoGrade.setProblemId(grade.get(COURSE_PROBLEM_ID).toString());
         }
-        protoGrade.setCurrentGrade((float) grade.get(CURRENT_GRADE));
-        if (grade.containsField(COMMENTS)) {
-            protoGrade.setComment(grade.get(COMMENTS).toString());
+
+        if (grade.containsField(CURRENT_GRADE)) {
+            protoGrade.setCurrentGrade((float) grade.get(CURRENT_GRADE));
         }
+
         if (grade.containsField(GRADE_HISTORY)) {
-            List<DBObject> history = (List<DBObject>) grade.get(GRADE_HISTORY);
+            final List<DBObject> history = (List<DBObject>) grade.get(GRADE_HISTORY);
             for (int i = 0; i < history.size(); i++) {
                 protoGrade.addGradeHistory(buildProtoGradeHistory(history.get(i)));
             }
         }
 
+        if (grade.containsField(EXTERNAL_GRADE)) {
+            protoGrade.setExternalGrade((boolean) grade.get(EXTERNAL_GRADE));
+        }
+
+        return protoGrade.build();
     }
 
-    private static GradeHistory buildProtoGradeHistory(DBObject history) {
+    /**
+     * @param history
+     *         Mongo DBObject representing grade history for one assignment grade for one user.
+     * @return ProtoObject representing the grade history for one assignment grade for one user.
+     * @throws DatabaseAccessException
+     *         Thrown if no grade history fields are found.
+     */
+    private static GradeHistory buildProtoGradeHistory(final DBObject history) throws DatabaseAccessException {
+        final GradeHistory.Builder protoHistory = GradeHistory.newBuilder();
+        if (history.containsField(GRADE_VALUE)) {
+            protoHistory.setGradeValue((float) history.get(GRADE_VALUE));
+        }
 
+        if (history.containsField(COMMENT)) {
+            protoHistory.setComment(history.get(COMMENT).toString());
+        }
+
+        if (history.containsField(GRADED_DATE)) {
+            protoHistory.setGradedDate((Util.DateTime) history.get(GRADED_DATE));
+        }
+
+        if (history.containsField(WHO_CHANGED)) {
+            protoHistory.setWhoChanged(history.get(WHO_CHANGED).toString());
+        }
+
+        // GetAllFields returns a map of fields that have values in the ProtoObject
+        // If the size of the map is 0, then no fields exist
+        if (protoHistory.getAllFields().size() == 0) {
+            throw new DatabaseAccessException("No fields found for gradeHistory object");
+        }
+
+        return protoHistory.build();
     }
 }
