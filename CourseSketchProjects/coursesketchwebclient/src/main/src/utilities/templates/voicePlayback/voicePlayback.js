7d48e018d089f2afcedb0210d993eeb935505b46
timardis <timardis@neo.tamu.edu>
2015-04-14 13:45:53 -0500

    First playback attempt

--- CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/templates/voicePlayback/voicePlayback.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/templates/voicePlayback/voicePlayback.js
@@ -6,11 +6,14 @@ function VoicePlayback() {
 
         var vid = this.shadowRoot.querySelector('#myaudio');
         vid.src = '/src/utilities/templates/voicePlayback/test.mp3';
-        vid.ontimeupdate = function() {myFunction()};
+        vid.ontimeupdate = function() { 
+            myFunction()
+        }
+
         vid.onplay = function() {
                                     playMe();
                                     playVoice();
-                                }
+        }
 
         var surface = document.body.querySelector('sketch-surface');
         var graphics = surface.graphics;
@@ -57,11 +60,58 @@ function VoicePlayback() {
             });
 
         function playVoice() {
-            localScope.audio.play();
+            this.stopRecording();
+            //localScope.audio.play();
         }
 
         function pauseVoice() {
             localScope.audio.pause();
+        }
+
+        this.startRecording = function() {
+            this.recorder.record();
+            console.log('Recording...');
+        }
+
+        this.stopRecording = function() {
+            this.recorder.stop();
+            console.log('Stopped recording.');
+
+            this.saveFile();
+        }
+
+        this.startUserMedia = function(stream) {
+            this.recorder = new Recorder(stream);
+            console.log('Recorder initialized.');
+
+            this.startRecording();
+        }
+
+        this.saveFile = function() {
+            this.recorder.exportMP3(function(blob, mp3name) {
+            });
+        }
+        init = function() {
+            try {
+                window.AudioContext = window.AudioContext || window.webkitAudioContext;
+                navigator.getUserMedia = (navigator.getUserMedia ||
+                                          navigator.webkitGetUserMedia ||
+                                          navigator.mozgetUserMedia ||
+                                          navigator.msGetUserMedia);
+                window.URL = window.URL || window.webkitURL;
+
+                console.log('Audio context set up');
+                console.log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not available'));
+            } catch (e) {
+                alert('No web audio support in this browser');
+            }
+
+            navigator.getUserMedia({ audio: true }, function(stream) {
+                localScope.recorder = new Recorder(stream);
+                console.log('Recorder initialized.');
+            }, function(e) {
+                console.log('No live audio input: ' + e);
+            });
         }
 
     };
