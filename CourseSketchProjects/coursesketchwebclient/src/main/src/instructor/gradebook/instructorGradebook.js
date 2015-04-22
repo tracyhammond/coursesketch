54288283009b7a0c73d41454281dfea2f68aa354
Gilberto <garizpe83@gmail.com>
2015-04-21 20:04:19 -0500

    Gradebook selection page
    Completed the gb selection fake page

--- CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/gradebook/instructorGradebook.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/gradebook/instructorGradebook.js
@@ -1,21 +1,18 @@
-CourseSketch.gradeBook.addCells = function (numberStudents,numberAssignments,table){
+CourseSketch.gradeBook.addCells = function(numberStudents, numberAssignments, table) {
     for (var i = 0; i < numberStudents; i++){
-        
-        var row = document.createElement("tr");
-        if (i === 0) 
-            {
-                for (var t = 0; t < numberAssignments; t++){
-                var title = document.createElement("th");
+        var row = document.createElement('tr');
+        if (i === 0) {
+            for (var t = 0; t < numberAssignments; t++) {
+                var title = document.createElement('th');
                 title.textContent = i;
-                table.appendChild(title); 
-                }
+                table.appendChild(title);
             }
-        table.appendChild(row);
-        for (var j = 0; j < numberAssignments; j++){  
-            var column = document.createElement("td");
-            column.textContent = [];
-            row.appendChild(column); 
-             
         }
-    }    
-}
+        table.appendChild(row);
+        for (var j = 0; j < numberAssignments; j++) {
+            var column = document.createElement('td');
+            column.textContent = [];
+            row.appendChild(column);
+        }
+    }
+};
