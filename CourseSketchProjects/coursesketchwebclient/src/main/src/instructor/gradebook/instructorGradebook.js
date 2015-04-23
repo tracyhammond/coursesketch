e226c4ce4914448d2dac76fc8f698d464d2c2a4a
Gabster13 <gabster_13@tamu.edu>
2015-04-23 12:57:28 -0500

    My final commit
    Thanks for everything, everyone. I will be back. Here's the last stuff I
    worked on for the gradebook.

--- CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/gradebook/instructorGradebook.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/gradebook/instructorGradebook.js
@@ -1,21 +1,23 @@
-CourseSketch.gradeBook.addCells = function (numberStudents,numberAssignments,table){
-    for (var i = 0; i < numberStudents; i++){
-        
-        var row = document.createElement("tr");
-        if (i === 0) 
-            {
-                for (var t = 0; t < numberAssignments; t++){
-                var title = document.createElement("th");
-                title.textContent = i;
-                table.appendChild(title); 
-                }
-            }
+CourseSketch.gradeBook.addCells = function (numberStudents,numberAssignments,table) {
+    //This code generates columns and rows in a test instructor gradebook.
+    for (var i = 0; i < numberStudents; i++) {
+        var  row = document.createElement("tr");
         table.appendChild(row);
-        for (var j = 0; j < numberAssignments; j++){  
-            var column = document.createElement("td");
-            column.textContent = [];
-            row.appendChild(column); 
-             
+        for (var j = 0; j < numberAssignments; j++) {
+            var column = undefined;
+            if (i === 0) {
+                var column = document.createElement("th");
+                var innerElement = document.createElement('div');
+                var text = "Assignment" + " " + j;
+                innerElement.textContent = text;
+                column.appendChild(innerElement);
+            } else {
+                var column = document.createElement("td");
+                var innerElement = document.createElement('div');
+                column.textContent = [];
+                column.appendChild(innerElement);
+            }
+            row.appendChild(column);
         }
-    }    
+    }
 }
