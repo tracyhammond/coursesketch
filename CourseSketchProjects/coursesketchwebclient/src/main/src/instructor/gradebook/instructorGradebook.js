e84f4c9e4e22537b3c9ff8ee3f13716015a85c57
Gilberto <garizpe83@gmail.com>
2015-04-30 13:25:21 -0500

    Added java docs

--- CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/gradebook/instructorGradebook.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/gradebook/instructorGradebook.js
@@ -1,6 +1,9 @@
     /**
-     * Called when opening instructorGradebook page
-     * This creates a table with # of assignments = # of columns and # of students = # of rows
+     * This creates a table with # of assignments = # of columns and # of students = # of rows.
+     *
+     * @param {Integer} numberStudents The number of students.
+     * @param {Integer} numberAssignments The number of assignments.
+     * @param {Element} table The table that is displaying students and assignments.
      */
 CourseSketch.gradeBook.addCells = function(numberStudents, numberAssignments, table) {
     for (var i = 0; i < numberStudents; i++){
