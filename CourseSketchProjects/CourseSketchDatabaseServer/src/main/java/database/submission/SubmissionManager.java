b4bdbfa5585307b06adc5f4a865cc4c48eee5684
dtracers <dtracers@gmail.com>
2015-05-03 22:00:00 -0500

    submissions now use the new format as well
    So when getting a list of experiments it now uses the new format which
    is awesome.
    This also makes paging easier.
    
    
    Former-commit-id: 171c006e3907a0d5fb012e75642fa5779d6257aa

--- CourseSketchProjects/CourseSketchDatabaseServer/src/main/java/database/submission/SubmissionManager.java
+++ CourseSketchProjects/CourseSketchDatabaseServer/src/main/java/database/submission/SubmissionManager.java
@@ -12,18 +12,17 @@ import database.DatabaseAccessException;
 import database.auth.AuthenticationException;
 import database.auth.Authenticator;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import protobuf.srl.query.Data.DataRequest;
 import protobuf.srl.query.Data.ItemQuery;
 import protobuf.srl.query.Data.ItemRequest;
 import protobuf.srl.request.Message.Request;
 import protobuf.srl.request.Message.Request.MessageType;
 import utilities.ConnectionException;
+import utilities.LoggingConstants;
 
 import java.util.ArrayList;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import utilities.LoggingConstants;
 
 import static database.DatabaseStringConstants.ADMIN;
 import static database.DatabaseStringConstants.COURSE_PROBLEM_COLLECTION;
@@ -173,6 +172,10 @@ public final class SubmissionManager {
         build.setQuery(ItemQuery.EXPERIMENT);
         final DBRef myDbRef = new DBRef(dbs, EXPERIMENT_COLLECTION, new ObjectId(problemId));
         final DBObject corsor = myDbRef.fetch();
+
+        if (corsor == null) {
+            throw new DatabaseAccessException("Students have not submitted any data for this problem: " + problemId);
+        }
         for (String key : corsor.keySet()) {
             if (SELF_ID.equals(key)) {
                 continue;
