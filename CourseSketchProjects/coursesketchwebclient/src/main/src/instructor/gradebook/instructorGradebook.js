c6f9bdfab8a509436d1e991a6ccfea130fe9b640
Gilberto <garizpe83@gmail.com>
2015-05-05 22:58:16 -0500

    style error

--- CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/gradebook/instructorGradebook.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/gradebook/instructorGradebook.js
@@ -1,10 +1,10 @@
-    /**
-     * This creates a table with # of assignments = # of columns and # of students = # of rows.
-     *
-     * @param {Integer} numberStudents The number of students.
-     * @param {Integer} numberAssignments The number of assignments.
-     * @param {Element} table The table that is displaying students and assignments.
-     */
+/**
+ * This creates a table with # of assignments = # of columns and # of students = # of rows.
+ *
+ * @param {Integer} numberStudents The number of students.
+ * @param {Integer} numberAssignments The number of assignments.
+ * @param {Element} table The table that is displaying students and assignments.
+ */
 CourseSketch.gradeBook.addCells = function(numberStudents, numberAssignments, table) {
     for (var i = 0; i < numberStudents; i++){
         var row = document.createElement('tr');
