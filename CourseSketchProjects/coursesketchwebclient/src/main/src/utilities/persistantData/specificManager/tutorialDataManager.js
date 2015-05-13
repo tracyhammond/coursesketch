de951e34353055d9f26c580a206a3bf8120c6f0f
dtracers <dtracers@gmail.com>
2015-05-12 22:31:08 -0500

    I think this is finished?
    Former-commit-id: 45dd61f3df6d5befd6ef885c721885b5633b33a2

--- CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/persistantData/specificManager/tutorialDataManager.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/persistantData/specificManager/tutorialDataManager.js
@@ -54,8 +54,8 @@ function TutorialDataManager(parent, dataListener, database, sendData, Request, 
      *                function to be called after tutorial setting is done
      */
     function insertTutorialServer(tutorial, tutorialCallback) {
-        advanceDataListener.setListener(Request.MessageType.DATA_INSERT, CourseSketch.PROTOBUF_UTIL.ItemQuery.ASSIGNMENT, function(evt, item) {
-            advanceDataListener.removeListener(Request.MessageType.DATA_INSERT, CourseSketch.PROTOBUF_UTIL.ItemQuery.ASSIGNMENT);
+        advanceDataListener.setListener(Request.MessageType.DATA_INSERT, CourseSketch.PROTOBUF_UTIL.ItemQuery.TUTORIAL, function(evt, item) {
+            advanceDataListener.removeListener(Request.MessageType.DATA_INSERT, CourseSketch.PROTOBUF_UTIL.ItemQuery.TUTORIAL);
             var resultArray = item.getReturnText().split(':');
             var oldId = resultArray[1].trim();
             var newId = resultArray[0].trim();
@@ -76,7 +76,7 @@ function TutorialDataManager(parent, dataListener, database, sendData, Request, 
                 }
             });
         });
-        sendData.sendDataInsert(CourseSketch.PROTOBUF_UTIL.ItemQuery.ASSIGNMENT, tutorial.toArrayBuffer());
+        sendData.sendDataInsert(CourseSketch.PROTOBUF_UTIL.ItemQuery.TUTORIAL, tutorial.toArrayBuffer());
     }
 
 
@@ -142,14 +142,14 @@ function TutorialDataManager(parent, dataListener, database, sendData, Request, 
             if (!isUndefined(localCallback)) {
                 localCallback();
             }
-            advanceDataListener.setListener(Request.MessageType.DATA_UPDATE, CourseSketch.PROTOBUF_UTIL.ItemQuery.ASSIGNMENT, function(evt, item) {
-                advanceDataListener.removeListener(Request.MessageType.DATA_UPDATE, CourseSketch.PROTOBUF_UTIL.ItemQuery.ASSIGNMENT);
+            advanceDataListener.setListener(Request.MessageType.DATA_UPDATE, CourseSketch.PROTOBUF_UTIL.ItemQuery.TUTORIAL, function(evt, item) {
+                advanceDataListener.removeListener(Request.MessageType.DATA_UPDATE, CourseSketch.PROTOBUF_UTIL.ItemQuery.TUTORIAL);
                 // we do not need to make server changes we just need to make sure it was successful.
                 if (!isUndefined(serverCallback)) {
                     serverCallback(item);
                 }
             });
-            sendData.sendDataUpdate(CourseSketch.PROTOBUF_UTIL.ItemQuery.ASSIGNMENT, tutorial.toArrayBuffer());
+            sendData.sendDataUpdate(CourseSketch.PROTOBUF_UTIL.ItemQuery.TUTORIAL, tutorial.toArrayBuffer());
         });
     }
 
@@ -187,125 +187,37 @@ function TutorialDataManager(parent, dataListener, database, sendData, Request, 
     parent.getTutorialLocal = getTutorialLocal;
 
     /**
-     * Returns a list of all of the tutorials from the local and server database for the given list
-     * of Ids.
+     * get tutorial list.
      *
-     * This does attempt to pull tutorials from the server!
-     *
-     * @param {List<String>} tutorialIdList
-     *            list of IDs of the tutorials to get
-     * @param {Function} tutorialCallbackPartial
-     *            {Function} called when tutorials are grabbed from the local
-     *            database only. This list may not be complete. This may also
-     *            not get called if there are no local tutorials.
-     * @param {Function} tutorialCallbackComplete
-     *            {Function} called when the complete list of tutorials are
-     *            grabbed.
+     * @param {String} url The url of the webpage.
+     * @param {Function} tutorialCallback called when the tutorial list has been recieved.
+     * @param {Number} page the page of the tutorial list that is wanted.
      */
-    function getTutorials(tutorialIdList, tutorialCallbackPartial, tutorialCallbackComplete) {
-        /*
-         * So what happens here might be a bit confusing to some new people so
-         * let me explain it. #1 there is a loop that goes through every item in
-         * the tutorialIdList (which is a list of tutorial ids)
-         *
-         * #2 there is a function declaration inside the loop the reason for
-         * this is so that the tutorialId is not overwritten when the callback
-         * is called.
-         *
-         * #3 we call getTutorialLocal which then calls a callback about if it
-         * got an tutorial or not if it didn't we add the id to a list of Id
-         * we need to get from the server
-         *
-         * #4 after the entire list has been gone through (which terminates in
-         * the callback with barrier = 0) if there are any that need to be
-         * pulled from the server then that happens
-         *
-         * #5 after talking to the server we get a response with a list of
-         * tutorials, these are combined with the local tutorials then the
-         * original callback is called.
-         *
-         * #6 the function pattern terminates.
-         */
-
-        // standard preventative checking
-        if (isUndefined(tutorialIdList) || tutorialIdList === null || tutorialIdList.length === 0) {
-            tutorialCallbackPartial(new DatabaseException('The given id is not assigned', 'getting Tutorial: ' + tutorialIdList));
-            if (tutorialCallbackComplete) {
-                tutorialCallbackComplete(new DatabaseException('The given id is not assigned', 'getting Tutorial: ' + tutorialIdList));
+    function getTutorialList(url, tutorialCallback, page) {
+        advanceDataListener.setListener(Request.MessageType.DATA_REQUEST, CourseSketch.PROTOBUF_UTIL.ItemQuery.TUTORIAL, function(evt, item) {
+            advanceDataListener.removeListener(Request.MessageType.DATA_REQUEST,
+                CourseSketch.PROTOBUF_UTIL.ItemQuery.TUTORIAL);
+            tutorialList = [];
+            for (var i = 0; i < item.data.length; i++) {
+                var tutorial = CourseSketch.PROTOBUF_UTIL.getTutorialClass().decode(item.data[i]);
+                tutorialList.append(tutorial);
             }
+            tutorialCallback(tutorialList);
+        });
+        var itemRequest = CourseSketch.PROTOBUF_UTIL.ItemRequest();
+        itemRequest.setQuery(CourseSketch.PROTOBUF_UTIL.ItemQuery.TUTORIAL);
+        itemRequest.itemId = [ url ];
+        if (isUndefined(page)) {
+            itemRequest.page = 0;
+        } else {
+            itemRequest.page = page;
         }
-
-        var barrier = tutorialIdList.length;
-        var tutorialList = [];
-        var leftOverId = [];
-
-        // create local tutorial list so everything appears really fast!
-        for (var i = 0; i < tutorialIdList.length; i++) {
-            var tutorialIdLoop = tutorialIdList[i];
-            // the purpose of this function is purely to scope the tutorialId
-            // so that it changes
-            (function(tutorialId) {
-                getTutorialLocal(tutorialId, function(tutorial) {
-                    if (!isUndefined(tutorial) && !(tutorial instanceof DatabaseException)) {
-                        tutorialList.push(tutorial);
-                    } else {
-                        leftOverId.push(tutorialId);
-                    }
-                    barrier -= 1;
-                    if (barrier <= 0) {
-                        // after the entire list has been gone through pull the
-                        // leftovers from the server
-                        if (leftOverId.length >= 1) {
-                            advanceDataListener.setListener(Request.MessageType.DATA_REQUEST, CourseSketch.PROTOBUF_UTIL.ItemQuery.ASSIGNMENT,
-                                function(evt, item) {
-                                    advanceDataListener.removeListener(Request.MessageType.DATA_REQUEST,
-                                        CourseSketch.PROTOBUF_UTIL.ItemQuery.ASSIGNMENT);
-
-                                    // after listener is removed
-                                    if (isUndefined(item.data) || item.data === null) {
-                                        tutorialCallbackComplete(new DatabaseException('The data sent back from the server does not exist.'));
-                                        return;
-                                    }
-                                    var school = CourseSketch.PROTOBUF_UTIL.getSrlSchoolClass().decode(item.data);
-                                    var tutorial = school.tutorials[0];
-                                    if (isUndefined(tutorial) || tutorial instanceof DatabaseException) {
-                                        var result = tutorial;
-                                        if (isUndefined(result)) {
-                                            result = new DatabaseException('Nothing is in the server database!',
-                                                'Grabbing tutorial from server: ' + tutorialIdList);
-                                        }
-                                        if (!isUndefined(tutorialCallbackComplete)) {
-                                            tutorialCallbackComplete(result);
-                                        }
-                                        return;
-                                    }
-                                    for (var i = 0; i < school.tutorials.length; i++) {
-                                        localScope.setTutorial(school.tutorials[i]);
-                                        tutorialList.push(school.tutorials[i]);
-                                    }
-                                    stateCallbackList(tutorialList, tutorialCallbackComplete);
-                                    tutorialIdList = null;
-                                });
-                            // creates a request that is then sent to the server
-                            sendData.sendDataRequest(CourseSketch.PROTOBUF_UTIL.ItemQuery.ASSIGNMENT, leftOverId);
-                        } else {
-                            stateCallbackList(tutorialList, tutorialCallbackComplete);
-                        }
-
-                        // this calls actually before the response from the
-                        // server is received!
-                        if (tutorialList.length > 0) {
-                            stateCallbackList(tutorialList, tutorialCallbackPartial);
-                        }
-                    } // end of if(barrier === 0)
-                }); // end of getting local tutorial
-            })(tutorialIdLoop); // end of loopContainer
-        } // end of loop
+        sendData.sendDataRequest(itemRequest);
     }
 
-    parent.getTutorials = getTutorials;
+    parent.getTutorialList = getTutorialList;
 
-    /**
+        /**
      * Returns a tutorial with the given tutorialId will ask the server if it
      * does not exist locally
      *
@@ -321,13 +233,50 @@ function TutorialDataManager(parent, dataListener, database, sendData, Request, 
         }
         var called = false;
 
-        function callOnce(tutorialList) {
+        function callOnce(tutorial) {
             if (!called) {
                 called = true;
-                tutorialCallback(tutorialList[0]);
+                tutorialCallback(tutorial[0]);
             }
         }
 
+        getTutorialLocal(tutorialId, function(localTutorial) {
+            if (!isUndefined(localTutorial) && !(localTutorial instanceof DatabaseException)) {
+                callOnce(localTutorial);
+                return;
+            }
+            advanceDataListener.setListener(Request.MessageType.DATA_REQUEST, CourseSketch.PROTOBUF_UTIL.ItemQuery.TUTORIAL, function(evt, item) {
+                advanceDataListener.removeListener(Request.MessageType.DATA_REQUEST,
+                    CourseSketch.PROTOBUF_UTIL.ItemQuery.TUTORIAL);
+
+                // after listener is removed
+                if (isUndefined(item.data) || item.data === null) {
+                    tutorialCallbackComplete(new DatabaseException('The data sent back from the server does not exist.'));
+                    return;
+                }
+                var tutorial = CourseSketch.PROTOBUF_UTIL.getTutorial().decode(item.data[0]);
+                if (isUndefined(tutorial) || tutorial instanceof DatabaseException) {
+                    var result = tutorial;
+                    if (isUndefined(result)) {
+                        result = new DatabaseException('Nothing is in the server database!',
+                            'Grabbing tutorial from server: ' + tutorialIdList);
+                    }
+                    if (!isUndefined(tutorialCallbackComplete)) {
+                        tutorialCallbackComplete(result);
+                    }
+                    return;
+                }
+                for (var i = 0; i < school.tutorials.length; i++) {
+                    localScope.setTutorial(school.tutorials[i]);
+                    tutorialList.push(school.tutorials[i]);
+                }
+                stateCallbackList(tutorialList, tutorialCallbackComplete);
+                tutorialIdList = null;
+            });
+            // creates a request that is then sent to the server
+            sendData.sendDataRequest(CourseSketch.PROTOBUF_UTIL.ItemQuery.TUTORIAL, leftOverId);
+
+        }); // end of getting local tutorial
         getTutorials([ tutorialId ], callOnce, callOnce);
     }
 
