6017c6d30afcd5ffba80939ca05d2aa6ebbd3d60
dtracers <dtracers@gmail.com>
2015-05-14 14:13:57 -0500

    created a tutorial request handler
    Former-commit-id: 30f3f955b323a397d8f040a04ead0b21f8bb1c46

--- CourseSketchProjects/CourseSketchDatabaseServer/src/main/java/handlers/subhandlers/TutorialRequestHandler.java
+++ CourseSketchProjects/CourseSketchDatabaseServer/src/main/java/handlers/subhandlers/TutorialRequestHandler.java
@@ -1,7 +1,14 @@
 package handlers.subhandlers;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import database.DatabaseAccessException;
+import database.auth.AuthenticationException;
+import database.institution.Institution;
 import protobuf.srl.query.Data;
 import protobuf.srl.tutorial.TutorialOuterClass;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by gigemjt on 5/14/15.
@@ -14,7 +21,24 @@ public class TutorialRequestHandler {
 
     }
 
-    public static TutorialOuterClass.Tutorial handleTutorialRequest(Data.ItemRequest req) {
-
+    /**
+     * Returns a list of tutorials based on the request
+     * @param institution
+     * @param request
+     * @param userId
+     * @return
+     * @throws AuthenticationException
+     * @throws DatabaseAccessException
+     * @throws InvalidProtocolBufferException
+     */
+    public static List<TutorialOuterClass.Tutorial> handleTutorialRequest(final Institution institution, final Data.ItemRequest request,
+            final String userId) throws AuthenticationException, DatabaseAccessException, InvalidProtocolBufferException {
+        List<TutorialOuterClass.Tutorial> result = new ArrayList<>();
+        if (!request.hasPage()) {
+            result.add(institution.getTutorial(request.getItemId(0), userId));
+        } else {
+            result = institution.getTutorialList(userId, request.getItemId(0), request.getPage());
+        }
+        return result;
     }
 }
