45307f2c29a5a91213704d5ca0b5c378dc849867
Gilberto <garizpe83@gmail.com>
2015-05-13 23:24:49 -0500

    fixed smaller issues with interface
    Former-commit-id: e333bc686271501c456b7bc8502503dc689e150f

--- CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/gradebook/instructorGradebook.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/gradebook/instructorGradebook.js
@@ -1,25 +1,72 @@
-/**
- * This creates a table with # of assignments = # of columns and # of students = # of rows.
- *
- * @param {Integer} numberStudents The number of students.
- * @param {Integer} numberAssignments The number of assignments.
- * @param {Element} table The table that is displaying students and assignments.
- */
-CourseSketch.gradeBook.addCells = function(numberStudents, numberAssignments, table) {
-    for (var i = 0; i < numberStudents; i++){
+(function() {
+
+    $(document).ready(function() {
+        CourseSketch.gradeBook.loadGrades('courseId');
+    });
+
+    CourseSketch.gradeBook.loadGrades = function(courseId) {
+        document.querySelector('.tabletalk').innerHtml = ''; // resets table
+
+        CourseSketch.dataManager.getCourse(courseId, function(course) {
+            // loads all of the grades
+            CourseSketch.dataManager.getAllAssignmentGrades(courseId, function(gradeList) {
+                var assignmentList = course.assignmentList;
+                CourseSketch.gradeBook.populateGrades(assignmentList, gradeList, document.querySelector('.tabletalk'));
+            });
+        });
+    };
+
+    /**
+     * This adds to the map a student row with columns for assignment grades.
+     * @param {Map<studentI,tr>} studentMap This is the Map where the student ID are stored.
+     * @param {String} studentId The student's ID number.
+     * @param {Integer} numberOfAssignments The number of assignments and length of the listAssignments list.
+     */
+    function addNewStudent(studentMap, studentId, numberOfAssignments, table) {
         var row = document.createElement('tr');
-        if (i === 0) {
-            for (var t = 0; t < numberAssignments; t++) {
-                var title = document.createElement('th');
-                title.textContent = i;
-                table.appendChild(title);
-            }
-        }
-        table.appendChild(row);
-        for (var j = 0; j < numberAssignments; j++) {
+        for (var i = 0; i < numberOfAssignments; i++) {
             var column = document.createElement('td');
-            column.textContent = [];
             row.appendChild(column);
         }
+        table.appendChild(row);
+        studentMap.set(studentId, row);
     }
-};
+
+    /**
+     * This populates the map of grades for each student and, calls student adding function if student has not been added yet.
+     *
+     * @param {List<String>} listAssignments The list of assignment IDs.
+     * @param {List<ProtoGrade>} listGrades The list of grades from the server.
+     */
+    CourseSketch.gradeBook.populateGrades = function(listAssignments, listGrades, table) {
+        var assignmentMap = new Map();
+        var studentMap = new Map();
+        var header = document.createElement('thead');
+        var row = document.createElement('tr');
+        for (var i = 0; i < listAssignments.length; i++) {
+            assignmentMap.set(listAssignments[i], i);
+            var th = document.createElement('th');
+            th.textContent = listAssignments[i];
+            row.appendChild(th);
+        }
+        header.appendChild(row);
+        table.appendChild(header);
+
+        var body = document.createElement('tbody');
+        table.appendChild(body);
+
+        for (var i = 0; i < listGrades.length; i++) {
+            var protoGrade = listGrades[i];
+            var studentId = protoGrade.userId;
+            var assignmentId = protoGrade.assignmentId;
+            if (!studentMap.has(studentId)) {
+                addNewStudent(studentMap, studentId, listAssignments.length, body);
+            }
+            var studentRow = studentMap.get(studentId);
+            var columnList = studentRow.children;
+            var cell = columnList[assignmentMap.get(assignmentId)];
+            cell.textContent = protoGrade.getCurrentGrade();
+        }
+    };
+
+})();
