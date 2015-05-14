517071ef7cd4b4dad7cd19ec209b975e37584fb2
dtracers <dtracers@gmail.com>
2015-05-14 13:42:27 -0500

    working on adding a tutorial request handler
    Former-commit-id: 338f312358ec6cb696655a0bcfc56342bf0dcbfd

---
+++ CourseSketchProjects/CourseSketchDatabaseServer/src/main/java/handlers/subhandlers/TutorialRequestHandler.java
@@ -0,0 +1,20 @@
+package handlers.subhandlers;
+
+import protobuf.srl.query.Data;
+import protobuf.srl.tutorial.TutorialOuterClass;
+
+/**
+ * Created by gigemjt on 5/14/15.
+ */
+public class TutorialRequestHandler {
+    /**
+     * Private constructor
+     */
+    private TutorialRequestHandler() {
+
+    }
+
+    public static TutorialOuterClass.Tutorial handleTutorialRequest(Data.ItemRequest req) {
+
+    }
+}
