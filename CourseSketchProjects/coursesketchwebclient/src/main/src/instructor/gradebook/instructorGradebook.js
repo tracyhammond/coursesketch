9713dce5a50afea2c6ce8065d82dd13d54244053
dtracers <dtracers@gmail.com>
2015-05-15 02:34:19 -0500

    rearranging code making the gradebook nicer
    Former-commit-id: dbf445c7bba9f4ead66c9c14085e33bdac8c442f

--- CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/gradebook/instructorGradebook.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/instructor/gradebook/instructorGradebook.js
@@ -4,7 +4,7 @@
         CourseSketch.dataManager.waitForDatabase(function() {
             var courseId = CourseSketch.dataManager.getState('gradebookCourseid');
             CourseSketch.gradeBook.loadGrades(courseId);
-            CourseSketch.gradeBook.createTabs(['Quiz', 'Homework', 'Test'], document.querySelector('.tabholder'));
+            CourseSketch.gradeBook.createTabs([ 'Quiz', 'Homework', 'Test' ], document.querySelector('.tabholder'));
         });
     });
 
@@ -20,8 +20,9 @@
     };
 
     /**
-     * This adds to the map a student row with columns for assignment grades.
-     * @param {Map<studentI,tr>} studentMap This is the Map where the student ID are stored.
+     * Creates a new tr with the number of assignments and adds it to the student map.
+     *
+     * @param {Map<String,Element>} studentMap This is a map of studentId's to table rows.
      * @param {String} studentId The student's ID number.
      * @param {Integer} numberOfAssignments The number of assignments and length of the listAssignments list.
      */
@@ -29,8 +30,9 @@
         var row = document.createElement('tr');
         for (var i = 0; i < numberOfAssignments; i++) {
             var column = document.createElement('td');
-            column.style.width = '6ch';
+           // column.style.width = '6ch';
             column.style.maxWidth = '8ch';
+            column.style.position = 'relative';
             column.onclick = gradeCellOnClick;
             row.appendChild(column);
         }
@@ -41,27 +43,15 @@
     /**
      * This populates the map of grades for each student and, calls student adding function if student has not been added yet.
      *
-     * @param {List<String>} listAssignments The list of assignment IDs.
+     * @param {List<String>} assignmentList The list of assignment IDs.
      * @param {List<ProtoGrade>} listGrades The list of grades from the server.
      */
-    CourseSketch.gradeBook.populateGrades = function(listAssignments, listGrades, table) {
+    CourseSketch.gradeBook.populateGrades = function(assignmentList, listGrades, table) {
         table.innerHTML = '';
         var assignmentMap = new Map();
         var studentMap = new Map();
-        var header = document.createElement('thead');
-        var row = document.createElement('tr');
-        var nameLabel = document.createElement('th');
-        nameLabel.textContent = 'Student Name';
-        row.appendChild(nameLabel);
-        for (var i = 0; i < listAssignments.length; i++) {
-            assignmentMap.set(listAssignments[i], i);
-            var th = document.createElement('th');
-            th.textContent = listAssignments[i];
-            row.appendChild(th);
-        }
-        header.appendChild(row);
-        table.appendChild(header);
 
+        createAssignmentHeader(assignmentList, table, assignmentMap);
         var body = document.createElement('tbody');
         table.appendChild(body);
 
@@ -70,21 +60,52 @@
             var studentId = protoGrade.userId;
             var assignmentId = protoGrade.assignmentId;
             if (!studentMap.has(studentId)) {
-                addNewStudent(studentMap, studentId, listAssignments.length, body);
+                addNewStudent(studentMap, studentId, assignmentList.length, body);
             }
             var studentRow = studentMap.get(studentId);
             var columnList = studentRow.children;
             var cell = columnList[assignmentMap.get(assignmentId)];
             cell.textContent = protoGrade.getCurrentGrade();
-
         }
+
+        populateStudentNames(studentMap);
+    };
+
+    /**
+     * Creates the assignment header while populating the assignment map.
+     * @param {List<String>} assignmentList
+     * @param {Element} table
+     * @param {Map<String, Integer>} assignmentMap
+     */
+    function createAssignmentHeader(assignmentList, table, assignmentMap) {
+        var header = document.createElement('thead');
+        var row = document.createElement('tr');
+        var nameLabel = document.createElement('th');
+        nameLabel.textContent = 'Student Name';
+        row.appendChild(nameLabel);
+        for (var i = 0; i < assignmentList.length; i++) {
+            assignmentMap.set(assignmentList[i], i);
+            var th = document.createElement('th');
+            th.style.maxWidth = '8ch';
+            th.textContent = assignmentList[i];
+            row.appendChild(th);
+        }
+        header.appendChild(row);
+        table.appendChild(header);
+    }
+
+    /**
+     * Adds the student names to the first column of every row.
+     * @param {Map<String, Element>} studentMap
+     */
+    function populateStudentNames(studentMap) {
         studentMap.forEach(function(value, key, map) {
             row = map.get(key);
             var cell = document.createElement('td');
             cell.textContent = key;
             row.insertBefore(cell, row.firstChild);
-        })
-    };
+        });
+    }
 
     function gradeCellOnClick() {
         if (this.querySelector('input') === null) {
@@ -96,7 +117,8 @@
             inputElement.value = grade;
             inputElement.pattern = '\\d*';
             inputElement.style.padding = '0px';
-            inputElement.style.width = 'auto';
+            inputElement.style.width = '5ch';
+            inputElement.maxlength = 5;
             this.appendChild(inputElement);
             inputElement.focus();
             inputElement.select();
@@ -114,7 +136,7 @@
         var tabs = document.createElement('ul');
         tabs.className = 'tabs';
         gradeCategories.unshift('All');
-        gradeCategories.push('Add New');
+        gradeCategories.push('New');
         for (var i = 0; i < gradeCategories.length; i++) {
             var item = document.createElement('li');
             item.className = 'tab col';
