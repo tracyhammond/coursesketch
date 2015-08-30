dda8c54bda7ff3ec7536239b9b5a452fd8162088
dtracers <dtracers@gmail.com>
2015-08-30 12:02:33 -0500

    added a rpc server.
    Former-commit-id: b5a4f2e22e46ec4fad6d818e5def750f5c03222a

---
+++ CourseSketchProjects/rpcserver/src/test/java/netty/WebSocketServerHandler.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.util.CharsetUtil;
+
+import java.nio.ByteBuffer;
+
+import static io.netty.handler.codec.http.HttpHeaders.Names.*;
+import static io.netty.handler.codec.http.HttpHeaders.*;
+import static io.netty.handler.codec.http.HttpMethod.*;
+import static io.netty.handler.codec.http.HttpResponseStatus.*;
+import static io.netty.handler.codec.http.HttpVersion.*;
+
+/**
+ * Handles handshakes and messages
+ */
+public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> {
+
+    private static final String WEBSOCKET_PATH = "/websocket";
+
+    private WebSocketServerHandshaker handshaker;
+
+    @Override
+    public void channelRead0(final ChannelHandlerContext ctx, final Object msg) throws Exception {
+        if (msg instanceof FullHttpRequest) {
+            handleHttpRequest(ctx, (FullHttpRequest) msg);
+        } else if (msg instanceof WebSocketFrame) {
+            handleWebSocketFrame(ctx, (WebSocketFrame) msg);
+        }
+    }
+
+    @Override
+    public void channelReadComplete(final ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    private void handleHttpRequest(final ChannelHandlerContext ctx, final FullHttpRequest req) {
+        // Handle a bad request.
+
+        if (!req.decoderResult().isSuccess()) {
+            sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HTTP_1_1, BAD_REQUEST));
+            return;
+        }
+
+        // Allow only GET methods.
+        if (req.method() != GET) {
+            sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HTTP_1_1, FORBIDDEN));
+            return;
+        }
+
+        // Send the demo page and favicon.ico
+        if ("/".equals(req.uri())) {
+            final ByteBuf content = WebSocketServerIndexPage.getContent(getWebSocketLocation(req));
+            final FullHttpResponse res = new DefaultFullHttpResponse(HTTP_1_1, OK, content);
+
+            res.headers().set(CONTENT_TYPE, "text/html; charset=UTF-8");
+            setContentLength(res, content.readableBytes());
+
+            sendHttpResponse(ctx, req, res);
+            return;
+        }
+        if ("/favicon.ico".equals(req.uri())) {
+            final FullHttpResponse res = new DefaultFullHttpResponse(HTTP_1_1, NOT_FOUND);
+            sendHttpResponse(ctx, req, res);
+            return;
+        }
+
+        // Handshake
+        final WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(
+                getWebSocketLocation(req), null, false);
+        handshaker = wsFactory.newHandshaker(req);
+        if (handshaker == null) {
+            WebSocketServerHandshakerFactory.sendUnsupportedVersionResponse(ctx.channel());
+        } else {
+            handshaker.handshake(ctx.channel(), req);
+        }
+    }
+
+    private void handleWebSocketFrame(final ChannelHandlerContext ctx, final WebSocketFrame frame) {
+
+        // Check for closing frame
+        if (frame instanceof CloseWebSocketFrame) {
+            handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
+            return;
+        }
+        if (frame instanceof PingWebSocketFrame) {
+            ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+            return;
+        }
+
+        if (frame instanceof BinaryWebSocketFrame) {
+            final BinaryWebSocketFrame bi = (BinaryWebSocketFrame) frame;
+            final ByteBuffer bytes = bi.content().nioBuffer();
+            System.out.println("RECIEVING BINARY MESSAGE?!?!");
+            return;
+        }
+
+        if (!(frame instanceof TextWebSocketFrame)) {
+            throw new UnsupportedOperationException(String.format("%s frame types not supported", frame.getClass()
+                    .getName()));
+        }
+
+        if (frame instanceof BinaryWebSocketFrame) {
+            System.out.println("RECIEVING BINARY MESSAGE?!?!");
+
+        }
+
+        // Send the uppercase string back.
+        final String request = ((TextWebSocketFrame) frame).text();
+        System.err.printf("%s received %s%n", ctx.channel(), request);
+        ctx.channel().write(new TextWebSocketFrame(request.toUpperCase()));
+    }
+
+    private static void sendHttpResponse(
+            final ChannelHandlerContext ctx, final FullHttpRequest req, final FullHttpResponse res) {
+        // Generate an error page if response getStatus code is not OK (200).
+        if (res.status() != HttpResponseStatus.OK) {
+            final ByteBuf buf = Unpooled.copiedBuffer(res.status().toString(), CharsetUtil.UTF_8);
+            res.content().writeBytes(buf);
+            buf.release();
+            setContentLength(res, res.content().readableBytes());
+        }
+
+        // Send the response and close the connection if necessary.
+        ChannelFuture f = ctx.channel().writeAndFlush(res);
+        if (!isKeepAlive(req) || res.status() != HttpResponseStatus.OK) {
+            f.addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    @Override
+    public final void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    private static String getWebSocketLocation(final FullHttpRequest req) {
+        final String location =  req.headers().get(HOST) + WEBSOCKET_PATH;
+        if (WebSocketRunner.SSL) {
+            return "wss://" + location;
+        } else {
+            return "ws://" + location;
+        }
+    }
+}
