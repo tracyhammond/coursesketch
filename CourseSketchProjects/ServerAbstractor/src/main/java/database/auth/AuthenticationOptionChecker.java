6a5fcb6802e5bf168e29b9544461f3575525bb4d
dtracers <dtracers@gmail.com>
2015-09-05 12:54:45 -0500

    Finished writing AuthenticationTests
    The Authenticator is now 90% covered in tests! (yayy)

--- CourseSketchProjects/ServerAbstractor/src/main/java/database/auth/AuthenticationOptionChecker.java
+++ CourseSketchProjects/ServerAbstractor/src/main/java/database/auth/AuthenticationOptionChecker.java
@@ -1,5 +1,6 @@
 package database.auth;
 
+import database.DatabaseAccessException;
 import protobuf.srl.school.School;
 
 /**
@@ -8,11 +9,11 @@ import protobuf.srl.school.School;
  * Created by gigemjt on 9/4/15.
  */
 public interface AuthenticationOptionChecker {
-    boolean authenticateDate(final AuthenticationDataCreator dataCreator, long checkTime);
+    boolean authenticateDate(final AuthenticationDataCreator dataCreator, long checkTime) throws DatabaseAccessException;
 
-    boolean isItemRegistrationRequired(final AuthenticationDataCreator dataCreator);
+    boolean isItemRegistrationRequired(final AuthenticationDataCreator dataCreator) throws DatabaseAccessException;
 
-    boolean isItemPublished(final AuthenticationDataCreator dataCreator);
+    boolean isItemPublished(final AuthenticationDataCreator dataCreator) throws DatabaseAccessException;
 
     /**
      * @return a data creator that grabs the data for any other uses by the option checker.
