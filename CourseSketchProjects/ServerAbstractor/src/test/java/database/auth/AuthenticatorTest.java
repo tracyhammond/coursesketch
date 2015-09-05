5f7517c18e8567069f61e6810b7479d03ad64a28
dtracers <dtracers@gmail.com>
2015-09-05 12:27:54 -0500

    Added unit tests
    Former-commit-id: c9d59ae5083205d303ffbfaad718c6364b13ca36

--- CourseSketchProjects/ServerAbstractor/src/test/java/database/auth/AuthenticatorTest.java
+++ CourseSketchProjects/ServerAbstractor/src/test/java/database/auth/AuthenticatorTest.java
@@ -1,23 +1,108 @@
 package database.auth;
 
+import database.DatabaseAccessException;
 import database.RequestConverter;
 import org.joda.time.DateTime;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import protobuf.srl.school.School;
+import protobuf.srl.services.authentication.Authentication;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AuthenticatorTest {
+    @Mock AuthenticationChecker authChecker;
+    @Mock AuthenticationOptionChecker optionChecker;
+    Authenticator authenticator;
 
     @Before
-    public void setUp() throws Exception {
+    public void setup() {
+        try {
+            when(authChecker.isAuthenticated(any(School.ItemType.class), anyString(), anyString(), any(Authentication.AuthType.class)))
+                    .thenReturn(Authentication.AuthResponse.getDefaultInstance());
+
+            when(optionChecker.authenticateDate(any(AuthenticationDataCreator.class), anyLong()))
+                    .thenReturn(false);
+
+            when(optionChecker.isItemPublished(any(AuthenticationDataCreator.class)))
+                    .thenReturn(false);
+
+            when(optionChecker.isItemRegistrationRequired(any(AuthenticationDataCreator.class)))
+                    .thenReturn(true);
+        } catch (DatabaseAccessException e) {
+            e.printStackTrace();
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+        }
+        authenticator = new Authenticator(authChecker, optionChecker);
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void teardown() {
+        authenticator = null;
     }
+
+    // Simple precondition testing
+
+    @Test(expected = NullPointerException.class)
+    public void creationFailsIfGivenNullChecker() {
+        new Authenticator(null, Mockito.mock(AuthenticationOptionChecker.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void creationFailsIfGivenOptionChecker() {
+        new Authenticator(Mockito.mock(AuthenticationChecker.class), null);
+    }
+
+    @Test()
+    public void creationSucceedsIfGivenValue() {
+        new Authenticator(Mockito.mock(AuthenticationChecker.class), Mockito.mock(AuthenticationOptionChecker.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void authenticatorThrowsExceptionIfInvalidCollectionType() throws Exception {
+        authenticator.checkAuthentication(null, null, null, 0, null);
+    }
+
+
+    @Test(expected = NullPointerException.class)
+    public void authenticatorThrowsExceptionIfInvalidItemId() throws Exception {
+        authenticator.checkAuthentication(School.ItemType.COURSE, null, null, 0, null);
+    }
+
+
+    @Test(expected = NullPointerException.class)
+    public void authenticatorThrowsExceptionIfInvalidUserId() throws Exception {
+        authenticator.checkAuthentication(School.ItemType.COURSE, "", null, 0, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void authenticatorThrowsExceptionIfNullAuthType() throws Exception {
+        authenticator.checkAuthentication(School.ItemType.COURSE, "", "", 0, null);
+    }
+
+    @Test(expected = AuthenticationException.class)
+    public void authenticatorThrowsExceptionIfInvalidAuthType() throws Exception {
+        authenticator.checkAuthentication(School.ItemType.COURSE, "", "", 0, Authentication.AuthType.getDefaultInstance());
+    }
+
+    // TIME CHECKING
 
     @Test
     public void returnsTrueWhenValidTimeGiven() {
@@ -44,5 +129,163 @@ public class AuthenticatorTest {
         final DateTime close = t.plus(1000);
         assertFalse(Authenticator.isTimeValid(t.getMillis(), RequestConverter.getProtoFromDate(open), RequestConverter.getProtoFromDate(open)));
         assertFalse(Authenticator.isTimeValid(t.getMillis(), open, open));
+    }
+
+    // Check that code calls methods only when certain auth types are set
+
+    @Test
+    public void authenticatorDoesNotCheckDateIfItIsNotSet() throws Exception {
+
+        authenticator.checkAuthentication(School.ItemType.COURSE, "", "", 0, Authentication.AuthType.newBuilder()
+                .setCheckAccess(true)
+                .build());
+
+        verify(optionChecker, never()).authenticateDate(any(AuthenticationDataCreator.class), anyLong());
+    }
+
+    @Test
+    public void authenticatorOnlyChecksDate() throws Exception {
+
+        authenticator.checkAuthentication(School.ItemType.COURSE, "", "", 0, Authentication.AuthType.newBuilder()
+                .setCheckDate(true)
+                .build());
+
+        verify(optionChecker, atLeastOnce()).authenticateDate(any(AuthenticationDataCreator.class), anyLong());
+        verify(optionChecker, atLeastOnce()).createDataGrabber(any(School.ItemType.class), anyString());
+        verifyNoMoreInteractions(optionChecker);
+    }
+
+    @Test
+    public void authenticatorDoesNotIfPublishedAndRegistrationIfItIsNotSet() throws Exception {
+
+        authenticator.checkAuthentication(School.ItemType.COURSE, "", "", 0, Authentication.AuthType.newBuilder()
+                .setCheckDate(true)
+                .build());
+
+        verify(optionChecker, never()).isItemPublished(any(AuthenticationDataCreator.class));
+        verify(optionChecker, never()).isItemRegistrationRequired(any(AuthenticationDataCreator.class));
+    }
+
+    @Test
+    public void authenticatorOnlyChecksPublishedAndRegistration() throws Exception {
+
+        authenticator.checkAuthentication(School.ItemType.COURSE, "", "", 0, Authentication.AuthType.newBuilder()
+                .setCheckAccess(true)
+                .build());
+
+        verify(optionChecker, atLeastOnce()).isItemPublished(any(AuthenticationDataCreator.class));
+        verify(optionChecker, atLeastOnce()).isItemRegistrationRequired(any(AuthenticationDataCreator.class));
+        verify(optionChecker, atLeastOnce()).createDataGrabber(any(School.ItemType.class), anyString());
+        verifyNoMoreInteractions(optionChecker);
+    }
+
+    @Test
+    public void authenticatorDoesNotCheckOptionIfNoneAreSet() throws Exception {
+
+        authenticator.checkAuthentication(School.ItemType.COURSE, "", "", 0, Authentication.AuthType.newBuilder()
+                .setCheckingUser(true)
+                .build());
+
+        verifyNoMoreInteractions(optionChecker);
+    }
+
+    @Test
+    public void authenticatorDoesNotCheckAuthIfNoneAreSet() throws Exception {
+
+        authenticator.checkAuthentication(School.ItemType.COURSE, "", "", 0, Authentication.AuthType.newBuilder()
+                .setCheckDate(true)
+                .build());
+
+        verifyNoMoreInteractions(authChecker);
+    }
+
+    @Test
+    public void authenticatorCheckAuthIfSet() throws Exception {
+
+        final Authentication.AuthType type = Authentication.AuthType.newBuilder()
+                .setCheckingUser(true)
+                .build();
+        authenticator.checkAuthentication(School.ItemType.COURSE, "", "", 0, type);
+
+        verify(authChecker, atLeastOnce()).isAuthenticated(any(School.ItemType.class), anyString(), anyString(), eq(type));
+        verifyNoMoreInteractions(optionChecker);
+        verifyNoMoreInteractions(authChecker);
+    }
+
+    // Authenticator Result Tests
+
+    @Test
+    public void authenticatorCheckNoPermissions() throws Exception {
+
+        final Authentication.AuthType type = Authentication.AuthType.newBuilder()
+                .setCheckAccess(true)
+                .setCheckDate(true)
+                .build();
+        AuthenticationResponder responder = authenticator.checkAuthentication(School.ItemType.COURSE, "", "", 0, type);
+
+        assertFalse(responder.hasAccess());
+        assertFalse(responder.hasStudentPermission());
+        assertFalse(responder.hasPeerTeacherPermission());
+        assertFalse(responder.hasModeratorPermission());
+        assertFalse(responder.hasTeacherPermission());
+        assertFalse(responder.isItemOpen());
+        assertFalse(responder.isItemPublished());
+        // remember this is the reverse
+        assertTrue(responder.isRegistrationRequired());
+    }
+
+
+    @Test
+    public void authenticatorHasAccessIfAuthCheckHasAccess() throws Exception {
+
+        when(authChecker.isAuthenticated(any(School.ItemType.class), anyString(), anyString(), any(Authentication.AuthType.class)))
+                .thenReturn(Authentication.AuthResponse.newBuilder()
+                .setHasAccess(true)
+                .build());
+
+        final Authentication.AuthType type = Authentication.AuthType.newBuilder()
+                .setCheckAccess(true)
+                .setCheckDate(true)
+                .build();
+        AuthenticationResponder responder = authenticator.checkAuthentication(School.ItemType.COURSE, "", "", 0, type);
+
+        // Specific value being tested
+        assertTrue(responder.hasAccess());
+
+        // other values that are not the highlight of the test
+        assertFalse(responder.hasStudentPermission());
+        assertFalse(responder.hasPeerTeacherPermission());
+        assertFalse(responder.hasModeratorPermission());
+        assertFalse(responder.hasTeacherPermission());
+        assertFalse(responder.isItemOpen());
+        assertFalse(responder.isItemPublished());
+        // remember this is the reverse
+        assertTrue(responder.isRegistrationRequired());
+    }
+
+
+    @Test
+    public void authenticatorHasAccessIfAuthNoRegistrationAndCourseIsOpen() throws Exception {
+
+        when(optionChecker.isItemRegistrationRequired(any(AuthenticationDataCreator.class)))
+                .thenReturn(false);
+
+        final Authentication.AuthType type = Authentication.AuthType.newBuilder()
+                .setCheckAccess(true)
+                .setCheckDate(true)
+                .build();
+        AuthenticationResponder responder = authenticator.checkAuthentication(School.ItemType.COURSE, "", "", 0, type);
+
+        // Specific values being tested
+        assertTrue(responder.hasAccess());
+        assertFalse(responder.isRegistrationRequired());
+
+        // other values that should be false
+        assertFalse(responder.hasStudentPermission());
+        assertFalse(responder.hasPeerTeacherPermission());
+        assertFalse(responder.hasModeratorPermission());
+        assertFalse(responder.hasTeacherPermission());
+        assertFalse(responder.isItemOpen());
+        assertFalse(responder.isItemPublished());
     }
 }
