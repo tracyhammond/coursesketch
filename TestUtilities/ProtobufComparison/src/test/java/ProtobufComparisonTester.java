0bb8e8d2b13eaacc90dd171d82300723954cd789
dtracers <dtracers@gmail.com>
2015-09-06 17:28:27 -0500

    added a protobuf test library
    Now protobuf code can be compared in a much simpler way than the normal
    equals

--- TestUtilities/ProtobufComparison/src/test/java/ProtobufComparisonTester.java
+++ TestUtilities/ProtobufComparison/src/test/java/ProtobufComparisonTester.java
@@ -1,4 +1,5 @@
 import com.coursesketch.test.utilities.ProtobufComparison;
+import com.coursesketch.test.utilities.ProtobufComparisonBuilder;
 import org.junit.Test;
 import protobuf.srl.request.Message;
 
@@ -8,7 +9,25 @@ import protobuf.srl.request.Message;
 public class ProtobufComparisonTester {
     @Test
     public void testComparisonOfSameProtoObjects() {
-        ProtobufComparison comp = new ProtobufComparison(null, null);
+        ProtobufComparison comp = new ProtobufComparisonBuilder().build();
+        comp.equals(Message.Request.getDefaultInstance(), Message.Request.getDefaultInstance());
+    }
+
+    @Test
+    public void testComparisonOfSameProtoObjectsdiffvalues() {
+        ProtobufComparison comp = new ProtobufComparisonBuilder().build();
+        comp.equals(Message.Request.newBuilder().setRequestId("6").buildPartial(), Message.Request.getDefaultInstance());
+    }
+
+    @Test
+    public void testComparisonOfSameProtoObjectsdiffvaluesButItIsIgnored() {
+        ProtobufComparison comp = new ProtobufComparisonBuilder().ignoreField(Message.Request.getDescriptor().findFieldByName("requestId")).build();
+        comp.equals(Message.Request.newBuilder().setRequestId("6").buildPartial(), Message.Request.getDefaultInstance());
+    }
+
+    @Test
+    public void testComparisonOfSameP2rotoObjects() {
+        ProtobufComparison comp = new ProtobufComparisonBuilder().build();
         comp.equals(Message.Request.getDefaultInstance(), Message.Request.getDefaultInstance());
     }
 }
