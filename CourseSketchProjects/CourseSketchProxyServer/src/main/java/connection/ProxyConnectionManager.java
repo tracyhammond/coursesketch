3597d41a64486f81aab0a81f2c72f1e1fd3050b9
dtracers <dtracers@gmail.com>
2015-09-14 16:14:15 -0500

    fixed issues

--- CourseSketchProjects/CourseSketchProxyServer/src/main/java/connection/ProxyConnectionManager.java
+++ CourseSketchProjects/CourseSketchProxyServer/src/main/java/connection/ProxyConnectionManager.java
@@ -19,20 +19,17 @@ public final class ProxyConnectionManager extends MultiConnectionManager {
     /**
      * IP address for login server.
      */
-    @SuppressWarnings("PMD.AvoidUsingHardCodedIP")
-    private static final String LOGIN_ADDRESS = "";
+    private static final String LOGIN_ADDRESS = "LOGIN_IP_PROP";
 
     /**
      * IP address for database server.
      */
-    @SuppressWarnings("PMD.AvoidUsingHardCodedIP")
-    private static final String DATABASE_ADDRESS = "";
+    private static final String DATABASE_ADDRESS = "DATABASE_IP_PROP";
 
     /**
      * IP address for answer checker server.
      */
-    @SuppressWarnings("PMD.AvoidUsingHardCodedIP")
-    private static final String ANSWER_ADDRESS = "";
+    private static final String ANSWER_ADDRESS = "ANSWER_IP_PROP";
 
     /**
      * Declaration and Definition of Logger.
