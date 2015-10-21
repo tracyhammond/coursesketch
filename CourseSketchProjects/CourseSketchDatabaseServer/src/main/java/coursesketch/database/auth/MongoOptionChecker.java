dec2b6c0e75178f4b348ea45be079b1eba7a6ea7
dtracers <dtracers@gmail.com>
2015-10-21 17:26:28 -0500

    Fixed bug with checking assignment registration

--- CourseSketchProjects/CourseSketchDatabaseServer/src/main/java/coursesketch/database/auth/MongoOptionChecker.java
+++ CourseSketchProjects/CourseSketchDatabaseServer/src/main/java/coursesketch/database/auth/MongoOptionChecker.java
@@ -28,24 +28,36 @@ public final class MongoOptionChecker implements AuthenticationOptionChecker {
         this.database = database;
     }
 
-    @Override public boolean authenticateDate(final AuthenticationDataCreator dataCreator, final long checkTime) {
+    @Override public boolean authenticateDate(final AuthenticationDataCreator dataCreator, final long checkTime) throws DatabaseAccessException {
         final DBObject result = (DBObject) dataCreator.getDatabaseResult();
+        if (!result.containsField(DatabaseStringConstants.ACCESS_DATE)) {
+            throw new DatabaseAccessException("DBObject does not contain value for key " + DatabaseStringConstants.ACCESS_DATE);
+        }
+        if (!result.containsField(DatabaseStringConstants.CLOSE_DATE)) {
+            throw new DatabaseAccessException("DBObject does not contain value for key " + DatabaseStringConstants.CLOSE_DATE);
+        }
         final long accessDate = (long) result.get(DatabaseStringConstants.ACCESS_DATE);
         final long closeDate = (long) result.get(DatabaseStringConstants.CLOSE_DATE);
-        return accessDate >= checkTime && checkTime <= closeDate;
+        return accessDate <= checkTime && checkTime <= closeDate;
     }
 
     @Override public boolean isItemRegistrationRequired(final AuthenticationDataCreator dataCreator) throws DatabaseAccessException {
         final DBObject result = (DBObject) dataCreator.getDatabaseResult();
-        final int access = (int) result.get(DatabaseStringConstants.COURSE_ACCESS);
-        final School.SrlCourse.Accessibility accessValue = School.SrlCourse.Accessibility.valueOf(access);
+        final Object access = result.get(DatabaseStringConstants.COURSE_ACCESS);
+        if (access == null) {
+            throw new DatabaseAccessException("DBObject does not contain value for key " + DatabaseStringConstants.COURSE_ACCESS);
+        }
+        final School.SrlCourse.Accessibility accessValue = School.SrlCourse.Accessibility.valueOf((int) access);
         return !(accessValue == School.SrlCourse.Accessibility.PUBLIC || accessValue == School.SrlCourse.Accessibility.SUPER_PUBLIC);
     }
 
     @Override public boolean isItemPublished(final AuthenticationDataCreator dataCreator) throws DatabaseAccessException {
         final DBObject result = (DBObject) dataCreator.getDatabaseResult();
-        final boolean published = (boolean) result.get(DatabaseStringConstants.STATE_PUBLISHED);
-        return published;
+        final Object published = result.get(DatabaseStringConstants.STATE_PUBLISHED);
+        if (published == null) {
+            throw new DatabaseAccessException("DBObject does not contain value for key " + DatabaseStringConstants.STATE_PUBLISHED);
+        }
+        return (boolean) published;
     }
 
     /**
@@ -53,9 +65,14 @@ public final class MongoOptionChecker implements AuthenticationOptionChecker {
      * @param itemId The id of the tiem that is being checked.
      * @return a data creator that grabs the data for any other uses by the option checker.
      */
-    @Override public AuthenticationDataCreator createDataGrabber(final School.ItemType collectionType, final String itemId) {
+    @Override public AuthenticationDataCreator createDataGrabber(final School.ItemType collectionType, final String itemId)
+            throws DatabaseAccessException {
         final String collectionName = DbSchoolUtility.getCollectionFromType(collectionType, true);
         final DBObject result = database.getCollection(collectionName).findOne(new ObjectId(itemId));
+        if (result == null) {
+            throw new DatabaseAccessException(DbSchoolUtility.getCollectionFromType(collectionType) + " Was not found in " + collectionName
+                    + " with id " + itemId);
+        }
         return new AuthenticationDataCreator() {
             @Override public Object getDatabaseResult() {
                 return result;
