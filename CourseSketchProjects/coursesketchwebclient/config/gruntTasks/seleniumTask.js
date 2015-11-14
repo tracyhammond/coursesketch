65216c9cf31b75c3d6f354de165147a1201bbb4e
dtracers <dtracers@gmail.com>
2015-11-13 18:47:38 -0600

    more work on getting unit tests to work

--- CourseSketchProjects/coursesketchwebclient/config/gruntTasks/seleniumTask.js
+++ CourseSketchProjects/coursesketchwebclient/config/gruntTasks/seleniumTask.js
@@ -2,29 +2,31 @@ var selenium = require('selenium-standalone');
 
 module.exports = function(grunt) {
 
-    var seleniumChildProcesses = {};
-
-    function kill() {
-        grunt.log.writeln('Killing child process');
-        /* jshint -W089 */
-        for (var target in seleniumChildProcesses)
-            seleniumChildProcesses[target].kill();
-    }
-
     grunt.util.hooker.hook(grunt.fail, function() {
         // Clean up selenium if we left it running after a failure.
         grunt.log.writeln('Attempting to clean up running selenium server.');
-        kill();
+        var seleniumChildProcesses = global['seleniumChildProcesses'];
+        if (seleniumChildProcesses && seleniumChildProcesses.kill) {
+            grunt.log.writeln('Child is killable');
+            seleniumChildProcesses.kill();
+            grunt.log.writeln('Child is dead');
+        }
         grunt.log.writeln('Server Gas been cleaned');
+    });
+
+    grunt.registerMultiTask('seleniumKill', 'kills selenium', function() {
+        grunt.log.writeln('killing the child using task');
+        var seleniumChildProcesses = global['seleniumChildProcesses'];
+        if (seleniumChildProcesses && seleniumChildProcesses.kill) {
+            grunt.log.writeln('Child is killable');
+            seleniumChildProcesses.kill();
+            grunt.log.writeln('Child is dead');
+        }
     });
 
     grunt.registerMultiTask('seleniumStandalone', 'Run and installs selenium', function() {
         grunt.log.write('Installing chrome driver\n');
         var done = this.async();
-        var target = this.target;
-        this.kill = kill;
-        grunt.selenium = {};
-        grunt.selenium.kill = kill;
 
         selenium.install({
             // check for more recent versions of selenium here:
@@ -66,7 +68,7 @@ module.exports = function(grunt) {
             function(err, child) {
                 if (err) return done(err);
                 grunt.log.writeln('Server should hopefully be running unless there is an error');
-                seleniumChildProcesses[target] = child;
+                global['seleniumChildProcesses'] = child;
                 child.stderr.on('data', function(data) {
                     grunt.log.write(data.toString());
                 });
