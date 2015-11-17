f816e7f5d856f3a066f480e73ba422a6b98e98e7
dtracers <dtracers@gmail.com>
2015-11-16 18:39:32 -0600

    Added more to the file parser
    It now parses the files for the qunit results and creates a stack trace
    if there are line number errors

--- CourseSketchProjects/coursesketchwebclient/src/test/src/testUtilities/mochaHelp/qunitFileParser.js
+++ CourseSketchProjects/coursesketchwebclient/src/test/src/testUtilities/mochaHelp/qunitFileParser.js
@@ -1,20 +1,114 @@
-var mainPath = require.main.filename;
-var home = mainPath.substring(0, mainPath.indexOf('node_modules'));
-var output = home + '/target/unitTests';
-var fs = require('fs');
 
-var $ = require('jquery');
-var jsdom = require("jsdom");
 
-module.exports = {
-    parseFile: function(testResults, callback) {
-        console.log(testResults);
-        for (var index in testResults) {
-            var html = testResults[index];
-            jsdom.env(html,[], function(err, window) {
-                var passingTests = window.$('.pass');
-                console.log(passingTests);
-            });
-        }
+var jsdom = require('jsdom');
+var Barrier = require('barrier');
+var localHostReg = /http:\/\/localhost:\d+\//;
+
+var exports = {};
+
+module.exports = exports;
+
+exports.parseFile = function(testResults, callback) {
+    var testObject = [];
+    var barrier = Barrier.createBarrier(testResults.length, function() {
+        callback(testObject);
+    });
+    for (var index in testResults) {
+        var html = testResults[index];
+        jsdom.env(html,[], function(err, window) {
+            var $ = require('jquery')(window);
+            var passingTests = $('.fail');
+            var html = $(passingTests).html();
+            if (typeof html !== "undefined") {
+                // Tests that failed
+                testObject.push(createFailingTest(getModuleName($), getTestName($), getTestTime($), $));
+            } else {
+                // Tests that passed
+                testObject.push(createPassingTest(getModuleName($), getTestName($), getTestTime($)));
+            }
+            barrier();
+        });
     }
 };
+
+function getModuleName($) {
+    return $('.module-name').html();
+}
+
+function getTestName($) {
+    return $('.test-name').html();
+}
+
+function getTestTime($) {
+    return $('.runtime').html();
+}
+
+function createPassingTest(moduleName, testName, runtime) {
+    return {
+        passing: true,
+        message: moduleName + ': [' + testName + '] ran in ' + runtime,
+        moduleName: moduleName,
+        testName: testName,
+        runtime: runtime
+    }
+}
+
+
+function createFailingTest(moduleName, testName, runtime, $) {
+    if (typeof moduleName == 'undefined') {
+        moduleName = 'global';
+    }
+
+    if (typeof testName == 'undefined') {
+        testName = 'global';
+        runtime = 'N/A';
+    }
+    var message = $('.test-message').first().html();
+    return {
+        passing: false,
+        message: message,
+        moduleName: moduleName,
+        testName: testName,
+        runtime: runtime,
+        stackTrace: getFailedLineNumbers($)
+    }
+}
+
+function getFailedLineNumbers($) {
+    var source = $('.test-source pre').html();
+    var stackTrace = source.split(/\sat\s/g).map(function(string) {
+        // removes "http://localhost:PORT/" and trims extra spaces
+        var stringResult = string.trim();
+        if (stringResult == '') {
+            return;
+        }
+        var lineNumberIndex = stringResult.indexOf(' ');
+        stringResult = stringResult.replace(localHostReg, '');
+        var object = stringResult.substring(0, lineNumberIndex).trim();
+        // gets everything after the localhost, removes the parenthesis then splits by colons
+        var lineInfo = stringResult.substring(lineNumberIndex).replace(/[()]/g,'').split(':');
+        return {
+            object: object,
+            // gets the file path and removes any ? from the url
+            file: lineInfo[0].replace(/[?].*/,'').trim(),
+            line: lineInfo[1].trim(),
+            column: lineInfo[2].trim()
+        };
+    });
+    return cleanArray(stackTrace);
+}
+
+/**
+ * Removes an undefined objects from the array
+ * @param actual
+ * @returns {Array}
+ */
+function cleanArray(actual) {
+    var newArray = [];
+    for (var i = 0; i < actual.length; i++) {
+        if (actual[i]) {
+            newArray.push(actual[i]);
+        }
+    }
+    return newArray;
+}
