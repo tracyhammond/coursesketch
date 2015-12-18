416e11c3fce25ec2789ffdac75e6422d09a6ca97
dtracers <dtracers@gmail.com>
2015-12-18 14:24:13 -0600

    Added code to the submission websocket client
    Former-commit-id: 26ace28d7445f10a48feb9ad51983d0ef8e3d39b

--- CourseSketchProjects/rpcserver/src/main/java/coursesketch/services/submission/SubmissionWebSocketClient.java
+++ CourseSketchProjects/rpcserver/src/main/java/coursesketch/services/submission/SubmissionWebSocketClient.java
@@ -1,5 +1,6 @@
 package coursesketch.services.submission;
 
+import com.google.protobuf.ServiceException;
 import coursesketch.database.auth.AuthenticationException;
 import coursesketch.database.auth.Authenticator;
 import coursesketch.database.submission.SubmissionManagerInterface;
@@ -8,10 +9,12 @@ import coursesketch.server.interfaces.AbstractServerWebSocketHandler;
 import database.DatabaseAccessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import protobuf.srl.request.Message;
 import protobuf.srl.services.submission.SubmissionServer;
 import protobuf.srl.submission.Submission;
 
 import java.net.URI;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -25,14 +28,14 @@ public class SubmissionWebSocketClient extends ClientWebSocket implements Submis
     private static final Logger LOG = LoggerFactory.getLogger(SubmissionWebSocketClient.class);
 
     /**
-     * The default address for the identity server.
+     * The default address for the Submission server.
      */
-    public static final String ADDRESS = "IDENTITY_IP_PROP";
+    public static final String ADDRESS = "SUBMISSION_IP_PROP";
 
     /**
-     * The default port of the Identity Server.
+     * The default port of the Submission Server.
      */
-    public static final int PORT = 8891;
+    public static final int PORT = 8892;
 
     /**
      * The blocker service that is used to communicate.
@@ -57,9 +60,30 @@ public class SubmissionWebSocketClient extends ClientWebSocket implements Submis
     }
 
     @Override public List<Submission.SrlExperiment> getSubmission(final String authId, final Authenticator authenticator,
-            final String problemId, final String... submissionIds) {
+            final String problemId, final String... submissionIds) throws DatabaseAccessException {
         if (submissionService == null) {
             submissionService = SubmissionServer.SubmissionService.newBlockingStub(getRpcChannel());
+        }
+
+        final SubmissionServer.SubmissionRequest request = SubmissionServer.SubmissionRequest.newBuilder()
+                .setAuthId(authId)
+                .setProblemId(problemId)
+                .addAllSubmissionIds(Arrays.asList(submissionIds))
+                .build();
+
+        SubmissionServer.ExperimentResponse response = null;
+        try {
+            LOG.debug("Sending submission request");
+            response = submissionService.getSubmission(getNewRpcController(), request);
+            LOG.debug("Submission response {}", response);
+            if (response.hasDefaultResponse() && response.getDefaultResponse().hasException()) {
+                final DatabaseAccessException authExcep =
+                        new DatabaseAccessException("Exception with submission server");
+                authExcep.setProtoException(response.getDefaultResponse().getException());
+                throw authExcep;
+            }
+        } catch (ServiceException e) {
+            throw new DatabaseAccessException("Exception inserting submission into submission server", e);
         }
         return null;
     }
@@ -69,7 +93,33 @@ public class SubmissionWebSocketClient extends ClientWebSocket implements Submis
         if (submissionService == null) {
             submissionService = SubmissionServer.SubmissionService.newBlockingStub(getRpcChannel());
         }
-        return null;
+
+        LOG.debug("Creating submission request");
+
+        final SubmissionServer.ExperimentInsert request = SubmissionServer.ExperimentInsert.newBuilder()
+                .setRequestData(SubmissionServer.SubmissionRequest.newBuilder()
+                        .setAuthId(authId)
+                        .setProblemId(submission.getProblemId())
+                        .build())
+                .setSubmission(submission)
+                .setSubmissionTime(submissionTime)
+                .build();
+
+        SubmissionServer.SubmissionResponse response = null;
+        try {
+            LOG.debug("Sending submission request");
+            response = submissionService.insertExperiment(getNewRpcController(), request);
+            LOG.debug("Submission response {}", response);
+            if (response.hasDefaultResponse() && response.getDefaultResponse().hasException()) {
+                final DatabaseAccessException authExcep =
+                        new DatabaseAccessException("Exception with submission server");
+                authExcep.setProtoException(response.getDefaultResponse().getException());
+                throw authExcep;
+            }
+        } catch (ServiceException e) {
+            throw new DatabaseAccessException("Exception inserting submission into submission server", e);
+        }
+        return response.getSubmissionId();
     }
 
     @Override public String insertSolution(final String authId, final Authenticator authenticator, final Submission.SrlSolution submission)
@@ -77,7 +127,32 @@ public class SubmissionWebSocketClient extends ClientWebSocket implements Submis
         if (submissionService == null) {
             submissionService = SubmissionServer.SubmissionService.newBlockingStub(getRpcChannel());
         }
-        return null;
+
+        LOG.debug("Creating submission request");
+
+        final SubmissionServer.SolutionInsert request = SubmissionServer.SolutionInsert.newBuilder()
+                .setRequestData(SubmissionServer.SubmissionRequest.newBuilder()
+                        .setAuthId(authId)
+                        .setProblemId(submission.getProblemBankId())
+                        .build())
+                .setSubmission(submission)
+                .build();
+
+        SubmissionServer.SubmissionResponse response = null;
+        try {
+            LOG.debug("Sending submission request");
+            response = submissionService.insertSolution(getNewRpcController(), request);
+            LOG.debug("Submission response {}", response);
+            if (response.hasDefaultResponse() && response.getDefaultResponse().hasException()) {
+                final DatabaseAccessException authExcep =
+                        new DatabaseAccessException("Exception with submission server");
+                authExcep.setProtoException(response.getDefaultResponse().getException());
+                throw authExcep;
+            }
+        } catch (ServiceException e) {
+            throw new DatabaseAccessException("Exception inserting submission into submission server", e);
+        }
+        return response.getSubmissionId();
     }
 
 }
