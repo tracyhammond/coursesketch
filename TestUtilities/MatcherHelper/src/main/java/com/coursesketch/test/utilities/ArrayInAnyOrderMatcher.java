ca666b198ea9414db74fb5326164bced5f96442c
dtracers <dtracers@gmail.com>
2015-12-28 00:19:40 -0600

    Fixed checkstyle errors
    Also added support for any Iterable that the matcher can match against
    
    
    Former-commit-id: 0dd6bb697b010602a0d911c0a042d3d05b00c265

--- TestUtilities/MatcherHelper/src/main/java/com/coursesketch/test/utilities/ArrayInAnyOrderMatcher.java
+++ TestUtilities/MatcherHelper/src/main/java/com/coursesketch/test/utilities/ArrayInAnyOrderMatcher.java
@@ -12,31 +12,53 @@ import java.util.Collections;
 import java.util.List;
 
 /**
+ * A matcher that matches lists or varags in any order.
  * Created by dtracers on 12/27/2015.
+ *
+ * @param <E> An element that should be an instance of Comparable
  */
-public class ArrayInAnyOrderMatcher<E> extends ArgumentMatcher<E[]> implements VarargMatcher {
+public final class ArrayInAnyOrderMatcher<E> extends ArgumentMatcher<E[]> implements VarargMatcher {
 
     /**
      * Declaration and Definition.
      */
     private static final Logger LOG = LoggerFactory.getLogger(ArrayInAnyOrderMatcher.class);
 
+    /**
+     * The list that is expected.
+     */
     private final Iterable<E> listToMatchTo;
 
+    /**
+     * Constructor that takes in the iterable.
+     * @param listToMatchTo The expected list that is compared later on.
+     */
     public ArrayInAnyOrderMatcher(final Iterable<E> listToMatchTo) {
         this.listToMatchTo = listToMatchTo;
     }
 
+    /**
+     * Matches if the given object matches the Iterable.
+     * @param es
+     * @return
+     */
     @Override public boolean matches(final Object es) {
         List<E> casting = null;
         if (es.getClass().isArray()) {
             final E[] obj = (E[]) es;
             casting = Arrays.asList(obj);
+        } else if (es instanceof Iterable) {
+            casting = new ArrayList<>();
+            for (E element: (Iterable<E>) es) {
+                casting.add(element);
+            }
         } else {
             casting = (List<E>) Arrays.asList(es);
         }
         final List<Comparable> matchingList = new ArrayList<>();
         final List<Comparable> expectedList = new ArrayList<>();
+
+        // converts E -> Comparable for the sort later on
         for (E element: casting) {
             matchingList.add((Comparable) element);
         }
--- TestUtilities/MatcherHelper/src/main/java/com/coursesketch/test/utilities/CourseSketchMatcher.java
+++ TestUtilities/MatcherHelper/src/main/java/com/coursesketch/test/utilities/CourseSketchMatcher.java
@@ -1,7 +1,6 @@
 package com.coursesketch.test.utilities;
 
 import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
 
 import java.util.Arrays;
 
@@ -10,10 +9,26 @@ import java.util.Arrays;
  */
 public final class CourseSketchMatcher {
 
+    /**
+     * Creates a matcher for the given list that can be matched in any order.
+     *
+     * It takes in an iterable.
+     * @param list The expected list that is being matched against.
+     * @param <E> Must extend comparable as the lists are sorted before matching.
+     * @return A matcher that compares lists that can be in any order.
+     */
     public static <E extends Comparable> Matcher<E[]> iterableEqualAnyOrder(final Iterable<E> list) {
         return new ArrayInAnyOrderMatcher<E>(list);
     }
 
+    /**
+     * Creates a matcher for the given list that can be matched in any order.
+     *
+     * It takes in an array.
+     * @param list The expected list that is being matched against.
+     * @param <E> Must extend comparable as the lists are sorted before matching.
+     * @return A matcher that compares lists that can be in any order.
+     */
     public static <E extends Comparable> Matcher<E[]> iterableEqualAnyOrder(final E[] list) {
         return new ArrayInAnyOrderMatcher<E>(Arrays.asList(list));
     }
