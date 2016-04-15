3aa98c35a4a8d4ef4846eb9a3f593826bb223f22
DonaldMcRonald <raunak1997@utexas.edu>
2016-04-15 15:26:49 -0500

    working on recogServer and recogMangaer

--- CourseSketchProjects/coursesketchwebclient/src/main/src/common/recognition/recognitionManager.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/common/recognition/recognitionManager.js
@@ -8,7 +8,17 @@
     var recognitionRpcDefinition = function(method, req, callback) {
         var generalRequest = CourseSketch.prutil.GeneralRecognitionRequest();
         if (method === '.RecognitionService.addUpdate') {
-
+            generalRequest.setRequestType(CourseSketch.prutil.RecognitionRequestType.ADD_UPDATE);
+            generalRequest.setAddUpdate(req);
+        } else if (method === '.RecognitionService.createUpdateList') {
+            generalRequest.setRequestType(CourseSketch.prutil.RecognitionRequestType.SET_NEW_LIST);
+            generalRequest.setSetUpdateList(req);
+        } else if (method === '.RecognitionService.addTemplate') {
+            generalRequest.setRequestType(CourseSketch.prutil.RecognitionRequestType.ADD_TEMPLATE);
+            generalRequest.setTemplate(req);
+        } else if (method === '.RecognitionService.recognize') {
+            generalRequest.setRequestType(CourseSketch.prutil.RecognitionRequestType.SET_NEW_LIST);
+            generalRequest.setTemplate(req);
         }
 
         var request = CourseSketch.prutil.createRequestFromData(generalRequest, CourseSketch.prutil.getRequestClass().MessageType.RECOGNITION);
