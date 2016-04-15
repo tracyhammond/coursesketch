d318546d6d022743d4ec5e2476bb6b25fbe17713
DonaldMcRonald <raunak1997@utexas.edu>
2016-04-15 03:16:14 -0500

    finished recognitionconnetion.java

--- CourseSketchProjects/CourseSketchProxyServer/src/main/java/connection/RecognitionConnection.java
+++ CourseSketchProjects/CourseSketchProxyServer/src/main/java/connection/RecognitionConnection.java
@@ -5,12 +5,16 @@ import java.net.URI;
 
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
+import coursesketch.recognition.framework.exceptions.RecognitionException;
 import coursesketch.server.base.ClientWebSocket;
 
 import coursesketch.server.interfaces.AbstractServerWebSocketHandler;
 import coursesketch.services.recognition.RecognitionWebSocketClient;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import protobuf.srl.commands.Commands;
+import protobuf.srl.request.Message;
 import protobuf.srl.services.recognition.RecognitionServer;
+import protobuf.srl.sketch.Sketch;
 import utilities.ConnectionException;
 
 /**
@@ -37,24 +41,48 @@ public class RecognitionConnection extends RecognitionWebSocketClient {
         super(destination, parent);
     }
 
-    public void parseConnection(final ByteString otherData) throws ConnectionException {
+    public void parseConnection(final Message.Request request) throws ConnectionException, RecognitionException {
         RecognitionServer.GeneralRecognitionRequest generalRecognitionRequest;
         try {
             generalRecognitionRequest = RecognitionServer.GeneralRecognitionRequest
-                    .parseFrom(otherData);
+                    .parseFrom(request.getOtherData());
         } catch (InvalidProtocolBufferException e) {
             throw new ConnectionException("Unable to parse proto request for recognition", e);
         }
+        RecognitionServer.RecognitionResponse.Builder response = null;
+        Commands.SrlUpdateList updateList;
         switch (generalRecognitionRequest.getRequestType()) {
             case ADD_UPDATE:
-                super.addUpdate(generalRecognitionRequest.getAddUpdate().getRecognitionId(), generalRecognitionRequest.getAddUpdate().)
+                updateList = super.addUpdate(generalRecognitionRequest.getAddUpdate().getRecognitionId(), generalRecognitionRequest.getAddUpdate().getUpdate());
+                response.setChanges(updateList);
                 break;
             case SET_NEW_LIST:
+                updateList = super.setUpdateList(generalRecognitionRequest.getSetUpdateList().getRecognitionId(), generalRecognitionRequest.getSetUpdateList().getUpdateList());
+                response.setChanges(updateList);
                 break;
             case ADD_TEMPLATE:
+                if (generalRecognitionRequest.getTemplate().hasShape()) {
+                     super.addTemplate(generalRecognitionRequest.getTemplate().getShape());
+                }
+                else if (generalRecognitionRequest.getTemplate().hasSketch()) {
+                    super.addTemplate(generalRecognitionRequest.getTemplate().getSketch());
+                }
+                else {
+                    super.addTemplate(generalRecognitionRequest.getTemplate().getStroke());
+                }
                 break;
             case RECOGNIZE:
+                updateList = super.recognize(generalRecognitionRequest.getSetUpdateList().getRecognitionId(), generalRecognitionRequest.getSetUpdateList().getUpdateList());
+                response.setChanges(updateList);
                 break;
         }
+
+        Message.Request.Builder requestResponse = Message.Request.newBuilder(request);
+        if (response != null) {
+            requestResponse.setOtherData(response.build().toByteString());
+        } else {
+            requestResponse.setOtherData(Message.DefaultResponse.getDefaultInstance().toByteString());
+        }
+        super.onMessage(requestResponse.build().toByteString().asReadOnlyByteBuffer());
     }
 }
