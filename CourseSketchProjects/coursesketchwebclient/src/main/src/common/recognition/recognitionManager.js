500d47fa1ea713ebf1ea74bd60b1aabb753b5377
dtracers <dtracers@gmail.com>
2016-04-15 21:03:05 +0000

    Worked on implemtning server connection and response

--- CourseSketchProjects/coursesketchwebclient/src/main/src/common/recognition/recognitionManager.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/common/recognition/recognitionManager.js
@@ -7,6 +7,7 @@
     CourseSketch.connection.setRecognitionListener(CourseSketch.dataListener.getListenerHook());
     var recognitionRpcDefinition = function(method, req, callback) {
         var generalRequest = CourseSketch.prutil.GeneralRecognitionRequest();
+        var returnType = CourseSketch.prutil.RecognitionResponseClass();
         if (method === '.RecognitionService.addUpdate') {
             generalRequest.setRequestType(CourseSketch.prutil.RecognitionRequestType.ADD_UPDATE);
             generalRequest.setAddUpdate(req);
@@ -16,15 +17,18 @@
         } else if (method === '.RecognitionService.addTemplate') {
             generalRequest.setRequestType(CourseSketch.prutil.RecognitionRequestType.ADD_TEMPLATE);
             generalRequest.setTemplate(req);
+            returnType = CourseSketch.prutil.DefaultResponseClass();
         } else if (method === '.RecognitionService.recognize') {
             generalRequest.setRequestType(CourseSketch.prutil.RecognitionRequestType.SET_NEW_LIST);
             generalRequest.setTemplate(req);
         }
 
         var request = CourseSketch.prutil.createRequestFromData(generalRequest, CourseSketch.prutil.getRequestClass().MessageType.RECOGNITION);
-        CourseSketch.dataListener.sendRequestWithTimeout(request, function (msg) {
-            
-        });
+        CourseSketch.dataListener.sendRequestWithTimeout(request, function (evt, msg) {
+            // TODO: add exception checking
+            // if (msg instanceof CourseSketch.)
+            callback(undefined, msg);
+        }, 1, returnType);
     };
 
     CourseSketch.recognition = CourseSketch.prutil.RecognitionService(recognitionRpcDefinition);
--- CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/connection/advanceDataListener.js
+++ CourseSketchProjects/coursesketchwebclient/src/main/src/utilities/connection/advanceDataListener.js
@@ -86,9 +86,10 @@ function AdvanceDataListener(Request, defListener) {
      * @param {String} requestId - The unique identifier for the request.
      * @param {Function} func - The function that is called as a result of listening.
      * @param {Number} [times] - the number of times you want the function to be called before it is removed.
+     * @param {ProtoBufMessage} returnType - This is the type of message that other data is decoded into.
      */
-    this.setDataResultListener = function(messageType, requestId, func, times) {
-        setListener(messageType, requestId, queryWrap(func), times);
+    this.setDataResultListener = function(messageType, requestId, func, times, returnType) {
+        setListener(messageType, requestId, queryWrap(func, returnType), times);
     };
 
     /**
@@ -108,13 +109,46 @@ function AdvanceDataListener(Request, defListener) {
      *
      * @param {Function} func - The function that is wrapper to process data results.
      * @returns {Function} A wrapped function that processes data results.
+     * @param {ProtoBufMessage} returnType - This is the type of message that other data is decoded into.
      */
-    function queryWrap(func) {
+    function queryWrap(func, returnType) {
         return function(evt, msg, listener) {
+            function manageTimeCallback(callbackData) {
+                if (listener.times >= 0) {
+                    listener.times -= 1;
+                    if (!isUndefined(func)) {
+                        try {
+                            func(evt, callbackData);
+                            if (listener.times === 0) {
+                                removeListener(msg.requestType, msg.requestId);
+                                return false;
+                            }
+                        } catch (exception) {
+                            if (listener.times === 0) {
+                                removeListener(msg.requestType, msg.requestId);
+                                return false;
+                            }
+                            CourseSketch.clientException(exception);
+                        }
+                    } else {
+                        defListener(evt, item);
+                    } // if isUndefined func
+                } else {
+                    removeListener(msg.requestType, msg.requestId);
+                    return false;
+                }
+                return true;
+            }
+
             var result = undefined;
             if (msg.otherData === TIMEOUT_CONST) {
                 removeListener(msg.requestType, msg.requestId);
                 func(evt, new AdvanceListenerException('Connection to the database Timed Out'));
+                return;
+            }
+            if (!isUndefined(messageType)) {
+                result = CourseSketch.prutil.decodeProtobuf(msg.otherData, returnType);
+                manageTimeCallback(result);
                 return;
             }
             try {
@@ -133,22 +167,9 @@ function AdvanceDataListener(Request, defListener) {
             for (var i = 0; i < dataList.length; i++) {
                 //console.log('Decoding listener');
                 var item = dataList[i];
-                if (listener.times >= 0) {
-                    listener.times -= 1;
-                    if (!isUndefined(func)) {
-                        try {
-                            func(evt, item);
-                        } catch (exception) {
-                            removeListener(msg.requestType, msg.requestId);
-                            CourseSketch.clientException(exception);
-                        }
-                    } else {
-                        defListener(evt, item);
-                    } // if isUndefined func
-                } else {
-                    removeListener(msg.requestType, msg.requestId);
-                    return; // no more callbacks
-                } // if times >= 0
+                if (!manageTimeCallback(item)) {
+                    break;
+                }
             } // for
             if (listener.times <= 0) {
                 removeListener(msg.requestType, msg.requestId);
@@ -209,8 +230,9 @@ function AdvanceDataListener(Request, defListener) {
      * @param {Request} request - The protobuf request being sent to the server.
      * @param {Function} callback - The function that is called as a result of listening.
      * @param {Number} [times] - The number of times you want the function to be called before it is removed.
+     * @param {ProtoBufMessage} returnType - This is the type of message that other data is decoded into
      */
-    this.sendRequestWithTimeout = function(request, callback, times) {
+    this.sendRequestWithTimeout = function(request, callback, times, returnType) {
         var callbackCalled = false;
         var callbackTimedOut = false;
         var timeoutVariable = undefined;
@@ -240,7 +262,7 @@ function AdvanceDataListener(Request, defListener) {
             throw new AdvanceListenerException('The server responded but took too long to respond.');
         };
         // set listener
-        this.setDataResultListener(request.requestType, request.requestId, wrappedCallback, times);
+        this.setDataResultListener(request.requestType, request.requestId, wrappedCallback, times, returnType);
 
         if (!CourseSketch.connection.isConnected()) {
             console.log('The server is not connected all messages will be queued till reconnection is made.');
