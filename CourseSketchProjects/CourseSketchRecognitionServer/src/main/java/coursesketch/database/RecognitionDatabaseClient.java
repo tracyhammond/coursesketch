62474cb4c3571e05f98e0e576238b19ac53b1031
DonaldMcRonald <raunak1997@utexas.edu>
2016-04-16 14:01:21 -0500

    can now store a template if passed in a stroke

--- CourseSketchProjects/CourseSketchRecognitionServer/src/main/java/coursesketch/database/RecognitionDatabaseClient.java
+++ CourseSketchProjects/CourseSketchRecognitionServer/src/main/java/coursesketch/database/RecognitionDatabaseClient.java
@@ -1,9 +1,6 @@
 package coursesketch.database;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
+import com.mongodb.*;
 import coursesketch.database.interfaces.AbstractCourseSketchDatabaseReader;
 import coursesketch.recognition.framework.TemplateDatabaseInterface;
 import coursesketch.server.interfaces.ServerInfo;
@@ -11,8 +8,10 @@ import database.DatabaseAccessException;
 import protobuf.srl.sketch.Sketch;
 
 import java.util.List;
+import java.util.UUID;
 
 import static coursesketch.database.RecognitionStringConstants.*;
+import static database.DatabaseStringConstants.EXPERIMENT_COLLECTION;
 
 /**
  * Created by David Windows on 4/13/2016.
@@ -46,11 +45,19 @@ public class RecognitionDatabaseClient extends AbstractCourseSketchDatabaseReade
 
     @Override
     public void addTemplate(Sketch.SrlInterpretation srlInterpretation, Sketch.SrlStroke srlStroke) {
-        final DBObject templateObject;
-        final BasicDBObject interpretationQuery = new BasicDBObject(LABEL, srlInterpretation.getLabel())
-                .append(CONFIDENCE, srlInterpretation.getConfidence());
+        final DBCollection templates = database.getCollection(TEMPLATE_COLLECTION);
 
-        final BasicDBObject strokeQuery = MakeDbStoke(srlStroke);
+        final BasicDBObject templateObject = new BasicDBObject();
+
+        final BasicDBObject interpretationDbObject = makeSrlInterpretation(srlInterpretation);
+        final BasicDBObject strokeDbObject = makeDbStoke(srlStroke);
+
+        // TODO: Take in a TEMPLATE_ID instead of creating one here
+        templateObject.append(TEMPLATE_ID, UUID.randomUUID());
+        templateObject.append(TEMPLATE_INT, interpretationDbObject);
+        templateObject.append(TEMPLATE_TYPE, strokeDbObject);
+
+        templates.insert(templateObject);
     }
 
     @Override
@@ -58,8 +65,50 @@ public class RecognitionDatabaseClient extends AbstractCourseSketchDatabaseReade
         return null;
     }
 
-    private BasicDBObject MakeDbStoke(Sketch.SrlStroke srlStroke) {
-        final BasicDBObject strokeQuery =
-        return null;
+    private BasicDBObject makeDbStoke(Sketch.SrlStroke srlStroke) {
+        final BasicDBObject strokeDbObject = new BasicDBObject(STROKE_ID, srlStroke.getId())
+                .append(STROKE_TIME, srlStroke.getTime());
+
+        if (srlStroke.hasName()) {
+            strokeDbObject.append(STROKE_NAME, srlStroke.getName());
+        }
+
+        List<Object> pointsDbList = new BasicDBList();
+        List<Sketch.SrlPoint> pointsList = srlStroke.getPointsList();
+        for (Sketch.SrlPoint point : pointsList) {
+            BasicDBObject pointObject = makeSrlPoint(point);
+            pointsDbList.add(pointObject);
+        }
+
+        strokeDbObject.append(STROKE_POINTS, pointsDbList);
+        return strokeDbObject;
+    }
+
+    private BasicDBObject makeSrlPoint(Sketch.SrlPoint srlPoint) {
+        BasicDBObject pointObject = new BasicDBObject();
+
+        pointObject.append(POINT_ID, srlPoint.getId());
+        pointObject.append(POINT_TIME, srlPoint.getTime());
+        pointObject.append(POINT_X, srlPoint.getX());
+        pointObject.append(POINT_Y, srlPoint.getY());
+        if (srlPoint.hasName()) {
+            pointObject.append(POINT_NAME, srlPoint.getName());
+        }
+        if (srlPoint.hasPressure()) {
+            pointObject.append(POINT_PRESSURE, srlPoint.getPressure());
+        }
+        if (srlPoint.hasSize()) {
+            pointObject.append(POINT_SIZE, srlPoint.getSize());
+        }
+        if (srlPoint.hasSpeed()) {
+            pointObject.append(POINT_SPEED, srlPoint.getSpeed());
+        }
+
+        return pointObject;
+    }
+
+    private BasicDBObject makeSrlInterpretation(Sketch.SrlInterpretation srlInterpretation) {
+        return new BasicDBObject(INT_LABEL, srlInterpretation.getLabel())
+                .append(INT_CONFIDENCE, srlInterpretation.getConfidence());
     }
 }
--- CourseSketchProjects/CourseSketchRecognitionServer/src/main/java/coursesketch/database/RecognitionStringConstants.java
+++ CourseSketchProjects/CourseSketchRecognitionServer/src/main/java/coursesketch/database/RecognitionStringConstants.java
@@ -8,18 +8,26 @@ package coursesketch.database;
 
 @SuppressWarnings({ "PMD.CommentRequired", "checkstyle:javadocvariable" })
 public class RecognitionStringConstants {
-    public static final String LABEL = "Label";
-    public static final String CONFIDENCE = "Confidence";
+    public static final String TEMPLATE_COLLECTION  = "Templates";
 
-    public static final String STROKE_ID = "StrokeId";
-    public static final String STROKE_TIME = "StrokeTime";
-    public static final String STROKE_NAME = "StrokeName";
-    public static final String STROKE_POINTS = "StrokePoints";
+    public static final String TEMPLATE_ID          = "TemplateId";
+    public static final String TEMPLATE_INT         = "TemplateInterpretation";
+    public static final String TEMPLATE_TYPE        = "TemplateType";
 
-    public static final String POINT_ID = "PointId";
-    public static final String POINT_TIME = "PointTime";
-    public static final String POINT_NAME = "PointName";
-    public static final String POINT_X = "PointX";
-    public static final String POINT_Y = "PointY";
-    
+    public static final String INT_LABEL            = "InterpretationLabel";
+    public static final String INT_CONFIDENCE       = "InterpretationConfidence";
+
+    public static final String STROKE_ID            = "StrokeId";
+    public static final String STROKE_TIME          = "StrokeTime";
+    public static final String STROKE_NAME          = "StrokeName";
+    public static final String STROKE_POINTS        = "StrokePoints";
+
+    public static final String POINT_ID             = "PointId";
+    public static final String POINT_TIME           = "PointTime";
+    public static final String POINT_NAME           = "PointName";
+    public static final String POINT_X              = "PointX";
+    public static final String POINT_Y              = "PointY";
+    public static final String POINT_PRESSURE       = "PointPressure";
+    public static final String POINT_SIZE           = "PointSize";
+    public static final String POINT_SPEED          = "PointSpeed";
 }
