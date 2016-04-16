3178e4074ebe84ea37bb308a2220a384766440ea
DonaldMcRonald <raunak1997@utexas.edu>
2016-04-16 13:12:20 -0500

    creating recognitionDatabaseClient

--- CourseSketchProjects/CourseSketchRecognitionServer/src/main/java/coursesketch/database/RecognitionDatabaseClient.java
+++ CourseSketchProjects/CourseSketchRecognitionServer/src/main/java/coursesketch/database/RecognitionDatabaseClient.java
@@ -1,13 +1,25 @@
 package coursesketch.database;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 import coursesketch.database.interfaces.AbstractCourseSketchDatabaseReader;
+import coursesketch.recognition.framework.TemplateDatabaseInterface;
 import coursesketch.server.interfaces.ServerInfo;
 import database.DatabaseAccessException;
+import protobuf.srl.sketch.Sketch;
+
+import java.util.List;
+
+import static coursesketch.database.RecognitionStringConstants.*;
 
 /**
  * Created by David Windows on 4/13/2016.
  */
-public class RecognitionDatabaseClient extends AbstractCourseSketchDatabaseReader {
+public class RecognitionDatabaseClient extends AbstractCourseSketchDatabaseReader implements TemplateDatabaseInterface {
+    private DB database;
+
     public RecognitionDatabaseClient(final ServerInfo serverInfo) {
         super(serverInfo);
     }
@@ -17,6 +29,37 @@ public class RecognitionDatabaseClient extends AbstractCourseSketchDatabaseReade
     }
 
     @Override protected void onStartDatabase() throws DatabaseAccessException {
+        final MongoClient mongoClient = new MongoClient(super.getServerInfo().getDatabaseUrl());
+        database = mongoClient.getDB(super.getServerInfo().getDatabaseName());
+        super.setDatabaseStarted();
+    }
 
+    @Override
+    public void addTemplate(Sketch.SrlInterpretation srlInterpretation, Sketch.SrlSketch srlSketch) {
+
+    }
+
+    @Override
+    public void addTemplate(Sketch.SrlInterpretation srlInterpretation, Sketch.SrlShape srlShape) {
+
+    }
+
+    @Override
+    public void addTemplate(Sketch.SrlInterpretation srlInterpretation, Sketch.SrlStroke srlStroke) {
+        final DBObject templateObject;
+        final BasicDBObject interpretationQuery = new BasicDBObject(LABEL, srlInterpretation.getLabel())
+                .append(CONFIDENCE, srlInterpretation.getConfidence());
+
+        final BasicDBObject strokeQuery = MakeDbStoke(srlStroke);
+    }
+
+    @Override
+    public List<Sketch.SrlObject> getTemplate(Sketch.SrlInterpretation srlInterpretation) {
+        return null;
+    }
+
+    private BasicDBObject MakeDbStoke(Sketch.SrlStroke srlStroke) {
+        final BasicDBObject strokeQuery =
+        return null;
     }
 }
---
+++ CourseSketchProjects/CourseSketchRecognitionServer/src/main/java/coursesketch/database/RecognitionStringConstants.java
@@ -0,0 +1,25 @@
+package coursesketch.database;
+
+/**
+ * Contains a list of useful constants used by the recognition database.
+ *
+ * Created by Rauank on 4/16/16.
+ */
+
+@SuppressWarnings({ "PMD.CommentRequired", "checkstyle:javadocvariable" })
+public class RecognitionStringConstants {
+    public static final String LABEL = "Label";
+    public static final String CONFIDENCE = "Confidence";
+
+    public static final String STROKE_ID = "StrokeId";
+    public static final String STROKE_TIME = "StrokeTime";
+    public static final String STROKE_NAME = "StrokeName";
+    public static final String STROKE_POINTS = "StrokePoints";
+
+    public static final String POINT_ID = "PointId";
+    public static final String POINT_TIME = "PointTime";
+    public static final String POINT_NAME = "PointName";
+    public static final String POINT_X = "PointX";
+    public static final String POINT_Y = "PointY";
+    
+}
