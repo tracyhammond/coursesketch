915d74892693bc2000bdc5416ea37beed284d20b
dtracers <dtracers@gmail.com>
2016-04-29 22:27:45 -0500

    Added logging for json type objects

--- CourseSketchProjects/ServerAbstractor/src/main/java/utilities/LoggingConstants.java
+++ CourseSketchProjects/ServerAbstractor/src/main/java/utilities/LoggingConstants.java
@@ -1,5 +1,7 @@
 package utilities;
 
+import com.cedarsoftware.util.io.JsonWriter;
+
 /**
  * Created by Raunak on 2/26/15.
  */
@@ -8,4 +10,14 @@ public class LoggingConstants {
      * A constant for the exception messages.
      */
     public static final String EXCEPTION_MESSAGE = "Exception: {}";
+
+    /**
+     * Pretty Prints json for logging.
+     *
+     * @param json Takes in a json string and outputs the result.
+     * @return
+     */
+    public static final String prettyPrintJson(final String json) {
+        return JsonWriter.formatJson(json);
+    }
 }
